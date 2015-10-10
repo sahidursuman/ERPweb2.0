ef4a2aea4ee05f25be6b715bea5bdd03cb047a8c
duqinqin <13880834409@163.com>
2015-10-10 17:03:44 +0800

    修改商家管理查看页面的整体样式，页面修改成表格样式

--- js/template/resource/shop/shop.js
+++ js/template/resource/shop/shop.js
@@ -625,7 +625,7 @@ define(function(require, exports) {
 						    type: 1,
 						    title:"查看商家信息",
 						    skin: 'layui-layer-rim', //加上边框
-						    area: ['95%', '90%'], //宽高
+						    area: ['1024px', '600px'], //宽高
 						    zIndex:1028,
 						    content: html,
 						    success:function(){
