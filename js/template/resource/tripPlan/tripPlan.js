cbf89182e98e2fed61aa9a8769067cca5f0110e3
yangcanyi <289478310@qq.com>
2015-10-12 23:20:52 +0800

    迭代任务 #162 发团安排 如果付款方式选择的是 现付 通过js特效，直接将导付金额=应付-已付，不用手动填写

--- js/template/resource/tripPlan/tripPlan.js
+++ js/template/resource/tripPlan/tripPlan.js
@@ -1873,6 +1873,7 @@ define(function(require, exports) {
 				});
 			}
 		},
+		//计算 应付 导付
 		calculatePrice : function(){
 			var table = $(".table-tripPlan-container tbody tr"), price = 0, num = 0, reduceMoney = 0;
 			table.each(function(){
@@ -1886,9 +1887,22 @@ define(function(require, exports) {
 				_this.find("input[name=reduceMoney]").on("keyup", function(){
 					plusPrice(this);
 				});
+				_this.find("input[name=payedMoney]").on("keyup", function(){
+					plusPrice(this);
+				});
+				_this.find("select[name=payType]").on("change", function(){
+					if($(this).val()!=0){
+						$(this).parent().parent().find("input[name=guidePayMoney]").val("");
+					}else{
+						plusPrice(this);
+					}
+				});
 				
 				function plusPrice(obj){
 					var parents = $(obj).parent().parent();
+					var payType = parents.find("select[name=payType]").val(),
+						payedMoney = parents.find("input[name=payedMoney]").val(),
+						payedMoney = isNaN(payedMoney) ? 0 : payedMoney;
 					price = parseFloat(parents.find("input[name=price], input[name=fee]").val());
 					price = isNaN(price) ? 0 : price;
 					num = parseFloat(parents.find("input[name=memberCount], input[name=memberCount], input[name=needRoomCount]").val());
@@ -1897,6 +1911,10 @@ define(function(require, exports) {
 					reduceMoney = isNaN(reduceMoney) ? 0 : reduceMoney;
 
 					parents.find("input[name=needPayMoney]").val(price * num - reduceMoney);
+					
+					if(payType == 0){
+						parents.find("input[name=guidePayMoney]").val((price * num - reduceMoney)-payedMoney);
+					}
 				}
 			});			
 		},
