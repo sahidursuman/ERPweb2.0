c4ac3d54af9025a54b590d4fb3bdfeed1c06f187
yangcanyi <289478310@qq.com>
2015-10-16 15:32:51 +0800

    添加游客小组 功能修复

--- js/template/arrange/tripPlan/tripPlan.js
+++ js/template/arrange/tripPlan/tripPlan.js
@@ -1011,7 +1011,7 @@ define(function(require, exports) {
 													"</td>"+
 												"</tr>";
 											// 
-									    		$(".newAddTripPlan ."+tBody+"").append(html);
+									    		$("#"+tab+" ."+tBody+"").append(html);
 											}
 										}
 										layer.close(addGroupTemplateLayer);
