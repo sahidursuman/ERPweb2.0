abdc86619f57ce97975205361cdfd3d1ec84274d
wenJoy <laojiaodaoniu@126.com>
2015-11-04 15:24:47 +0800

    修复bug#2363 【财务管理-按团统计】安排预算表为安排数据，应付、到付等所有金额都不应该为实际报账金额 #2388 【按团统计】审核界面自费栏显示有误

--- js/template/financial/count/count.js
+++ js/template/financial/count/count.js
@@ -119,10 +119,7 @@ define(function(require, exports) {
                     	
         				var tripPlanList = JSON.parse(data.tripPlanList);
         				data.tripPlanList = tripPlanList;
-                        var html = listTableTemplate(data);
-
-                        html = count.authFilter(html,data.tripPlanList);
-                        $('.counterList').html(html);
+                        $('.counterList').html(listTableTemplate(data))
                         
                       //搜索按钮事件
                         $(".main-content .financialCount .clearBlur").blur(function(){
@@ -406,6 +403,7 @@ define(function(require, exports) {
 							count.edited["checkBill"] = "checkBill";
 							count.oldCheckBillId = id;
 						});
+						console.log("init");
             			$('.countUpdate .btn-saveCount').off('click').on('click',function() {
             				var id = $(this).attr('data-entity-id');
             				var financialTripPlanId = $(this).attr('data-entity-financial-id');
@@ -955,12 +953,10 @@ define(function(require, exports) {
                             var id = $(this).attr('data-entity-id');
                             count.viewGroupDetail(id);
                         });
-                        //中转明细
                         $('.countReimbursement .btn-viewTripTransit').off('click').on('click',function() {
                             var id = $(this).attr('data-entity-id');
                             count.ViewOutDetail(id);
                         });
-
                         //安排明细表
                         $('.countReimbursement .btn-tripPlanArrange').off('click').on('click',function() {
                             var id = $(this).attr('data-entity-id');
@@ -1403,25 +1399,7 @@ define(function(require, exports) {
 			});
 			//其他收入
 			$('.countUpdate').find('#otherIn').find('tr').each(function() {
-				/*if($(this).attr('otherInId')) {
-					var otherIn = {
-							"id":count.changeTwoDecimalToString($(this).attr('otherInId')),
-							"title":$(this).find('input[name=title]').val(),
-							"price":count.changeTwoDecimalToString($(this).find('input[name=price]').val()),
-							"count":count.changeTwoDecimalToString($(this).find('input[name=count]').val())
-					}
-					saveJson.otherInList.push(otherIn);
-					var log = {
-							"aid":otherIn.id,
-							"ot":$(this).find('input[name=title]').attr('old'),
-							"nt":otherIn.title,
-							"op":count.changeTwoDecimalToString(parseFloat($(this).find('input[name=price]').attr('old'))),
-							"np":otherIn.price,
-							"oc":count.changeTwoDecimalToString(parseFloat($(this).find('input[name=count]').attr('old'))),
-							"nc":otherIn.count
-					}
-					saveJson.log.otherInLog.push(log);
-				}*/
+				
                 if($(this).attr('otherInId')){
                     var otherIn = {
                     "id":$(this).attr('otherInId'),
@@ -2592,33 +2570,6 @@ define(function(require, exports) {
              	exportXLS(url)
              });
 	    },
-        authFilter : function(obj,tripPlanList){
-            var $obj = $(obj);
-            //报账过滤
-            $obj.find(".R-right").each(function(){
-                var right = $(this).data("right");
-                var auth = isAuth(right);
-                if(!auth){
-                    $(this).remove();
-                }   
-            });
-            //审核过滤
-            $obj.find(".T-audit").each(function(i){
-                var right,status = tripPlanList[i].tripPlan.billStatus;
-                if(status == 0 || status == 2){//财务审核
-                    right = "1190002"; 
-                } else if(status == 1){//计调审核
-                    right = "1190003";
-                } else{
-                    right = "无";
-                }
-                var auth = isAuth(right);
-                if(!auth){
-                    $(this).remove();
-                }
-            });
-            return $obj;
-        },
 		save : function(saveType){
 			console.log(saveType);
 			if(saveType == "checkBill"){
