326c5404a16f3f4619f2cfbd251cbbec91389d88
zhangjiangfeng <2826758674@qq.com>
2015-11-06 18:08:31 +0800

    业务分析 客户客量自适应

--- js/template/businessAnalyst/customerVolume/view/customerDetail.js
+++ js/template/businessAnalyst/customerVolume/view/customerDetail.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":69,"md5":"13db34ef5900d586a1840b6938311eaf"}*/
+/*TMODJS:{"debug":true,"version":95,"md5":"c0859801077601c882ee39c24957c346"}*/
 define(function(require) {
     return require("../../../template")("businessAnalyst/customerVolume/view/customerDetail", function($data, $filename) {
         try {
@@ -9,8 +9,8 @@ define(function(require) {
             $line = 13, $each(resultList, function(rs) {
                 $out += " <tr> <td>", $line = 15, $out += $escape(rs.name), $out += "</td> <td>", 
                 $line = 16, $out += $escape(rs.sumMember), $out += "</td> </tr> ", $line = 18;
-            }), $out += ' </tbody> </table> <div class="row pageMode"> <div class="col-xs-6"> <small>共计 ', 
-            $line = 24, $out += $escape(searchParam.totalCount), $out += ' 条记录</small> </div> <div class="col-xs-6"> <div class="dataTables_paginate paging_simple_numbers T-Detail-pagenation"> </div> </div> </div> ', 
+            }), $out += ' </tbody> </table> <div class="row pageMode"> <div class="col-xs-4"> <small>共计 ', 
+            $line = 24, $out += $escape(searchParam.totalCount), $out += ' 条记录</small> </div> <div style="width:320px; float:right;"> <div class="dataTables_paginate paging_simple_numbers T-Detail-pagenation"> </div> </div> </div> ', 
             new String($out);
         } catch (e) {
             throw {
@@ -18,7 +18,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '\r\n<table class="table table-striped table-bordered table-hover table-fixed" width="80%">\r\n	<colgroup>\r\n    	<col style="width:10%;"></col>\r\n    	<col style="width:10%;"></col>\r\n    </colgroup>\r\n	<thead>\r\n		<tr class="bg-blur">\r\n			<th width="22.5%" colspan="2">{{travelAgencyName}}--线路客量</th>\r\n		</tr>\r\n	</thead>\r\n	<tbody>\r\n	{{each resultList as rs}}\r\n		<tr>\r\n			<td>{{rs.name}}</td>\r\n			<td>{{rs.sumMember}}</td>\r\n		</tr>\r\n	{{/each}}			\r\n	</tbody>\r\n</table>\r\n\r\n<div class="row pageMode">\r\n	<div class="col-xs-6">\r\n		<small>共计 {{searchParam.totalCount}} 条记录</small>\r\n	</div>\r\n	<div class="col-xs-6">\r\n		<div class="dataTables_paginate paging_simple_numbers T-Detail-pagenation">\r\n		</div>\r\n	</div>\r\n</div>\r\n'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '\r\n<table class="table table-striped table-bordered table-hover table-fixed" width="80%">\r\n	<colgroup>\r\n    	<col style="width:10%;"></col>\r\n    	<col style="width:10%;"></col>\r\n    </colgroup>\r\n	<thead>\r\n		<tr class="bg-blur">\r\n			<th width="22.5%" colspan="2">{{travelAgencyName}}--线路客量</th>\r\n		</tr>\r\n	</thead>\r\n	<tbody>\r\n	{{each resultList as rs}}\r\n		<tr>\r\n			<td>{{rs.name}}</td>\r\n			<td>{{rs.sumMember}}</td>\r\n		</tr>\r\n	{{/each}}			\r\n	</tbody>\r\n</table>\r\n\r\n<div class="row pageMode">\r\n	<div class="col-xs-4">\r\n		<small>共计 {{searchParam.totalCount}} 条记录</small>\r\n	</div>\r\n	<div style="width:320px; float:right;">\r\n		<div class="dataTables_paginate paging_simple_numbers T-Detail-pagenation">\r\n		</div>\r\n	</div>\r\n</div>\r\n'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
