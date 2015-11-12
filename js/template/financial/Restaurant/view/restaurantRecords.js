96c79c3e13a8013a8b3a292968c793889047fa54
zhanghuan <1132672711@qq.com>
2015-11-12 17:52:34 +0800

    餐厅账务--结算--操作记录页面内容溢出

--- js/template/financial/Restaurant/view/restaurantRecords.js
+++ js/template/financial/Restaurant/view/restaurantRecords.js
@@ -1,10 +1,10 @@
-/*TMODJS:{"debug":true,"version":44,"md5":"eb91010574ef0143b6e8206afc772253"}*/
+/*TMODJS:{"debug":true,"version":58,"md5":"ae3b25622c358cdd5ca219147e8bb4de"}*/
 define(function(require) {
     return require("../../../template")("financial/Restaurant/view/restaurantRecords", function($data, $filename) {
         try {
             var $utils = this, $helpers = $utils.$helpers, $line = 0, $each = $utils.$each, financialRestaurantSettlementRecordList = $data.financialRestaurantSettlementRecordList, $escape = ($data.record, 
             $data.index, $utils.$escape), $out = "";
-            return $out += '<div class="row restaurantFinancialRecords"> <table class="table table-striped table-bordered table-hover"> <thead> <tr> <th>操作详情</th> </tr> </thead> <tbody> ', 
+            return $out += '<div class=" restaurantFinancialRecords"> <table class="table table-striped table-bordered table-hover"> <thead> <tr> <th class="th-border">操作详情</th> </tr> </thead> <tbody> ', 
             $line = 9, $each(financialRestaurantSettlementRecordList, function(record) {
                 $out += " <tr> <td>操作者:", $line = 11, $out += $escape(record.operationUser), $out += ",操作时间:", 
                 $line = 11, $out += $escape($helpers.dateFormat(record.operationTime, "yyyy-MM-dd hh:mm:ss")), 
@@ -19,7 +19,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '<div class="row restaurantFinancialRecords">\r\n <table class="table table-striped table-bordered table-hover">\r\n        <thead>\r\n        <tr>\r\n            <th>操作详情</th>\r\n        </tr>\r\n        </thead>\r\n        <tbody>\r\n          {{each financialRestaurantSettlementRecordList as record index}}\r\n	        <tr>\r\n	           <td>操作者:{{record.operationUser}},操作时间:{{record.operationTime | dateFormat: \'yyyy-MM-dd hh:mm:ss\'}},\r\n	                                            操作详情:(:{{record.year}}年{{record.month}}月,付款金额:{{record.payMoney}},付款方式:{{record.payType}},备注:{{record.remark}})</td>\r\n	        </tr>\r\n	      {{/each}}\r\n        </tbody>   \r\n    </table>\r\n</div> '.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '<div class=" restaurantFinancialRecords">\r\n <table class="table table-striped table-bordered table-hover">\r\n        <thead>\r\n        <tr>\r\n            <th class="th-border">操作详情</th>\r\n        </tr>\r\n        </thead>\r\n        <tbody>\r\n          {{each financialRestaurantSettlementRecordList as record index}}\r\n	        <tr>\r\n	           <td>操作者:{{record.operationUser}},操作时间:{{record.operationTime | dateFormat: \'yyyy-MM-dd hh:mm:ss\'}},\r\n	                                            操作详情:(:{{record.year}}年{{record.month}}月,付款金额:{{record.payMoney}},付款方式:{{record.payType}},备注:{{record.remark}})</td>\r\n	        </tr>\r\n	      {{/each}}\r\n        </tbody>   \r\n    </table>\r\n</div> '.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
