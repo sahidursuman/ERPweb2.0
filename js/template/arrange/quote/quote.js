3d86b99d1dacc8a618448ef693c6a1b3c87a1f38
yangcanyi <289478310@qq.com>
2015-11-27 17:53:41 +0800

    查询酒店在某天是否询价 bug修复2

--- js/template/arrange/quote/quote.js
+++ js/template/arrange/quote/quote.js
@@ -1094,7 +1094,7 @@ define(function(require, exports) {
 					    	var $hotelLayerContent = $(".T-hotelInquiryContainer");
 
 							//酒店是否已询价
-							quote.isInquiryHotel(quoteId, $hotelLayerContent);
+							quote.isInquiryHotel(quoteId, $hotelLayerContent, data, lineProductInfo);
 					    	quote.chooseRoomType($hotelLayerContent);
 					    	quote.dateTimePicker($hotelLayerContent);
 					    	var validator = rule.quoteCheckor($hotelLayerContent);
@@ -1192,7 +1192,7 @@ define(function(require, exports) {
 		})
 	};
 	//酒店是否已询价 (在某天)
-	quote.isInquiryHotel = function(quoteId, $layerContainer){
+	quote.isInquiryHotel = function(quoteId, $layerContainer, dataHotel, lineProductInfo){
 		if (!!quoteId) {
 			$.ajax({
 				url: KingServices.build_url("hotelInquiry","findIsInquiryHotleList"),
@@ -1213,6 +1213,14 @@ define(function(require, exports) {
 					}
 				}
 			})
+		}else{
+			dataHotel.days = [];
+			var html = '';
+			for (var i = 1,len = lineProductInfo.days.replace(/[^0-9]/ig,""); i <= len; i++) {
+				var time = quote.checkInTime(i,lineProductInfo.startTime);
+				html += '<option value="'+time+'">'+time+'</option>'
+			}
+			$layerContainer.find('[name=checkInTime]').html(html);
 		}
 	}
 
