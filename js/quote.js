9fe2feb819407429fa3012501e64056e80ef7fec
yangcanyi <289478310@qq.com>
2015-11-27 21:24:12 +0800

    增加其它安排 其他改为其它
    分享报价信息中 添加年月日计算

--- js/quote.js
+++ js/quote.js
@@ -12,11 +12,23 @@ define(function(require, exports) {
 				data.daysList = JSON.parse(data.daysList);
 				data.busCompanyArrange = JSON.parse(data.busCompanyArrange);
 				//var shareQuoteHtml = template("shareQuoteTemplate",data);
+				var startTime = data.quoteDetailJson.startTime;
+				for (var i = 0, len = data.daysList.length; i < len; i++) {
+					data.daysList[i].times = checkInTime(i,startTime);
+				}
 				var shareQuoteHtml = quoteTemplate(data);
 				$("body").html(shareQuoteHtml);
 			}
 			else{
 				alert(data.message);
+			}
+
+			function checkInTime(i,startTime) {
+				var	date = new Date(startTime.replace("-", "/").replace("-", "/"));
+				var timer = date.getTime()+(i)*24*60*60*1000;
+				date.setTime(timer);
+				var checkInTime = date.getFullYear()+ "-"+ (date.getMonth() + 1) + "-"+ (date.getDate() < 10 ? "0" + date.getDate() : date.getDate());
+				return checkInTime;
 			}
 		}
 	})
