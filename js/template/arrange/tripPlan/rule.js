66859e1a8c8f6602356d3d79bb67bba737999699
duqinqin <13880834409@163.com>
2015-11-30 17:58:31 +0800

    发团计划 车、丝、导

--- js/template/arrange/tripPlan/rule.js
+++ js/template/arrange/tripPlan/rule.js
@@ -28,47 +28,7 @@ var rule = {
     	    	        }
 		    	        ]
 		       },
-		    //    {//车队
-		    // 	    $ele: $checkdCreateTripPlanObj.find('input[name="BusCompanyName"]'), 
-			  	// 	$valObj: $checkdCreateTripPlanObj.find('input[name="busCompanyId"]'),   
-			  	// 	rules: [  
-			  	// 		{
-	    	// 	  			type:'null',
-	    	// 	  			errMsg: '车队不能为空'
-			  	// 		}
-			 		// ]
-		    //   },
-		      // ,{//车牌号
-		    	 //    	$ele: $checkdCreateTripPlanObj.find('input[name="LicenseNumber"]'),  
-		    	 //    	$valObj: $checkdCreateTripPlanObj.find('input[name="busLicenseNumberId"]'),
-
-		    	 //    	rules: [
-	  	    // 	        {
-	  	    // 	        	type: 'null', 
-	  	    // 	        	errMsg: '车牌号不能为空'
-	  	    // 	        }
-		    	 //        ]
-		      // }
-		      // ,{//导游driverId
-		    	 //       	$ele: $checkdCreateTripPlanObj.find('input[name="AddTPchooseGuide"]'),  
-		    	 //    	$valObj: $checkdCreateTripPlanObj.find('input[name="AddTPchooseGuideId"]'),  
-
-		    	 //    	rules: [  
-	  	    // 	        {
-	  	    // 	        	type: 'null', 
-	  	    // 	        	errMsg: '导游不能为空'
-	  	    // 	        }
-		    	 //        ]
-		      // },{//司机
-		    	 //       	$ele: $checkdCreateTripPlanObj.find('input[name="driverName"]'),  
-		    	 //    	$valObj: $checkdCreateTripPlanObj.find('input[name="driverId"]'),  
-		    	 //    	rules: [
-	  	    // 	        {
-	  	    // 	        	type: 'null', 
-	  	    // 	        	errMsg: '司机不能为空'
-	  	    // 	        }
-		    	 //        ]
-		      //    },
+		    
 		         {//全陪电话
 		    	       	$ele: $checkdCreateTripPlanObj.find('input[name="accompanyGuideMobile"]'),  
 		    	    	rules: [
