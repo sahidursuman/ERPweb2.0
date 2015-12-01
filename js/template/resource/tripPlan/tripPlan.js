15e5f6c03679bf35d27047b8c1035cac65e18a8f
zhangjiangfeng <2826758674@qq.com>
2015-12-01 21:08:00 +0800

    发团安排精度限制

--- js/template/resource/tripPlan/tripPlan.js
+++ js/template/resource/tripPlan/tripPlan.js
@@ -344,6 +344,9 @@ define(function(require, exports) {
 			$("#tripPlan_addPlan_ticket .addTicket").on("click",{validator:validator}, tripPlan.addTicket);
 			$("#tripPlan_addPlan_other .addOther").on("click",{validator:validator}, tripPlan.addOther);
 		
+			//精度控件 
+			var $price=$("#tripPlan_addPlan_content").find('.price');
+			Tools.inputCtrolFloat($price);
 
 			//一键下单操作
 			$("#tripPlan_addPlan_content").find('.T-singleClick-Order').on('click', function(event) {
@@ -598,14 +601,17 @@ define(function(require, exports) {
 				tableContainer = _this.parents(".ui-sortable-handle").find(".table tbody"),
 				html = '<tr><td><div class="col-sm-12"><input type="text" maxlength="32" name="insuranceName" class="col-sm-12 chooseInsurance bind-change"/><input type="hidden" name="insuranceId"/><span class="addResourceBtn T-addInsuranceResource R-right" data-right="1080002" title="添加保险公司"><i class="ace-icon fa fa-plus bigger-110 icon-only"></i></span></div></td>' +
 						'<td><input type="text" name="type" maxlength="32" class="col-sm-12"/></td>' +
-						'<td><input type="text" name="price" maxlength="6" class="col-sm-12"/></td>' +
+						'<td><input type="text" name="price" maxlength="6" class="col-sm-12 price"/></td>' +
 						'<td><input type="text" name="memberCount" class="col-sm-12" maxlength="8"/></td>' +
 						'<td><input type="text" name="needPayMoney" readonly="readonly" class="col-sm-12"/></td>' +
-						'<td><input type="text" name="payedMoney" class="col-sm-12" maxlength="9"/></td>' +
+						'<td><input type="text" name="payedMoney" class="col-sm-12 price" maxlength="9"/></td>' +
 						'<td><select name="payType" class="col-sm-12 no-padding"><option value="0">现付</option><option value="1">签单</option><option value="2">转账</option><option value="3">网付</option></select></td>' +		
 						'<td><input name="remark" type="text" class="col-sm-12" maxlength="500"/></td>' +
 						'<td><a class="cursor btn-deleteTripPlanList" title="删除">删除</a></td></tr>';
 			tableContainer.append(filterUnAuth(html));
+			//精度控件
+			var $price=tableContainer.find('.price');
+			Tools.inputCtrolFloat($price);
 			tripPlan.bindDeleteEvent();
 			tripPlan.bindInsuranceChoose();
 			tripPlan.calculatePrice();
@@ -618,12 +624,15 @@ define(function(require, exports) {
 			tableContainer = _this.parents(".ui-sortable-handle").find(".table tbody"),
 			html = '<tr><td><input type="text" name="guideName" readonly="readonly" class="col-sm-12 chooseGuide"/><input type="hidden" name="guideId"/></td>' +
 			'<td><input type="text" name="mobileNumber" class="col-sm-12" readonly="readonly"/></td>' +
-			'<td><input type="text" name="guideFee" class="col-sm-12"/></td>' +
-			'<td><input type="text" name="manageFee" class="col-sm-12"/></td>' +
+			'<td><input type="text" name="guideFee" class="col-sm-12 price"/></td>' +
+			'<td><input type="text" name="manageFee" class="col-sm-12 price"/></td>' +
 			'<td><select name="payType" class="col-sm-12 no-padding"><option value="0">现付</option><option value="1">签单</option><option value="2">转账</option><option value="3">网付</option></select></td>' +
 			'<td><input name="remark" type="text" class="col-sm-12"/></td>' +
 			'<td><button class="btn btn-xs btn-success" title="发送订单"><i class="ace-icon fa fa-paper-plane-o bigger-120"></i></button></td></tr>';
 			tableContainer.append(html);
+			//精度控件
+			var $price=tableContainer.find('.price');
+			Tools.inputCtrolFloat($price);
 			tripPlan.bindDeleteEvent();
 			//tripPlan.bindGuideChoose();
 			tripPlan.calculatePrice();
@@ -638,15 +647,18 @@ define(function(require, exports) {
 			'<td><input type="text" name="brand" class="col-sm-12"/></td>' +
 			'<td><input type="text" name="mobileNumber" class="col-sm-12"/></td>' +
 			'<td><input type="text" name="driverName" class="col-sm-12"/></td>' +
-			'<td><input type="text" name="price" class="col-sm-12" style="width: 60px;"/></td>' +
-			'<td><input type="text" name="reduceMoney" class="col-sm-12" style="width: 60px;"/></td>' +
+			'<td><input type="text" name="price" class="col-sm-12 price" style="width: 60px;"/></td>' +
+			'<td><input type="text" name="reduceMoney" class="col-sm-12 price" style="width: 60px;"/></td>' +
 			'<td><input type="text" name="needPayMoney" readonly="readonly" class="col-sm-12" style="width: 60px;"/></td>' +
-			'<td><input type="text" name="payedMoney" class="col-sm-12" style="width: 60px;"/></td>' +
+			'<td><input type="text" name="payedMoney" class="col-sm-12 price" style="width: 60px;"/></td>' +
 			'<td><select name="payType" class="col-sm-12 no-padding"><option value="0">现付</option><option value="1">签单</option><option value="2">转账</option><option value="3">网付</option></select></td>' +
 			'<td><input type="text" name="" class="col-sm-12" style="width: 60px;"/></td>' +
 			'<td><input type="text" name="remark" class="col-sm-12"/></td>' +
 			'<td><button class="btn btn-xs btn-success" title="发送订单"><i class="ace-icon fa fa-paper-plane-o bigger-120"></i></button></td></tr>';
 			tableContainer.append(html);
+			//精度控件
+			var $price=tableContainer.find('.price');
+			Tools.inputCtrolFloat($price);
 			tripPlan.bindDeleteEvent();
 			//tripPlan.bindBusCompanyChoose();
 			tripPlan.calculatePrice();
@@ -663,15 +675,18 @@ define(function(require, exports) {
 			'<td><select name="type" class="col-sm-12 restauranType" style="width:80px;"><option value="早餐">早餐</option><option value="午餐">午餐</option><option value="晚餐">晚餐</option></select></td>' +
 			'<td><input type="text" name="price" value="" class="col-sm-12 typeNameChoose"/><input type="hidden" name="restaurantStandardId" value="0"/></td>' +
 			'<td><input name="memberCount" type="text" class="col-sm-12" style="width: 60px;" maxlength="4"/></td>' +
-			'<td><input name="reduceMoney" type="text" class="col-sm-12" style="width: 60px;" maxlength="9"/></td>' +
+			'<td><input name="reduceMoney" type="text" class="col-sm-12 price" style="width: 60px;" maxlength="9"/></td>' +
 			'<td><input name="needPayMoney" readonly="readonly" type="text" class="col-sm-12" style="width: 60px;"/></td>' +
-			'<td><input name="payedMoney" type="text" class="col-sm-12" style="width: 60px;" maxlength="9"/></td>' +
+			'<td><input name="payedMoney" type="text" class="col-sm-12 price" style="width: 60px;" maxlength="9"/></td>' +
 			'<td><select name="payType" class="col-sm-12 no-padding" style="width:55px;"><option value="0">现付</option><option value="1">签单</option><option value="2">转账</option><option value="3">网付</option></select></td>' +
 			'<td><input name="guidePayMoney" type="text" class="col-sm-12" style="width: 60px;" maxlength="9"/></td>' +
 			'<td><input name="remark" type="text" class="col-sm-12"/></td>' +
 			//'<td><button class="btn btn-xs btn-success" title="发送订单"><i class="ace-icon fa fa-paper-plane-o bigger-120"></i></button></td></tr>';
 			'<td><a class="cursor btn-deleteTripPlanList" data-entity-name="restaurant" title="删除">删除</a></td>';
 			tableContainer.append(filterUnAuth(html));
+			//精度控件
+			var $price=tableContainer.find('.price');
+			Tools.inputCtrolFloat($price);
 			tripPlan.bindDeleteEvent();
 			tripPlan.bindRestaurantChoose();
 			tripPlan.bindMoneyTripPlan();
@@ -693,17 +708,20 @@ define(function(require, exports) {
 			'<td><input type="text" class="col-sm-12" readonly="readonly" name="managerName"/></td>' +
 			'<td><input type="text" class="col-sm-12" readonly="readonly" name="mobileNumber"/></td>' +
 			'<td><input type="text" class="col-sm-12 chooseHotelRoom" name="hotelRoom"/><input type="hidden" name="hotelRoomId"></td>' +
-			'<td><input type="text" class="col-sm-12" name="fee" style="width: 60px;" maxlength="6"/></td>' +
+			'<td><input type="text" class="col-sm-12 price" name="fee" style="width: 60px;" maxlength="6"/></td>' +
 			'<td><input type="text" class="col-sm-12" name="memberCount" style="width: 60px;" maxlength="6"/></td>' +
-			'<td><input type="text" class="col-sm-12" name="reduceMoney" style="width: 60px;" maxlength="9"/></td>' +
+			'<td><input type="text" class="col-sm-12 price" name="reduceMoney" style="width: 60px;" maxlength="9"/></td>' +
 			'<td><input type="text" class="col-sm-12" name="needPayMoney" readonly="readonly" style="width: 60px;"/></td>' +
-			'<td><input type="text" class="col-sm-12" name="payedMoney" style="width: 60px;" maxlength="9"/></td>' +
+			'<td><input type="text" class="col-sm-12 price" name="payedMoney" style="width: 60px;" maxlength="9"/></td>' +
 			'<td><select name="payType" class="col-sm-12 no-padding" style="width:55px;"><option value="0">现付</option><option value="1">签单</option><option value="2">转账</option><option value="3">网付</option></select></td>' +
 			'<td><input type="text" class="col-sm-12" name="guidePayMoney" style="width: 60px;" maxlength="9"/></td>' +
 			'<td><input type="text" class="col-sm-12" name="remark" maxlength="500"/></td>' +
 			'<td><a class="cursor btn-deleteTripPlanList" title="删除">删除</a></td></tr>';
 
 			tableContainer.append(filterUnAuth(html));
+			//精度控件
+			var $price=tableContainer.find('.price');
+			Tools.inputCtrolFloat($price);
 			tripPlan.bindDeleteEvent();
 			tripPlan.bindHotelChoose();
 			tripPlan.bindHotelRoomeChoose();
@@ -724,16 +742,19 @@ define(function(require, exports) {
 			'<td><select name="tourTime" class="col-sm-12 no-padding" style="width: 75px;"> <option value="全天">全天</option> <option value="上午">上午</option> <option value="下午">下午</option> </select> </td>' +
 			'<td><input type="text" name="tourDuration" class="col-sm-12" value="" style="width: 60px;" maxlength="3"> </td>' +
 			'<td><input type="text" name="orderNumber" class="col-sm-12" value="" maxlength="20"/></td>'+
-			'<td><input type="text" name="fee" class="col-sm-12" style="width: 60px;" maxlength="6"/></td>' +
+			'<td><input type="text" name="fee" class="col-sm-12 price" style="width: 60px;" maxlength="6"/></td>' +
 			'<td><input type="text" name="memberCount" class="col-sm-12" style="width: 60px;" maxlength="8"/></td>' +
-			'<td><input type="text" name="reduceMoney" class="col-sm-12" style="width: 60px;" maxlength="9"/></td>' +
+			'<td><input type="text" name="reduceMoney" class="col-sm-12 price" style="width: 60px;" maxlength="9"/></td>' +
 			'<td><input type="text" name="needPayMoney" readonly="readonly" class="col-sm-12" style="width: 60px;"/></td>' +
-			'<td><input type="text" name="payedMoney" class="col-sm-12" style="width: 60px;" maxlength="9"/></td>' +
+			'<td><input type="text" name="payedMoney" class="col-sm-12 price" style="width: 60px;" maxlength="9"/></td>' +
 			'<td><select name="payType" class="col-sm-12 no-padding" style="width:55px;"><option value="0">现付</option><option value="1">签单</option><option value="2">转账</option><option value="3">网付</option></select></td>' +
 			'<td><input type="text" name="guidePayMoney" class="col-sm-12" style="width: 60px;" maxlength="9"/></td>' +
 			'<td><input type="text" name="remark" class="col-sm-12" maxlength="500"/></td>' +
 			'<td><a class="cursor btn-deleteTripPlanList" title="删除">删除</a></td></tr>';
 			tableContainer.append(filterUnAuth(html));
+			//精度控件
+			var $price=tableContainer.find('.price');
+			Tools.inputCtrolFloat($price);
 			tripPlan.bindDeleteEvent(); 
 			tripPlan.bindScenicChoose();
 			tripPlan.bindMoneyTripPlan();
@@ -777,18 +798,21 @@ define(function(require, exports) {
 			'<td><input type="hidden" name="selfitemId" value="" /><input type="text" name="selfitem" class="col-sm-12 chooseSelfitem" value="" /></td>'+
 			'<td><input type="text" readonly="readonly" name="managerName" class="col-sm-12"/></td>' +
 			'<td><input type="text" readonly="readonly" name="mobileNumber" class="col-sm-12"/></td>' +
-			'<td><input type="text" name="oldPrice" class="col-sm-12" maxlength="6"/></td>' +
-			'<td><input type="text" name="price" class="col-sm-12" maxlength="6"/></td>' +
+			'<td><input type="text" name="oldPrice" class="col-sm-12 price" maxlength="6"/></td>' +
+			'<td><input type="text" name="price" class="col-sm-12 price" maxlength="6"/></td>' +
 			'<td><input type="text" name="memberCount" class="col-sm-12" maxlength="8"/></td>' +
-			'<td><input type="text" name="reduceMoney" class="col-sm-12" maxlength="9"/></td>' +
+			'<td><input type="text" name="reduceMoney" class="col-sm-12 price" maxlength="9"/></td>' +
 			'<td><input type="text" name="needPayMoney" readonly="readonly" class="col-sm-12" maxlength="9"/></td>' +
-			'<td><input type="text" name="payedMoney" class="col-sm-12" maxlength="9"/></td>' +
+			'<td><input type="text" name="payedMoney" class="col-sm-12 price" maxlength="9"/></td>' +
 			'<td><select name="payType" class="col-sm-12 no-padding" style="width:55px;"><option value="0">现付</option><option value="1">签单</option><option value="2">转账</option><option value="3">网付</option></select></td>' +
 			'<td><input type="text" name="guidePayMoney" class="col-sm-12" maxlength="9"/></td>' +
 			'<td><input type="text" name="remark" class="col-sm-12" maxlength="500"/></td>' +
 			'<td><a class="cursor btn-deleteTripPlanList" title="删除">删除</a></td></tr>';
 
 			tableContainer.append(filterUnAuth(html));
+			//精度控件
+			var $price=tableContainer.find('.price');
+			Tools.inputCtrolFloat($price);
 			tripPlan.bindDeleteEvent();
 			tripPlan.bindSelfPayChoose();
 			tripPlan.bindMoneyTripPlan();
@@ -810,17 +834,20 @@ define(function(require, exports) {
 			'<td><input type="text" name="shift" class="col-sm-12" maxlength="9"/></td>' +
 			'<td><input type="text" name="startTime" class="col-sm-13 col-xs-12 date-time-picker"/></td>' +
 			'<td><input type="text" name="seatLevel" class="col-sm-12" maxlength="32"/></td>' +
-			'<td><input type="text" name="fee" class="col-sm-12" maxlength="6"/></td>' +
+			'<td><input type="text" name="fee" class="col-sm-12 price" maxlength="6"/></td>' +
 			'<td><input type="text" name="memberCount" class="col-sm-12" maxlength="8"/></td>' +
-			'<td><input type="text" name="reduceMoney" class="col-sm-12" maxlength="9"/></td>' +
+			'<td><input type="text" name="reduceMoney" class="col-sm-12 price" maxlength="9"/></td>' +
 			'<td><input type="text" name="needPayMoney" readonly="readonly" class="col-sm-12"/></td>' +
-			'<td><input type="text" name="payedMoney" class="col-sm-12" maxlength="9"/></td>' +
+			'<td><input type="text" name="payedMoney" class="col-sm-12 price" maxlength="9"/></td>' +
 			'<td><select name="payType" class="col-sm-12 no-padding" style="width:55px;"><option value="0">现付</option><option value="1">签单</option><option value="2">转账</option><option value="3">网付</option></select></td>' +
 			'<td><input type="text" name="guidePayMoney" class="col-sm-12" maxlength="9"/></td>' +
 			'<td><input type="text" name="remark" class="col-sm-12" maxlength="500"/></td>' +
 			'<td><a class="cursor btn-deleteTripPlanList" title="删除">删除</a></td></tr>';
 
 			tableContainer.append(filterUnAuth(html));
+			//精度控件
+			var $price=tableContainer.find('.price');
+			Tools.inputCtrolFloat($price);
 			tripPlan.bindDeleteEvent();
 			tripPlan.bindTicketChoose();
 			tripPlan.bindMoneyTripPlan();
@@ -839,17 +866,20 @@ define(function(require, exports) {
 			'<td><input type="text" name="name" maxlength="32" class="col-sm-12"/></td>' +
 			'<td><input type="text" name="managerName" maxlength="32" class="col-sm-12"/></td>' +
 			'<td><input type="text" name="mobileNumber" class="col-sm-12" maxlength="11"/></td>' +
-			'<td><input type="text" name="fee" class="col-sm-12" maxlength="6"/></td>' +
+			'<td><input type="text" name="fee" class="col-sm-12 price" maxlength="6"/></td>' +
 			'<td><input type="text" name="memberCount" class="col-sm-12" maxlength="8"/></td>' +
-			'<td><input type="text" name="reduceMoney" class="col-sm-12" maxlength="9"/></td>' +
+			'<td><input type="text" name="reduceMoney" class="col-sm-12 price" maxlength="9"/></td>' +
 			'<td><input type="text" name="needPayMoney" readonly="readonly" class="col-sm-12"/></td>' +
-			'<td><input type="text" name="payedMoney" class="col-sm-12" maxlength="9"/></td>' +
+			'<td><input type="text" name="payedMoney" class="col-sm-12 price" maxlength="9"/></td>' +
 			'<td><select name="payType" class="col-sm-12 no-padding" style="width:55px;"><option value="0">现付</option><option value="1">签单</option><option value="2">转账</option><option value="3">网付</option></select></td>' +
 			'<td><input type="text" name="guidePayMoney" class="col-sm-12" maxlength="9"/></td>' +
 			'<td><input type="text" name="remark" class="col-sm-12" maxlength="500"/></td>' +
 			'<td><a class="cursor btn-deleteTripPlanList" title="删除">删除</a></td></tr>';
 
 			tableContainer.append(html);
+			//精度控件
+			var $price=tableContainer.find('.price');
+			Tools.inputCtrolFloat($price);
 			tripPlan.bindDeleteEvent();
 			tripPlan.bindMoneyTripPlan();
 			tripPlan.setChooseDays("tripPlan_addPlan_other");
