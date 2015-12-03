a678b14744f05b02e0ab510e66b15e80bdbde9f9
zhanghuan <1132672711@qq.com>
2015-12-03 21:27:06 +0800

    财务管理--其他账务--重构 list页面接口调试

--- js/template/financial/OtherAccounts/OtherAccounts.js
+++ js/template/financial/OtherAccounts/OtherAccounts.js
@@ -1,162 +1,233 @@
-define(function(require,exports){
-	var menuKey = "financial_Other_accounts",
-	listTemplate = require("./view/list"),
-	AccountsCheckingTemplate = require("./view/AccountsChecking"),
-	AccountsPaymentTemplate = require("./view/AccountsPayment"),
-	lookDetailTemplate = require("./view/lookDetail"),
-	viewhandleTemplate = require("./view/viewhandle"),
-	ViewAmountPaidTemplate = require("./view/ViewAmountPaid"),
-	viewOrderDetailTemplate = require("./view/viewOrderDetail"),
-	checkTabId = menuKey+"-checking";
-	var  OtherAccounts={
+define(function(require, exports) {
+    var menuKey = "financial_Other_accounts",
+        listTemplate = require("./view/list"),
+        AccountsCheckingTemplate = require("./view/AccountsChecking"),
+        AccountsPaymentTemplate = require("./view/AccountsPayment"),
+        lookDetailTemplate = require("./view/lookDetail"),
+        viewhandleTemplate = require("./view/viewhandle"),
+        ViewAmountPaidTemplate = require("./view/ViewAmountPaid"),
+        viewOrderDetailTemplate = require("./view/viewOrderDetail"),
+        checkTabId = menuKey + "-checking",
+        tabId = "tab-" + menuKey + "-content",
+        checkTabId = menuKey + "-checking",
+        blanceTabId = menuKey + "-blance";
 
-	};
+    var OtherAccounts = {
 
-		OtherAccounts.listFinancialOtherAccounts = function(page,OtherAccountsID,year,month){
-			// $.ajax({
-			// 	url: "",
-			// 	type: 'POST',
-			// 	dataType: "",
-			// 	data: "安安",
-			// 	dataType:'json',
-			// 	beforeSend:function(){
-			// 		globalLoadingLayer = openLoadingLayer();
-			// 	}
-					var html = listTemplate();
-						Tools.addTab(menuKey,"其他账务",html);
-					// 对账		
-					$(".T-other").find('.T-checking').click(function(event) {
-						OtherAccounts.AccountsChecking($(this).data('id'));
-					});
-					// 付款
-					$(".T-other").find('.T-payment').click(function(event) {
-						OtherAccounts.AccountsPayment($(this).data('id'));
-					});
+    };
+    OtherAccounts.initModule = function() {
+        var myDate = new Date(),
+            thisyear = myDate.getFullYear(),
+            thismonth = myDate.getMonth()+1,
+            endAccountTime= moment(new Date(myDate)).format("YYYY-MM-DD"),//结束日期
+            myDate =  new Date(thisyear+'-'+thismonth+'-01'),
+            startAccountTime = moment(new Date(myDate)).format("YYYY-MM-DD");//开始日期
+            OtherAccounts.listFinancialOtherAccounts(0,"",startAccountTime,endAccountTime);
+            
+    };
+
+    OtherAccounts.listFinancialOtherAccounts = function(pageNo, name, startAccountTime, endAccountTime) {
+        OtherAccounts.searchData = {
+            pageNo : pageNo,
+            name : name,
+            startAccountTime : startAccountTime,
+            endAccountTime : endAccountTime,
+            sortType: 'auto'
+        }
+        $.ajax({
+        	url:KingServices.build_url("account/arrangeOtherFinancial","listFinancialOther"),
+            type:"POST",
+            data:OtherAccounts.searchData,
+        	success: function(data){
+                console.log(data);
+                var result = showDialog(data);
+                if (result) {
+                    data.startAccountTime = startAccountTime
+                    data.endAccountTime = endAccountTime
+                    var html = listTemplate(data);
+                    Tools.addTab(menuKey, "其他账务", html);
+                    OtherAccounts.initList();
+                      // 对账     
+                    
+                    
+                }
+            }
+        
+      
 
 
-			// })
-			
-			// 对账
-		OtherAccounts.AccountsChecking = function(){
-			var html = AccountsCheckingTemplate();
-			Tools.addTab(checkTabId,"其他对账",html);
-			var $checkTabId = $("#tab-"+checkTabId+"-content");
-			// 查看已付金额明细
-			$(".T-Accounts").find('.T-lookDetail').click(function(event) {
-				event.preventDefault();
-				/* Act on the event */
-				OtherAccounts.lookDetail();
-			});
+        })
+    },
+    OtherAccounts.initList = function(){
+        // 初始化jQuery 对象
+    
+        OtherAccounts.$tab = $('#' + tabId);
+        OtherAccounts.$searchArea=OtherAccounts.$tab.find('.T-search-area');
+        //搜索按钮事件
+        OtherAccounts.$tab.find('.T-search').on('click', function(event) {
+            event.preventDefault();
+            OtherAccounts.listInsure(0);
+        });
+        // 搜索
+         OtherAccounts.$tab.find('.T-search').click(function(event) {
+            alert();
+        });
+        // 报表内的操作
+        OtherAccounts.$tab.find('.T-other').on('click', '.T-option', function(event) {
+            alert();
+            event.preventDefault();
+            var $that = $(this),
+                id = $that.closest('tr').data('id'),
+                yearList = OtherAccounts.$searchArea.find("select[name=year]").val(),
+                monthList = OtherAccounts.$searchArea.find("select[name=month]").val();
+                console.log(id);
 
-			//应付金额明细
-			$(".T-Accounts").find('.T-viewhandle').click(function(event) {
-				event.preventDefault();
-				/* Act on the event */
-				OtherAccounts.viewhandle();
-			});
-			
-			//时间控件
-			var $container = $(".T-Accounts");
-			$container.find("input[name=joinTime]").datepicker({
-				autoclose: true,
-				todayHighlight: true,
-				format: 'yyyy-mm-dd',
-				language: 'zh-CN'
-			});
-			//关闭页面事件
-	        $container.find(".T-closeTab").click(function(){
-	            closeTab(checkTabId);
-	        });
-			}	
-			//付款
-		OtherAccounts.AccountsPayment = function(){
-			var html = AccountsPaymentTemplate();
-			Tools.addTab("-payment","其他付款",html);
+            if ($that.hasClass('T-check')) {
+                // 对账
+                OtherAccounts.AccountsChecking();
+            } else if ($that.hasClass('T-payment')) {
+                // 结算
+                OtherAccounts.AccountsPayment();
+            }
+        });
+    };
 
-			//已付付金额明细
-			$(".T-AccountsPayment").find('.T-ViewAmountPaid').click(function(event) {
-				event.preventDefault();
-				/* Act on the event */
-				OtherAccounts.ViewAmountPaid();
-			});
-			//应付金额明细
-			$(".T-AccountsPayment").find('.T-viewPaid').click(function(event) {
-				event.preventDefault();
-				/* Act on the event */
-				OtherAccounts.viewOrderDetail();
-			});
-									//时间控件
-			var $container = $(".T-AccountsPayment");
-			$container.find("input[name=joinTime]").datepicker({
-				autoclose: true,
-				todayHighlight: true,
-				format: 'yyyy-mm-dd',
-				language: 'zh-CN'
-			});
-		}
+        // 对账
+        OtherAccounts.AccountsChecking = function() {
+                var html = AccountsCheckingTemplate();
+                Tools.addTab(checkTabId, "其他对账", html);
+                var $checkTabId = $("#tab-" + checkTabId + "-content");
+                // 查看已付金额明细
+                $(".T-Accounts").find('.T-lookDetail').click(function(event) {
+                    event.preventDefault();
+                    /* Act on the event */
+                    OtherAccounts.lookDetail();
+                });
 
-			// // 查看已付金额a-1
-		OtherAccounts.lookDetail = function(){
-				var html = lookDetailTemplate();
-				var lookDetailLayer = layer.open({
-				    type: 1,
-				    title:"已付金额明细",
-				    skin: 'layui-layer-rim', //加上边框
-				    area: '55%', //宽高
-				    zIndex:1028,
-				    content: html,
-				    scrollbar: false,
+                //应付金额明细
+                $(".T-Accounts").find('.T-viewhandle').click(function(event) {
+                    event.preventDefault();
+                    /* Act on the event */
+                    OtherAccounts.viewhandle();
+                });
 
-				    });
-		}
-		//应付金额明细a-2
-		OtherAccounts.viewhandle = function(){
-				var html = viewhandleTemplate();
-				var lookDetailLayer = layer.open({
-				    type: 1,
-				    title:"应付金额明细",
-				    skin: 'layui-layer-rim', //加上边框
-				    area: '55%', //宽高
-				    zIndex:1028,
-				    content: html,
-				    scrollbar: false,
+                //时间控件
+                var $container = $(".T-Accounts");
+                $container.find("input[name=joinTime]").datepicker({
+                    autoclose: true,
+                    todayHighlight: true,
+                    format: 'yyyy-mm-dd',
+                    language: 'zh-CN'
+                });
+                //关闭页面事件
+                $container.find(".T-closeTab").click(function() {
+                    closeTab(checkTabId);
+                });
+                //给全选按钮绑定事件
+                $container.find('.T-selectAll').click(function(event) {
+                var checkboxList = $(".T-Accounts").find(".T-checkListNum tr input[type=checkbox]");
+                if($(this).is(":checked")){
+                    checkboxList.each(function(i){
+                        $(this).prop("checked",true);
+                    });
+                } else{
+                    checkboxList.each(function(i){
+                        if(!$(this).prop("disabled")){
+                            $(this).prop("checked",false);
+                        }                                
+                    });
+                }
+                });
+            }
+            //付款
+        OtherAccounts.AccountsPayment = function() {
+            var html = AccountsPaymentTemplate();
+            Tools.addTab("-payment", "其他付款", html);
 
-				    });
-		}
-		//已付金额明细b-1
-		OtherAccounts.ViewAmountPaid = function(){
-				var html = ViewAmountPaidTemplate();
-				var lookDetailLayer = layer.open({
-				    type: 1,
-				    title:"已付金额明细",
-				    skin: 'layui-layer-rim', //加上边框
-				    area: '55%', //宽高
-				    zIndex:1028,
-				    content: html,
-				    scrollbar: false,
+            //已付付金额明细
+            $(".T-AccountsPayment").find('.T-ViewAmountPaid').click(function(event) {
+                event.preventDefault();
+                /* Act on the event */
+                OtherAccounts.ViewAmountPaid();
+            });
+            //应付金额明细
+            $(".T-AccountsPayment").find('.T-viewPaid').click(function(event) {
+                event.preventDefault();
+                /* Act on the event */
+                OtherAccounts.viewOrderDetail();
+            });
+            //时间控件
+            var $container = $(".T-AccountsPayment");
+            $container.find("input[name=joinTime]").datepicker({
+                autoclose: true,
+                todayHighlight: true,
+                format: 'yyyy-mm-dd',
+                language: 'zh-CN'
+            });
+        }
 
-				    });
-		}
-		//应付金额明细b-2
-		OtherAccounts.viewOrderDetail = function(){
-				var html = viewOrderDetailTemplate();
-				var lookDetailLayer = layer.open({
-				    type: 1,
-				    title:"应付金额明细",
-				    skin: 'layui-layer-rim', //加上边框
-				    area: '55%', //宽高
-				    zIndex:1028,
-				    content: html,
-				    scrollbar: false,
-				    });
-		}
-			
-			
+        // // 查看已付金额a-1
+        OtherAccounts.lookDetail = function() {
+                var html = lookDetailTemplate();
+                var lookDetailLayer = layer.open({
+                    type: 1,
+                    title: "已付金额明细",
+                    skin: 'layui-layer-rim', //加上边框
+                    area: '55%', //宽高
+                    zIndex: 1028,
+                    content: html,
+                    scrollbar: false,
 
-		}
-		
+                });
+            }
+            //应付金额明细a-2
+        OtherAccounts.viewhandle = function() {
+                var html = viewhandleTemplate();
+                var lookDetailLayer = layer.open({
+                    type: 1,
+                    title: "应付金额明细",
+                    skin: 'layui-layer-rim', //加上边框
+                    area: '55%', //宽高
+                    zIndex: 1028,
+                    content: html,
+                    scrollbar: false,
 
-	
+                });
+            }
+            //已付金额明细b-1
+        OtherAccounts.ViewAmountPaid = function() {
+                var html = ViewAmountPaidTemplate();
+                var lookDetailLayer = layer.open({
+                    type: 1,
+                    title: "已付金额明细",
+                    skin: 'layui-layer-rim', //加上边框
+                    area: '55%', //宽高
+                    zIndex: 1028,
+                    content: html,
+                    scrollbar: false,
 
-	exports.init = OtherAccounts.listFinancialOtherAccounts;
+                });
+            }
+            //应付金额明细b-2
+        OtherAccounts.viewOrderDetail = function() {
+            var html = viewOrderDetailTemplate();
+            var lookDetailLayer = layer.open({
+                type: 1,
+                title: "应付金额明细",
+                skin: 'layui-layer-rim', //加上边框
+                area: '55%', //宽高
+                zIndex: 1028,
+                content: html,
+                scrollbar: false,
+            });
+        }
+
+
+
+    
+
+
+
+
+    exports.init = OtherAccounts.initModule;
 })
--- js/template/financial/OtherAccounts/view/list.js
+++ js/template/financial/OtherAccounts/view/list.js
@@ -1,4 +1,46 @@
-/*TMODJS:{"debug":true,"version":218,"md5":"cc488b9e93e949a0dcfcdb31d4d95d97"}*/
+/*TMODJS:{"debug":true,"version":275,"md5":"068ccd7e1fae9208b3eefe5c0642c4c0"}*/
 define(function(require) {
-    return require("../../../template")("financial/OtherAccounts/view/list", '<div class="row globalAdd T-other" > <div class="form-horizontal search-area"> <div class="form-group" > <label class="control-label pull-left marginLeft-30" >项目名称：</label> <select class="col-xs-1" name="busCompanyId" > <option value ="">全部</option> <option value=""</option> </select> <label class="control-label pull-left marginLeft-30" >账期：</label> <select class="col-sm-1" name="year" > <option value=""></option> </select> <select class="col-sm-1 marginLeft-30" name="month" > <option value ="">全部</option> <option value="">月</option> </select> <button class="btn-sm btn-busCompany-search search-btn marginLeft-30" > <i class="ace-ico n fa fa-search"></i> 搜索 </button> </div> </div> <div class="col-xs-12"> <table class="table table-striped table-bordered table-hover T-showHighLight"> <thead> <tr class="bg-blur"> <th>项目名称</th> <th>账面应付</th> <th>结算金额</th> <th>已付金额</th> <th>未付金额</th> <th>对账进度</th> <th>付款进度</th> <th>操作</th> </tr> </thead> <tbody> <tr> <td>122</td> <td>122</td> <td>122</td> <td>122</td> <td>122</td> <td>122<span style="color:red"></span></td> <td>1212</td> <td> <a class=" btn-divide T-checking cursor R-right" data-right=""> 对账 </a> <label class="cursor R-right" data-right=""><a class="R-right" data-right=""> |</a></label> <a class="btn-guide-view R-right cursor btn-transfter T-payment " data-right=""> 付款 </a> </td> </tr> </tbody> </table> </div> </div>');
+    return require("../../../template")("financial/OtherAccounts/view/list", function($data, $filename) {
+        try {
+            var $utils = this, $line = ($utils.$helpers, 0), $each = $utils.$each, financialOtherList = $data.financialOtherList, $escape = ($data.other, 
+            $data.index, $utils.$escape), searchParam = $data.searchParam, $out = ($data.year, 
+            $data.month, $data.$index, "");
+            return $out += '<div class="row globalAdd T-other" > <div class="form-horizontal search-area"> <div class="form-group" > <label class="control-label pull-left marginLeft-30" >项目名称：</label> <select class="col-xs-1" name="busCompanyId" > ', 
+            $line = 6, $each(financialOtherList, function(other) {
+                $out += " <option>", $line = 7, $out += $escape(other.name), $out += "</option> ", 
+                $line = 8;
+            }), $out += ' </select> <label class="control-label pull-left marginLeft-30" >账期：</label> <select class="col-sm-1" name="year" > ', 
+            $line = 12, $each(searchParam, function(year) {
+                $out += ' <option value="', $line = 13, $out += $escape(year.startAccountTime), 
+                $out += '" ', $line = 13, searchParam.year == year.startAccountTime && ($out += 'selected="selected"', 
+                $line = 13), $out += ">", $line = 13, $out += $escape(year.startAccountTime), $out += "</option> ", 
+                $line = 14;
+            }), $out += ' </select> <select class="col-sm-1 marginLeft-30" name="month" > ', 
+            $line = 17, $each(searchParam, function(month) {
+                $out += ' <option value="', $line = 18, $out += $escape(month.endAccountTime), $out += '" ', 
+                $line = 18, searchParam.month == month.endAccountTime && ($out += 'selected="selected"', 
+                $line = 18), $out += ">", $line = 18, $out += $escape(month.endAccountTime), $out += "</option> ", 
+                $line = 19;
+            }), $out += ' </select> <button class="btn-sm T-search search-btn marginLeft-30" > <i class="ace-ico n fa fa-search"></i> 搜索 </button> </div> </div> <div class="col-xs-12"> <table class="table table-striped table-bordered table-hover T-showHighLight"> <thead> <tr class="bg-blur"> <th>项目名称</th> <th>账面应付</th> <th>结算金额</th> <th>已付金额</th> <th>未付金额</th> <th>对账进度</th> <th>付款进度</th> <th>操作</th> </tr> </thead> <tbody> ', 
+            $line = 41, $each(financialOtherList, function(other) {
+                $out += " <tr> <td>", $line = 43, $out += $escape(other.name), $out += "</td> <td>", 
+                $line = 44, $out += $escape(other.sumNeedPayMoney), $out += "</td> <td>", $line = 45, 
+                $out += $escape(other.sumSettlementMoney), $out += "</td> <td>", $line = 46, $out += $escape(other.sumPayedMoney), 
+                $out += "</td> <td>", $line = 47, $out += $escape(other.sumUnPayedMoney), $out += '</td> <td><span style="color:red">', 
+                $line = 48, $out += $escape(other.confirmedCount), $out += "/", $line = 48, $out += $escape(other.confirmedCount), 
+                $out += "</span></td> <td>", $line = 49, $out += $escape(other.payedCount), $out += "/", 
+                $line = 49, $out += $escape(other.payedCount), $out += '</td> <td> <a class=" btn-divide T-checking cursor R-right" data-right=""> 对账 </a> <label class="cursor R-right" data-right=""><a class="R-right" data-right=""> |</a></label> <a class="btn-guide-view R-right cursor btn-transfter T-payment " data-right=""> 付款 </a> </td> </tr> ', 
+                $line = 61;
+            }), $out += ' </tbody> </table> <div class="row pageMode"> <div class="col-xs-6"> <small>共计 2 条记录</small> </div> <div class="col-xs-6"> <div class="dataTables_paginate paging_simple_numbers T-pagenation"> </div> </div> </div> </div> </div>', 
+            new String($out);
+        } catch (e) {
+            throw {
+                filename: $filename,
+                name: "Render Error",
+                message: e.message,
+                line: $line,
+                source: '<div class="row  globalAdd T-other" >\r\n    <div class="form-horizontal search-area">    \r\n        <div class="form-group" >      \r\n          <label class="control-label pull-left marginLeft-30" >项目名称：</label>\r\n                <select  class="col-xs-1" name="busCompanyId" >\r\n                     {{each financialOtherList as other index}}\r\n                        <option>{{other.name}}</option>\r\n                    {{/each}}\r\n                </select>        \r\n           <label class="control-label pull-left marginLeft-30" >账期：</label>\r\n                <select class="col-sm-1" name="year" >\r\n                    {{each searchParam as year index}}\r\n                    <option value="{{year.startAccountTime}}" {{if searchParam.year == year.startAccountTime}}selected="selected"{{/if}}>{{year.startAccountTime}}</option>\r\n                    {{/each}}\r\n                </select> \r\n                <select class="col-sm-1 marginLeft-30" name="month" >\r\n                    {{each searchParam as month index}}\r\n                        <option value="{{month.endAccountTime}}" {{if searchParam.month == month.endAccountTime}}selected="selected"{{/if}}>{{month.endAccountTime}}</option>\r\n                    {{/each}}\r\n                </select>\r\n            <button class="btn-sm T-search search-btn marginLeft-30" >\r\n                <i class="ace-ico n fa fa-search"></i> 搜索\r\n            </button>\r\n        </div>\r\n    </div>\r\n        <div class="col-xs-12">\r\n            <table class="table table-striped table-bordered table-hover T-showHighLight">\r\n                <thead>\r\n                <tr class="bg-blur">\r\n                    <th>项目名称</th>\r\n                    <th>账面应付</th>\r\n                    <th>结算金额</th>\r\n                    <th>已付金额</th>\r\n                    <th>未付金额</th>\r\n                    <th>对账进度</th>\r\n                    <th>付款进度</th>\r\n                    <th>操作</th>\r\n                </tr>\r\n                </thead>\r\n                <tbody>\r\n                 {{each financialOtherList as other}}\r\n                <tr>\r\n                    <td>{{other.name}}</td>\r\n                    <td>{{other.sumNeedPayMoney}}</td>\r\n                    <td>{{other.sumSettlementMoney }}</td>\r\n                    <td>{{other.sumPayedMoney }}</td>\r\n                    <td>{{other.sumUnPayedMoney }}</td>\r\n                    <td><span style="color:red">{{other.confirmedCount }}/{{other.confirmedCount}}</span></td>\r\n                    <td>{{other.payedCount }}/{{other.payedCount}}</td>\r\n                    <td>\r\n                    <a  class=" btn-divide T-checking cursor R-right" data-right="">\r\n                            对账\r\n                        </a>\r\n                        <label class="cursor R-right" data-right=""><a class="R-right" data-right=""> |</a></label>\r\n                        <a  class="btn-guide-view R-right cursor btn-transfter T-payment " data-right="">\r\n                        \r\n                            付款\r\n                        </a>\r\n                    </td>\r\n                </tr>\r\n                {{/each}}\r\n               \r\n                </tbody>\r\n            </table>\r\n             <div class="row pageMode">\r\n                <div class="col-xs-6">\r\n                    <small>共计 2 条记录</small>\r\n                </div>\r\n                <div class="col-xs-6">\r\n                    <div class="dataTables_paginate paging_simple_numbers T-pagenation">\r\n                    </div>\r\n                </div>\r\n            </div>\r\n        </div>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+            };
+        }
+    });
 });
