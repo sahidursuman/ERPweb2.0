8b5c542fd7ef0e8e9a0e8ffa0be7c29743d7d90d
guiyun <wei_guiyun@163.com>
2015-12-09 11:25:53 +0800

    【景区账务】完成前端业务重构，接口待联调

--- js/template/financial/Scenic/rule.js
+++ js/template/financial/Scenic/rule.js
@@ -2,40 +2,34 @@ define(function(require, exports) {
 	var rule = {
 		check:function($obj){
 			var validator = $obj.formValidate([
-	    	    {	//实际未付
-	    	    	$ele: $obj.find('input[name=FinancialScenicRealUnPayedMoney]'),
+	    	    {	//结算金额
+	    	    	$ele: $obj.find('input[name=settlementMoney]'),
 	    	    	rules: [
-	    	    	        {
-	    	    	        	type: 'null', 
-	    	    	        	errMsg: '不能为空'
-	    	    	        },
-	    	    	        {
-	    	    	        	type: 'float',
-	    	    	        	errMsg: '请输入数字'
-	    	    	        }
-		    	        ]
+    	    	        {
+    	    	        	type: 'float',
+    	    	        	errMsg: '请输入数字'
+    	    	        }
+	    	        ]
 	    	    },
-	    	    
-	    	    {//付款金额
-	    	    	$ele: $obj.find('input[name=blancerealPayedMoney]'),
-	    	    	
+	    	    {	//本次付款总额
+	    	    	$ele: $obj.find('input[name=sumPayMoney]'),
+	    	    	rules: [
+    	    	        {
+    	    	        	type: 'float',
+    	    	        	errMsg: '请输入数字'
+    	    	        }
+	    	        ]
+	    	    },
+	    	  	{//付款金额
+	    	    	$ele: $obj.find('input[name=payMoney]'),
 	    	    	rules:[
-	    	    	       
-	    	    	       {
-	    	    	    	   type:'null',
-	    	    	    	   errMsg:'金额不能为空'   
-	    	    	       },
-	    	    	       
 	    	    	       {
 	    	    	    	   type: 'float',
 	    	    	        	errMsg: '请输入数字'   
 	    	    	       }
-	    	    	       
-	    	    	       ]
-	    	    }
-	    	    
+    	    	       ]
+	    	    	}
                 ]);
-			
 			return validator;
 		}
 	}
