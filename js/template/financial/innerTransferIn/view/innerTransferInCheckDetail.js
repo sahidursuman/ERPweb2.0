876d30d634a029508798402172c8c21c419229d5
wenJoy <laojiaodaoniu@126.com>
2015-12-11 21:52:17 +0800

    内转转入账务

---
+++ js/template/financial/innerTransferIn/view/innerTransferInCheckDetail.js
@@ -0,0 +1,26 @@
+/*TMODJS:{"debug":true,"version":7,"md5":"35945713de151e1086cfb53291d10e63"}*/
+define(function(require) {
+    return require("../../../template")("financial/innerTransferIn/view/innerTransferInCheckDetail", function($data, $filename) {
+        try {
+            var $utils = this, $helpers = $utils.$helpers, $line = 0, $each = $utils.$each, incomeDetailsList = $data.incomeDetailsList, $escape = ($data.detail, 
+            $data.$index, $utils.$escape), $out = "";
+            return $out += '<div style="form-group"> <table class="table table-striped table-bordered table-hover" > <thead> <tr> <th class="th-border">应收类别</th> <th class="th-border">发生业务</th> <th class="th-border">金额</th> <th class="th-border">备注</th> <th class="th-border">操作者</th> <th class="th-border">操作时间</th> </tr> </thead> <tbody> ', 
+            $line = 14, $each(incomeDetailsList, function(detail) {
+                $out += " <tr> <td>", $line = 16, $out += $escape(detail.receivableType), $out += "</td> <td>", 
+                $line = 17, $out += $escape(detail.businessType), $out += "</td> <td>", $line = 18, 
+                $out += $escape(detail.incomeDifferenceMoney), $out += "</td> <td>", $line = 19, 
+                $out += $escape(detail.remark), $out += "</td> <td>", $line = 20, $out += $escape(detail.checkRealname), 
+                $out += "</td> <td>", $line = 21, $out += $escape($helpers.dateFormat(detail.checkTime, "yyyy-MM-dd")), 
+                $out += "</td> </tr> ", $line = 23;
+            }), $out += " </tbody> </table> </div> ", new String($out);
+        } catch (e) {
+            throw {
+                filename: $filename,
+                name: "Render Error",
+                message: e.message,
+                line: $line,
+                source: '<div style="form-group">\r\n	<table class="table table-striped table-bordered table-hover" >\r\n		<thead>\r\n			<tr>\r\n				<th class="th-border">应收类别</th>\r\n				<th class="th-border">发生业务</th>\r\n				<th class="th-border">金额</th>\r\n				<th class="th-border">备注</th>\r\n				<th class="th-border">操作者</th>\r\n				<th class="th-border">操作时间</th>\r\n			</tr>\r\n		</thead>\r\n		<tbody>\r\n		{{each incomeDetailsList as detail}}\r\n			<tr>\r\n				<td>{{detail.receivableType}}</td>\r\n				<td>{{detail.businessType}}</td>\r\n				<td>{{detail.incomeDifferenceMoney}}</td>\r\n				<td>{{detail.remark}}</td>\r\n				<td>{{detail.checkRealname}}</td>\r\n				<td>{{detail.checkTime | dateFormat: \'yyyy-MM-dd\'}}</td>\r\n			</tr>\r\n		{{/each}}\r\n		</tbody>\r\n	</table>\r\n</div>\r\n\r\n'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+            };
+        }
+    });
+});
