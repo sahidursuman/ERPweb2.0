84c5590cd5dabd216fe6db1c6abe66b61dfc740f
熊祖瑶 <x@xiongzy.com>
2015-12-11 19:40:25 +0800

    修改购物、代订、票务账务。

--- js/template/financial/ticket/view/payDetails.js
+++ js/template/financial/ticket/view/payDetails.js
@@ -1,10 +1,10 @@
-/*TMODJS:{"debug":true,"version":3,"md5":"be5752fac870dd65d4cecfe53e024cb7"}*/
+/*TMODJS:{"debug":true,"version":4,"md5":"957be6133a6712825b6c48a457cb871d"}*/
 define(function(require) {
     return require("../../../template")("financial/ticket/view/payDetails", function($data, $filename) {
         try {
             var $utils = this, $line = ($utils.$helpers, 0), $each = $utils.$each, payedMoneyDetailList = $data.payedMoneyDetailList, $escape = ($data.item, 
             $data.$index, $utils.$escape), $out = "";
-            return $out += '<div class="row" style="margin: 0; padding: 10px 10px 0;"> <table class="table table-striped table-bordered table-hover"> <thead> <tr> <td>付款类别</td> <td>发生业务</td> <td>付款方</td> <td>金额</td> <td>付款方式</td> <td>备注</td> <td>操作人</td> <td>操作时间</td> </tr> </thead> <tbody> ', 
+            return $out += '<div class="row" style="margin: 0; padding: 10px 10px 0;"> <table class="table table-striped table-bordered table-hover"> <thead> <tr> <td class="th-border">付款类别</td> <td class="th-border">发生业务</td> <td class="th-border">付款方</td> <td class="th-border">金额</td> <td class="th-border">付款方式</td> <td class="th-border">备注</td> <td class="th-border">操作人</td> <td class="th-border">操作时间</td> </tr> </thead> <tbody> ', 
             $line = 16, $each(payedMoneyDetailList, function(item) {
                 $out += " <tr> <td>", $line = 18, $out += $escape(item.businessType), $out += "</td> <td>", 
                 $line = 19, $out += $escape(item.businessType), $out += "</td> <td>", $line = 20, 
@@ -20,7 +20,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '<div class="row" style="margin: 0; padding: 10px 10px 0;">\r\n	<table class="table table-striped table-bordered table-hover">\r\n        <thead>\r\n            <tr>\r\n                <td>付款类别</td>\r\n                <td>发生业务</td>\r\n                <td>付款方</td>\r\n                <td>金额</td>\r\n                <td>付款方式</td>\r\n                <td>备注</td>\r\n                <td>操作人</td>\r\n                <td>操作时间</td>\r\n            </tr>\r\n        </thead>\r\n        <tbody>\r\n            {{each payedMoneyDetailList as item}}\r\n            <tr>\r\n                <td>{{item.businessType}}</td>\r\n                <td>{{item.businessType}}</td>\r\n                <td>{{item.incomePart}}</td>\r\n                <td>{{item.incomeMoney}}</td>\r\n                <td>{{item.payType}}</td>\r\n                <td>{{item.remark}}</td>\r\n                <td>{{item.checkRealname}}</td>\r\n                <td>{{item.checkTime}}</td>\r\n            </tr>\r\n            {{/each}}\r\n        </tbody>\r\n    </table>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '<div class="row" style="margin: 0; padding: 10px 10px 0;">\r\n	<table class="table table-striped table-bordered table-hover">\r\n        <thead>\r\n            <tr>\r\n                <td class="th-border">付款类别</td>\r\n                <td class="th-border">发生业务</td>\r\n                <td class="th-border">付款方</td>\r\n                <td class="th-border">金额</td>\r\n                <td class="th-border">付款方式</td>\r\n                <td class="th-border">备注</td>\r\n                <td class="th-border">操作人</td>\r\n                <td class="th-border">操作时间</td>\r\n            </tr>\r\n        </thead>\r\n        <tbody>\r\n            {{each payedMoneyDetailList as item}}\r\n            <tr>\r\n                <td>{{item.businessType}}</td>\r\n                <td>{{item.businessType}}</td>\r\n                <td>{{item.incomePart}}</td>\r\n                <td>{{item.incomeMoney}}</td>\r\n                <td>{{item.payType}}</td>\r\n                <td>{{item.remark}}</td>\r\n                <td>{{item.checkRealname}}</td>\r\n                <td>{{item.checkTime}}</td>\r\n            </tr>\r\n            {{/each}}\r\n        </tbody>\r\n    </table>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
