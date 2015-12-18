daa069cff215dc0285b2bef968695c14ba74b53c
wenJoy <laojiaodaoniu@126.com>
2015-12-18 22:35:55 +0800

    修复外转账务点击已付金额链接页面显示错误

--- js/template/financial/transfer/view/viewPayedDetail.js
+++ js/template/financial/transfer/view/viewPayedDetail.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":8,"md5":"ab66b2051fd163b2a5091f3ca00570df"}*/
+/*TMODJS:{"debug":true,"version":17,"md5":"688820b128b6cf5f46fca19995e1d6a9"}*/
 define(function(require) {
     return require("../../../../../template")("js/template/financial/transfer/view/viewPayedDetail", function($data, $filename) {
         try {
@@ -13,13 +13,14 @@ define(function(require) {
                 $out += $escape(rs.businessType), $line = 26), $out += "</td> <td>", $line = 27, 
                 null == rs.payParty || "" == rs.payParty ? ($out += "-", $line = 27) : ($line = 27, 
                 $out += $escape(rs.payParty), $line = 27), $out += "</td> <td>", $line = 28, $out += $escape(rs.payDifferenceMoney), 
-                $out += "</td> <td>", $line = 29, $out += $escape($helpers.getPayTypeText(rs.payType)), 
-                $out += "</td> <td>", $line = 30, $out += $escape(rs.remark), $out += "</td> <td>", 
-                $line = 31, null == rs.checkRealname || "" == rs.checkRealname ? ($out += "-", $line = 31) : ($line = 31, 
-                $out += $escape(rs.checkRealname), $line = 31), $out += "</td> <td>", $line = 32, 
-                null == rs.checkTime || "" == rs.checkTime ? ($out += "-", $line = 32) : ($line = 32, 
-                $out += $escape($helpers.dateFormat(rs.checkTime, "yyyy-MM-dd hh:mm:ss")), $line = 32), 
-                $out += "</td> </tr> ", $line = 34;
+                $out += "</td> <td>", $line = 29, 0 == rs.payType ? ($out += " 现金 ", $line = 31) : 1 == rs.payType ? ($out += " 银行转账 ", 
+                $line = 33) : 2 == rs.payType ? ($out += " 网上支付 ", $line = 35) : 3 == rs.payType ? ($out += " 支票 ", 
+                $line = 37) : 4 == rs.payType && ($out += " 其他 ", $line = 39), $out += " </td> <td>", 
+                $line = 41, $out += $escape(rs.remark), $out += "</td> <td>", $line = 42, null == rs.checkRealname || "" == rs.checkRealname ? ($out += "-", 
+                $line = 42) : ($line = 42, $out += $escape(rs.checkRealname), $line = 42), $out += "</td> <td>", 
+                $line = 43, null == rs.checkTime || "" == rs.checkTime ? ($out += "-", $line = 43) : ($line = 43, 
+                $out += $escape($helpers.dateFormat(rs.checkTime, "yyyy-MM-dd hh:mm:ss")), $line = 43), 
+                $out += "</td> </tr> ", $line = 45;
             }), $out += " </tbody> </table> </div> </div> </div> </div> </div> </form> </div>", 
             new String($out);
         } catch (e) {
@@ -28,7 +29,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '<div class="col-xs-12">\r\n	<form class="form-horizontal" role="form" style="margin-top:10px" onsubmit="return false">\r\n		<div class="form-group">\r\n			<div class="col-xs-12 col-sm-12 widget-container-col">\r\n				<div class=" ui-sortable-handle">\r\n					<div class="widget-body">\r\n						<div class="widget-main">\r\n							<table class="table table-striped table-bordered table-hover">\r\n								<thead>\r\n									<tr>\r\n										<th class="th-border">付款类别</th>\r\n										<th class="th-border">发生业务</th>\r\n										<th class="th-border">付款方</th>\r\n										<th class="th-border">金额</th>\r\n										<th class="th-border">付款方式</th>\r\n										<th class="th-border">备注</th>\r\n										<th class="th-border">操作人</th>\r\n										<th class="th-border">操作时间</th>\r\n									</tr>\r\n								</thead>\r\n\r\n								<tbody>\r\n									{{each payedMoneyDetailList as rs}}\r\n									<tr data-id="{{rs.id}}">\r\n										<td>{{if rs.receivableType == null || rs.receivableType == ""}}-{{else}}{{rs.receivableType}}{{/if}}</td>\r\n										<td>{{if rs.businessType == null || rs.businessType == ""}}-{{else}}{{rs.businessType}}{{/if}}</td>\r\n										<td>{{if rs.payParty == null || rs.payParty == ""}}-{{else}}{{rs.payParty}}{{/if}}</td>\r\n										<td>{{rs.payDifferenceMoney}}</td>\r\n										<td>{{rs.payType | getPayTypeText}}</td>\r\n										<td>{{rs.remark}}</td>\r\n										<td>{{if rs.checkRealname == null || rs.checkRealname == ""}}-{{else}}{{rs.checkRealname}}{{/if}}</td>\r\n										<td>{{if rs.checkTime == null || rs.checkTime == ""}}-{{else}}{{rs.checkTime | dateFormat:\'yyyy-MM-dd hh:mm:ss\'}}{{/if}}</td>\r\n									</tr>\r\n									{{/each}}\r\n								</tbody>\r\n							</table>\r\n						</div>\r\n					</div>\r\n				</div>\r\n			</div>\r\n		</div>\r\n	</form>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '<div class="col-xs-12">\r\n	<form class="form-horizontal" role="form" style="margin-top:10px" onsubmit="return false">\r\n		<div class="form-group">\r\n			<div class="col-xs-12 col-sm-12 widget-container-col">\r\n				<div class=" ui-sortable-handle">\r\n					<div class="widget-body">\r\n						<div class="widget-main">\r\n							<table class="table table-striped table-bordered table-hover">\r\n								<thead>\r\n									<tr>\r\n										<th class="th-border">付款类别</th>\r\n										<th class="th-border">发生业务</th>\r\n										<th class="th-border">付款方</th>\r\n										<th class="th-border">金额</th>\r\n										<th class="th-border">付款方式</th>\r\n										<th class="th-border">备注</th>\r\n										<th class="th-border">操作人</th>\r\n										<th class="th-border">操作时间</th>\r\n									</tr>\r\n								</thead>\r\n\r\n								<tbody>\r\n									{{each payedMoneyDetailList as rs}}\r\n									<tr data-id="{{rs.id}}">\r\n										<td>{{if rs.receivableType == null || rs.receivableType == ""}}-{{else}}{{rs.receivableType}}{{/if}}</td>\r\n										<td>{{if rs.businessType == null || rs.businessType == ""}}-{{else}}{{rs.businessType}}{{/if}}</td>\r\n										<td>{{if rs.payParty == null || rs.payParty == ""}}-{{else}}{{rs.payParty}}{{/if}}</td>\r\n										<td>{{rs.payDifferenceMoney}}</td>\r\n										<td>{{if rs.payType == 0}}\r\n												现金\r\n											{{else if rs.payType == 1}}\r\n												银行转账\r\n											{{else if rs.payType == 2}}\r\n												网上支付\r\n											{{else if rs.payType == 3}}\r\n												支票\r\n											{{else if rs.payType == 4}}\r\n												其他\r\n											{{/if}}\r\n										</td>\r\n										<td>{{rs.remark}}</td>\r\n										<td>{{if rs.checkRealname == null || rs.checkRealname == ""}}-{{else}}{{rs.checkRealname}}{{/if}}</td>\r\n										<td>{{if rs.checkTime == null || rs.checkTime == ""}}-{{else}}{{rs.checkTime | dateFormat:\'yyyy-MM-dd hh:mm:ss\'}}{{/if}}</td>\r\n									</tr>\r\n									{{/each}}\r\n								</tbody>\r\n							</table>\r\n						</div>\r\n					</div>\r\n				</div>\r\n			</div>\r\n		</div>\r\n	</form>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
