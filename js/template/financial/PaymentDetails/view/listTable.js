5e01d6a2022255d3381d17cb50c74ed90e2cf151
guiyun <wei_guiyun@163.com>
2015-12-19 14:51:32 +0800

    T235:【收支明细】按需求调整页面内容

--- js/template/financial/PaymentDetails/view/listTable.js
+++ js/template/financial/PaymentDetails/view/listTable.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":7,"md5":"07cf624355b3ec8c075e02cb63bc8a08"}*/
+/*TMODJS:{"debug":true,"version":12,"md5":"2cdf57eed38340e3338dbb037912b97c"}*/
 define(function(require) {
     return require("../../../template")("financial/PaymentDetails/view/listTable", function($data, $filename) {
         try {
@@ -7,17 +7,16 @@ define(function(require) {
             return $line = 1, $each(result, function(rs) {
                 $out += " <tr> <td>", $line = 3, $out += $escape(rs.resourceName), $out += "</td> <td>", 
                 $line = 4, rs.receivableType && ($line = 4, $out += $escape(rs.receivableType.name), 
-                $line = 4), $out += "</td> <td>", $line = 5, rs.incomeOrPayType && ($line = 5, $out += $escape(rs.incomeOrPayType.name), 
-                $line = 5), $out += "</td> <td>", $line = 6, rs.costType && ($line = 6, $out += $escape(rs.costType.name), 
-                $line = 6), $out += "</td> <td>", $line = 7, rs.businessType && ($line = 7, $out += $escape(rs.businessType.name), 
-                $line = 7), $out += "</td> <td>", $line = 8, 0 == rs.payType ? ($out += "现金", $line = 8) : 1 == rs.payType ? ($out += "银行转账", 
-                $line = 8) : 2 == rs.payType ? ($out += "网上支付", $line = 8) : 3 == rs.payType ? ($out += "支票", 
-                $line = 8) : 4 == rs.payType && ($out += "其它", $line = 8), $out += "</td> <td>", 
-                $line = 9, 0 == rs.incomeDifferenceMoney ? ($out += "-", $line = 9) : ($line = 9, 
-                $out += $escape(rs.incomeDifferenceMoney), $line = 9), $out += "</td> <td>", $line = 10, 
-                0 == rs.payDifferenceMoney ? ($out += "-", $line = 10) : ($line = 10, $out += $escape(rs.payDifferenceMoney), 
-                $line = 10), $out += "</td> <td>", $line = 11, $out += $escape(rs.remark), $out += "</td> <td>", 
-                $line = 12, $out += $escape(rs.createTime), $out += "</td> </tr> ", $line = 14;
+                $line = 4), $out += "</td> <td>", $line = 5, rs.businessType && ($line = 5, $out += $escape(rs.businessType.name), 
+                $line = 5), $out += "</td> <td>", $line = 6, 0 == rs.payType ? ($out += "现金", $line = 6) : 1 == rs.payType ? ($out += "银行转账", 
+                $line = 6) : 2 == rs.payType ? ($out += "网上支付", $line = 6) : 3 == rs.payType ? ($out += "支票", 
+                $line = 6) : 4 == rs.payType && ($out += "其它", $line = 6), $out += "</td> ", $line = 7, 
+                rs.moneyType ? ($out += " <td>", $line = 8, $out += $escape(rs.incomeDifferenceMoney), 
+                $out += "</td> <td>-</td> ", $line = 10) : ($out += " <td>-</td> <td>", $line = 12, 
+                $out += $escape(rs.payDifferenceMoney), $out += "</td> ", $line = 13), $out += " <td>", 
+                $line = 14, $out += $escape(rs.remark), $out += "</td> <td>", $line = 15, $out += $escape(rs.user.realName), 
+                $out += "</td> <td>", $line = 16, $out += $escape(rs.createTime), $out += "</td> </tr> ", 
+                $line = 18;
             }), new String($out);
         } catch (e) {
             throw {
@@ -25,7 +24,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: "{{each result as rs}}\r\n    <tr>\r\n        <td>{{rs.resourceName}}</td>\r\n        <td>{{if rs.receivableType}}{{rs.receivableType.name}}{{/if}}</td>\r\n        <td>{{if rs.incomeOrPayType}}{{rs.incomeOrPayType.name}}{{/if}}</td>\r\n        <td>{{if rs.costType}}{{rs.costType.name}}{{/if}}</td>\r\n        <td>{{if rs.businessType}}{{rs.businessType.name}}{{/if}}</td>\r\n        <td>{{if rs.payType == 0}}现金{{else if rs.payType == 1}}银行转账{{else if rs.payType == 2}}网上支付{{else if rs.payType == 3}}支票{{else if rs.payType == 4}}其它{{/if}}</td>\r\n        <td>{{if rs.incomeDifferenceMoney == 0}}-{{else}}{{rs.incomeDifferenceMoney}}{{/if}}</td>\r\n        <td>{{if rs.payDifferenceMoney == 0}}-{{else}}{{rs.payDifferenceMoney}}{{/if}}</td>\r\n        <td>{{rs.remark}}</td>\r\n        <td>{{rs.createTime}}</td>\r\n    </tr>\r\n{{/each}}".split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: "{{each result as rs}}\r\n    <tr>\r\n        <td>{{rs.resourceName}}</td>\r\n        <td>{{if rs.receivableType}}{{rs.receivableType.name}}{{/if}}</td>\r\n        <td>{{if rs.businessType}}{{rs.businessType.name}}{{/if}}</td>\r\n        <td>{{if rs.payType == 0}}现金{{else if rs.payType == 1}}银行转账{{else if rs.payType == 2}}网上支付{{else if rs.payType == 3}}支票{{else if rs.payType == 4}}其它{{/if}}</td>\r\n        {{if !!rs.moneyType}}\r\n        <td>{{rs.incomeDifferenceMoney}}</td>\r\n        <td>-</td>\r\n        {{else}}\r\n        <td>-</td>\r\n        <td>{{rs.payDifferenceMoney}}</td>\r\n        {{/if}}\r\n        <td>{{rs.remark}}</td>\r\n        <td>{{rs.user.realName}}</td>\r\n        <td>{{rs.createTime}}</td>\r\n    </tr>\r\n{{/each}}".split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
