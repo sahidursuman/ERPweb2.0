a4f82befec2bd899974ac4f0ca6173c33aa41a6f
wenJoy <laojiaodaoniu@126.com>
2015-12-19 20:18:20 +0800

    修复bug#3648 【酒店账务】【餐厅账务】对账和付款界面->选中时间或财务类别搜索后，离开再次进入对账或付款界面，还是显示的刚刚的搜索结果页。再次进入应该显示全部；

--- js/template/financial/hotel/hotel.js
+++ js/template/financial/hotel/hotel.js
@@ -101,19 +101,11 @@ define(function(require, exports) {
                 name = $that.closest('tr').data('name');
             if ($that.hasClass('T-check')) {
                 // 对账
-                var $checkTab = $("#tab-" + menuKey + "-checking-content");
-                if($checkTab.length && $checkTab.find('.T-newData').data("id") == id){
-                    $('.tab-' + menuKey + '-checking').children('a').trigger('click');
-                    return false;
-                }
+               
                 hotel.hotelCheck(0,id,name,"",startDate,endDate);
             } else if ($that.hasClass('T-clear')) {
                 // 结算
-                var $clearTab = $("#tab-" + menuKey + "-clearing-content");
-                if($clearTab.length && $clearTab.find('.T-newData').data("id") == id){
-                    $('.tab-' + menuKey + '-clearing').children('a').trigger('click');
-                    return false;
-                }
+                
                 hotel.hotelClear(0,0,id,name,"",startDate,endDate);
             }
         });
