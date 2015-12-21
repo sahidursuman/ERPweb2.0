b01ffd0d7c4c44ab75f8a8af4b7943d8824f30da
熊祖瑶 <x@xiongzy.com>
2015-12-21 17:59:23 +0800

    收支明细最终优化完毕

--- js/template/financial/PaymentDetails/paymentDetails.js
+++ js/template/financial/PaymentDetails/paymentDetails.js
@@ -11,73 +11,56 @@ define(function(require, exports){
 
 	var Payment = {
 		$tab : false,
-		list : {},
-		total : {},
-		isList : false,
-		isTotal : false,
-		isSelect : false
+		total : {}
 	};
 	/**
 	 * 初始化模块
 	 */
 	Payment.initModule = function(){
 		Payment.$tab = null;
-		Payment.isList = Payment.isTotal = Payment.isSelect = false;
-		Payment.getList();
-	};
-	/**
-	 * 获取购物账务列表，初始化列表页面
-	 * @param  {int} page 页码
-	 */
-	Payment.getList = function(page){
-		Payment.isList = Payment.isTotal = false;
 		var date = new Date(),
         year = date.getFullYear(),
         month = Tools.addZero2Two(date.getMonth() + 1),
         day = Tools.addZero2Two(date.getDate()),
 		args = {
-			pageNo : (page || 0),
+			pageNo : 0,
 			endTime : year + "-" + month + "-" + day,
 			startTime : year + "-" + month + "-01"
+		},
+		data = {
+			searchParam : args
+		};
+		Tools.addTab(menuKey, "收支明细", listTemplate(data));
+		Payment.$tab = $('#tab-' + menuKey + '-content');
+		Payment.getTotal(args);
+		Payment.ajaxInit(args);
+		Payment.initSearch(args);
+	};
+	/**
+	 * 获取收/付款合计
+	 * @param  {object/number} args 参数
+	 * @param  {object} $tab $tab
+	 */
+	Payment.getTotal = function(args, $tab){
+		var page = typeof(args) !== "number" ? (args || 0) : (args.pageNo || 0);
+		if(!!Payment.$tab){
+			args = Payment.getArgs(page);
 		}
-		if (!!Payment.$tab) {
-			var name = Payment.$tab.find('.T-search-unit').val();
-			args = {
-				pageNo: (page || 0),
-				businessTypeId : Payment.$tab.find('.T-search-business').val(),
-				moneyType : Payment.$tab.find('#fin-type').val(),
-				receivableTypeId : Payment.$tab.find('.T-search-category').val(),
-				resourceId : Payment.$tab.find('.T-search-unit').data('id'),
-				resourceName : name == "全部" ? "" : name,
-				endTime : Payment.$tab.find('.T-search-end-time').val(),
-				startTime : Payment.$tab.find('.T-search-start-time').val(),
-				payType : Payment.$tab.find('.T-search-payment').val()
-			}
-		}
-		Payment.globalLoadingLayer = openLoadingLayer();
 		$.ajax({
 			url : KingServices.build_url('financialIncomeOrPay', 'findTotal'),
 			type : "POST",
 			data : {searchParam : JSON.stringify(args)},
 			showLoading: false
 		}).done(function(data){
+			Payment.$tab.find('.T-incomeMoney').html(data.total.incomeMoney);
+			Payment.$tab.find('.T-payMoney').html(data.total.payMoney);
 			Payment.total = data.total;
-			data.searchParam = args;
-			Payment.isTotal = true;
-			Payment.closeLayer();
-			if(!Payment.$tab || !Payment.$tab.data('ajax')){
-				Tools.addTab(menuKey, "收支明细", listTemplate(data));
-			}else{
-				Payment.$tab.find('.T-incomeMoney').html(data.total.incomeMoney);
-				Payment.$tab.find('.T-payMoney').html(data.total.payMoney);
-			}
-			Payment.$tab = $('#tab-' + menuKey + '-content');
-			Payment.$tab.data('ajax', 'true');
-			Payment.ajaxInit(0, args, Payment.$tab);
 		});
-		Payment.initSearch(args);
 	};
-
+	/**
+	 * 初始化事件
+	 * @param  {object} $tab $tab
+	 */
 	Payment.init_event = function($tab){
 		var $searchArea = $tab.find('.T-search-area'),
 			$datepicker = $searchArea.find('.datepicker');
@@ -85,37 +68,42 @@ define(function(require, exports){
 
 		$searchArea.find('.T-btn-search').on('click', function(event){
 			event.preventDefault();
-			Payment.getList(0);
+			Payment.getTotal(0);
+			Payment.ajaxInit(0);
 		})
 		
 	};
-
+	/**
+	 * 初始化搜索栏
+	 * @param  {object} args 初始化时填充参数
+	 */
 	Payment.initSearch = function(args){
-		if(!Payment.$tab || !Payment.$tab.data('ajax')){
-			$.ajax({
-				url : KingServices.build_url('financialIncomeOrPay', 'findSelectValue'),
-				type : "POST",
-				showLoading: false
-			}).done(function(data){
-				data.businessTypes = JSON.parse(data.businessTypes);
-				data.costTypes = JSON.parse(data.costTypes);
-				data.incomeOrPayTypes = JSON.parse(data.incomeOrPayTypes);
-				data.receivableTypes = JSON.parse(data.receivableTypes);
-				data.searchParam = args;
-
-				Payment.isSelect = true;
-
-				data.total = Payment.total;
-				data.payTypeList = ['现金', '银行转账', '网上支付', '支票', '其它'];
-				
-				var html = listSearchTemplate(data);
-				Payment.$tab.find('.T-search-area').html(html);
-				Payment.init_event(Payment.$tab);
-			});
-		}
+		$.ajax({
+			url : KingServices.build_url('financialIncomeOrPay', 'findSelectValue'),
+			type : "POST",
+			showLoading: false
+		}).done(function(data){
+			data.businessTypes = JSON.parse(data.businessTypes);
+			data.costTypes = JSON.parse(data.costTypes);
+			data.incomeOrPayTypes = JSON.parse(data.incomeOrPayTypes);
+			data.receivableTypes = JSON.parse(data.receivableTypes);
+			data.total = Payment.total;
+			data.searchParam = args;
+			data.payTypeList = ['现金', '银行转账', '网上支付', '支票', '其它'];
+			var html = listSearchTemplate(data);
+			Payment.$tab.find('.T-search-area').html(html);
+			Payment.init_event(Payment.$tab);
+		});
 	};
-	Payment.ajaxInit = function(page, args, $tab){
-		args.pageNo = page || 0;
+	/**
+	 * 初始化列表
+	 * @param  {object/number} args 请求参数
+	 */
+	Payment.ajaxInit = function(args){
+		var page = typeof(args) === "number" ? (args || 0) : (args.pageNo || 0);
+		if(!!Payment.$tab){
+			args = Payment.getArgs(page);
+		}
 		$.ajax({
 			url : KingServices.build_url('financialIncomeOrPay', 'findPager'),
 			type : "POST",
@@ -126,31 +114,41 @@ define(function(require, exports){
 			if(showDialog(data)){
 				data.result = JSON.parse(data.result);
 				var html = listTableTemplate(data);
-				$tab.find('.T-list').html(html);
+				Payment.$tab.find('.T-list').html(html);
 				// 设置记录条数及页面
-                $tab.find('.T-sumItem').text('共计' + data.searchParam.totalCount + '条记录');
-                Payment.isList = true;
-                Payment.closeLayer();
+                Payment.$tab.find('.T-sumItem').text('共计' + data.searchParam.totalCount + '条记录');
 				// 绑定翻页组件
 				laypage({
-				    cont: $tab.find('.T-pagenation'), 
+				    cont: Payment.$tab.find('.T-pagenation'), 
 				    pages: data.searchParam.totalPage, //总页数
 				    curr: (data.searchParam.pageNo + 1),
 				    jump: function(obj, first) {
 				    	if (!first) {  // 避免死循环，第一次进入，不调用页面方法
-				    		Payment.ajaxInit(obj.curr -1, args, $tab);
+				    		Payment.ajaxInit(obj.curr -1);
 				    	}
 				    }
 				});	
 			}
 		});
 	};
-
-	Payment.closeLayer = function(){
-        if(Payment.isList && Payment.isTotal)
-        {
-			layer.close(Payment.globalLoadingLayer);
-        }
+	/**
+	 * 获取请求参数
+	 * @param  {number} page 当前页
+	 */
+	Payment.getArgs = function(page){
+		var name = Payment.$tab.find('.T-search-unit').val();
+		args = {
+			pageNo: (page || 0),
+			businessTypeId : Payment.$tab.find('.T-search-business').val(),
+			moneyType : Payment.$tab.find('#fin-type').val(),
+			receivableTypeId : Payment.$tab.find('.T-search-category').val(),
+			resourceId : Payment.$tab.find('.T-search-unit').data('id'),
+			resourceName : name == "全部" ? "" : name,
+			endTime : Payment.$tab.find('.T-search-end-time').val(),
+			startTime : Payment.$tab.find('.T-search-start-time').val(),
+			payType : Payment.$tab.find('.T-search-payment').val()
+		}
+		return args;
 	}
 	// 暴露方法
 	exports.init = Payment.initModule;
