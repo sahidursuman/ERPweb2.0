7f26573db9738514c030a89a06581cd79a0ce8ca
yangcanyi <289478310@qq.com>
2015-12-24 15:07:35 +0800

    BUG #3942 【发团安排】安排->车辆和住宿，已过期的资源保留期空着，不需显示等待确认

--- js/template/resource/tripPlan/view/busInquiryResult.js
+++ js/template/resource/tripPlan/view/busInquiryResult.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":44,"md5":"2f06456557668497b84e6e1a4a0bd2cc"}*/
+/*TMODJS:{"debug":true,"version":47,"md5":"595c1ce4790fc033d4d3de9a9bb71db0"}*/
 define(function(require) {
     return require("../../../template")("resource/tripPlan/view/busInquiryResult", function($data, $filename) {
         try {
@@ -11,10 +11,10 @@ define(function(require) {
                 $line = 22, $out += $escape(rs.queryJson), $out += "</td> ", $line = 23, $each(rs.busOffers, function(offer) {
                     $out += " <td>", $line = 24, $out += $escape(offer.companyName), $out += '</td> <td class="T-price">', 
                     $line = 25, $out += $escape(offer.price), $out += "</td> <td>", $line = 26, $out += $escape(offer.remark), 
-                    $out += "</td> <td>", $line = 27, "已过期" != offer.status && ($line = 27, $out += $escape(offer.reserveMinutes), 
-                    $line = 27), $out += "</td> <td>", $line = 28, $out += $escape($helpers.dateFormat(offer.expiryTime, "yyyy-MM-dd")), 
-                    $out += '</td> <td class="T-status">', $line = 29, $out += $escape(offer.status), 
-                    $out += "</td> ", $line = 30;
+                    $out += "</td> <td>", $line = 27, "已过期" != offer.status && "已拒绝" != offer.status && ($line = 27, 
+                    $out += $escape(offer.reserveMinutes), $line = 27), $out += "</td> <td>", $line = 28, 
+                    $out += $escape($helpers.dateFormat(offer.expiryTime, "yyyy-MM-dd")), $out += '</td> <td class="T-status">', 
+                    $line = 29, $out += $escape(offer.status), $out += "</td> ", $line = 30;
                 }), $out += " </tr> ", $line = 32;
             }), $out += " </tbody> </table> </div>", new String($out);
         } catch (e) {
@@ -23,7 +23,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '<div class="col-xs-12" style="padding: 20px 0 0 0">\r\n	<table class="table table-striped table-bordered table-hover">\r\n		<thead>\r\n			<tr>  \r\n				<th class="th-border">线路产品</th>\r\n				<th class="th-border">出游日期</th>\r\n				<th class="th-border">需求内容</th>               \r\n				<th class="th-border">车队</th>\r\n				<th class="th-border">车费报价</th>\r\n				<th class="th-border">反馈内容</th>\r\n				<th class="th-border">资源保留期</th>\r\n				<th class="th-border">询价截止期</th>\r\n				<th class="th-border">询价状态</th>\r\n			</tr>\r\n		</thead>\r\n	\r\n		<tbody>\r\n			{{each mapList as rs}}\r\n				<tr>\r\n					<td>{{rs.name}}</td>\r\n					<td>{{rs.startTime | dateFormat: \'yyyy-MM-dd\'}}</td>\r\n					<td>{{rs.queryJson}}</td>\r\n					{{each rs.busOffers as offer}}\r\n							<td>{{offer.companyName}}</td>\r\n							<td class="T-price">{{offer.price}}</td>\r\n							<td>{{offer.remark}}</td>\r\n							<td>{{if offer.status != \'已过期\'}}{{offer.reserveMinutes}}{{/if}}</td>\r\n							<td>{{offer.expiryTime | dateFormat: \'yyyy-MM-dd\'}}</td>\r\n							<td class="T-status">{{offer.status}}</td>\r\n					{{/each}}\r\n				</tr>\r\n			{{/each}}\r\n		</tbody>\r\n	</table>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '<div class="col-xs-12" style="padding: 20px 0 0 0">\r\n	<table class="table table-striped table-bordered table-hover">\r\n		<thead>\r\n			<tr>  \r\n				<th class="th-border">线路产品</th>\r\n				<th class="th-border">出游日期</th>\r\n				<th class="th-border">需求内容</th>               \r\n				<th class="th-border">车队</th>\r\n				<th class="th-border">车费报价</th>\r\n				<th class="th-border">反馈内容</th>\r\n				<th class="th-border">资源保留期</th>\r\n				<th class="th-border">询价截止期</th>\r\n				<th class="th-border">询价状态</th>\r\n			</tr>\r\n		</thead>\r\n	\r\n		<tbody>\r\n			{{each mapList as rs}}\r\n				<tr>\r\n					<td>{{rs.name}}</td>\r\n					<td>{{rs.startTime | dateFormat: \'yyyy-MM-dd\'}}</td>\r\n					<td>{{rs.queryJson}}</td>\r\n					{{each rs.busOffers as offer}}\r\n							<td>{{offer.companyName}}</td>\r\n							<td class="T-price">{{offer.price}}</td>\r\n							<td>{{offer.remark}}</td>\r\n							<td>{{if offer.status != \'已过期\' && offer.status != \'已拒绝\'}}{{offer.reserveMinutes}}{{/if}}</td>\r\n							<td>{{offer.expiryTime | dateFormat: \'yyyy-MM-dd\'}}</td>\r\n							<td class="T-status">{{offer.status}}</td>\r\n					{{/each}}\r\n				</tr>\r\n			{{/each}}\r\n		</tbody>\r\n	</table>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
