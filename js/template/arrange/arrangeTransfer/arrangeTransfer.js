07715c7bb0f85b94020f784e1f38190e1318fc70
zhangjiangfeng <2826758674@qq.com>
2015-12-28 19:55:22 +0800

    付款方式--字段去除

--- js/template/arrange/arrangeTransfer/arrangeTransfer.js
+++ js/template/arrange/arrangeTransfer/arrangeTransfer.js
@@ -797,8 +797,7 @@ define(function(require, exports) {
 					"id":id,  //转客表ID
 					"remark":remark, //转客备注
 					"status":status,//转客状态，0未完成，1已完成
-					"partnerAgencyId":partnerAgencyId, //转到的地接社
-					"payType":payType				
+					"partnerAgencyId":partnerAgencyId
 				},
 			    transferFee : {
 					"transNeedPayAllMoney":transNeedPayAllMoney,//应付
