82b9e317d4f2b5388fa58ac249a44d04ee067917
wenJoy <laojiaodaoniu@126.com>
2015-12-30 14:30:32 +0800

    修复内转转入--银行账户事件

--- js/template/financial/innerTransferIn/innerTransferIn.js
+++ js/template/financial/innerTransferIn/innerTransferIn.js
@@ -317,6 +317,7 @@ define(function(require,exports) {
 				//自动计算本次收款金额
 				InnerTransferIn.autoSumIncomeMoney($obj);
 			});
+			FinancialService.initPayEvent($obj.find('.T-summary'));
 		}else{
 			$obj.find('.T-checkList').off('change').on('change','input',function(){
 				$(this).closest('tr').data('change',true);
