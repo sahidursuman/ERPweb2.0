6304d297d0e2a5580f152d59e27540baef5f3b96
xiongzuyao <x@xiongzy.com>
2016-01-08 17:24:40 +0800

    删除多余代码并且监听TAB切换功能

--- js/template/arrange/tripPlan/rule.js
+++ js/template/arrange/tripPlan/rule.js
@@ -103,7 +103,7 @@ define(function(require, exports) {
                 $ele: $obj.find('input[name="preIncomeMoney"]'),
                 rules : [
                     {
-                        type: 'nonnegative-float',
+                        type: 'float',
                         errMsg: '预收款必须为数字'
                     }
                 ]
@@ -113,7 +113,7 @@ define(function(require, exports) {
                 $ele: $obj.find('input[name="currentNeedPayMoney"]'),
                 rules : [
                     {
-                        type: 'nonnegative-float',
+                        type: 'float',
                         errMsg: '计划现收必须为数字'
                     }
                 ]
