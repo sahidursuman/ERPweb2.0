fc6cc50b0ecc1085ff1a826ba8c947ab0ac31f7e
zhangjiangfeng <2826758674@qq.com>
2016-01-08 20:20:20 +0800

    资源管理模块     景区、商家、自费、保险模块千分位添加 规则：金额 F-float,F-money， 数量：F-float,F-count

--- js/template/resource/scenic/scenic.js
+++ js/template/resource/scenic/scenic.js
@@ -179,7 +179,7 @@ define(function(require,exports){
 			    		var html = "<tr>" +
 			    				"<td><input name=\"name\" class='col-sm-12' type=\"text\" maxlength=\"100\"/></td>" +
 			    				"<td class=\"time\"><div class=\"clearfix\" style=\"margin-top:1px\">日常价格<label class=\"timeArea\" style=\"float:right;padding-top:3px;\"><button class=\"btn btn-success btn-sm btn-white T-add addScenice\"><i class=\"ace-icon fa fa-plus bigger-110 icon-only\"></i></button></label></div></td>" +
-			    				"<td><div class=\"clearfix\" style=\"margin-top:0px\"><input name=\"normalInnerPrice\" style='margin-top: 2px' class='col-sm-12' type=\"text\" maxlength=\"7\"/></div></td>" +
+			    				"<td><div class=\"clearfix\" style=\"margin-top:0px\"><input name=\"normalInnerPrice\" style='margin-top: 2px' class='col-sm-12  F-float F-money ' type=\"text\" maxlength=\"7\"/></div></td>" +
 			    				"<td><input name=\"remark\" class='col-sm-12' type=\"text\" maxlength=\"1000\"/></td>" +
 			    				"<td style=\"width:70px\"><a data-entity-id=\"\" class=\"T-scenic-standard-delete\">删除</a></td>" +
 			    				"</tr>";
@@ -311,7 +311,7 @@ define(function(require,exports){
 						    		var html = "<tr>" +
 						    				"<td><input name=\"name\" class='col-sm-12' type=\"text\" maxlength=\"100\"/></td>" +
 						    				"<td class=\"time\"><div class=\"clearfix\" style=\"margin-top:1px\">日常价格<label class=\"timeArea\" style=\"float:right\"><button class=\"btn btn-success btn-sm btn-white T-add\"><i class=\"ace-icon fa fa-plus bigger-110 icon-only\"></i></button></label></div></td>" +
-						    				"<td><div class=\"clearfix\" style=\"margin-top:2px\"><input name=\"normalInnerPrice\" class='col-sm-12' type=\"text\" maxlength=\"7\"/></div></td>" +
+						    				"<td><div class=\"clearfix\" style=\"margin-top:2px\"><input name=\"normalInnerPrice\" class='col-sm-12 F-float F-money' type=\"text\" maxlength=\"7\"/></div></td>" +
 						    				"<td><input name=\"remark\"  class='col-sm-12' type=\"text\" maxlength=\"1000\"/></td>" +
 						    				"<td style=\"width:70px\"><a data-entity-id=\"\" class=\"T-scenic-standard-delete\">删除</a></td>" +
 						    				"</tr>";
