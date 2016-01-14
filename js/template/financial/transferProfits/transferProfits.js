4e3eb9b0b287072ebc9195ad9902ecf86c77d60a
liaojialing <609095033@qq.com>
2016-01-14 16:14:24 +0800

    中转利润

---
+++ js/template/financial/transferProfits/transferProfits.js
@@ -0,0 +1,259 @@
+define(function(require, exports) {
+	var menuKey = "financial_transferProfits",
+        listMainTemplate = require("./view/listMain"),
+        listTemplate = require("./view/list"),
+        touristsTemplate = require("./view/viewTouristGroup");
+
+    var transfer = {
+    	searchData : false,
+        $searchArea: false,
+        lineProductList : false,
+        partnerAgencyList : false
+	};
+
+    transfer.initModule = function(isTurn) {
+        var dateJson = FinancialService.getInitDate();
+        transfer.searchData = {
+            pageNo : 0,
+            startTime : dateJson.startDate,
+            endTime : dateJson.endDate,
+        };
+        var data = {};
+        data.searchParam = transfer.searchData;
+        var html = listMainTemplate(data);
+        Tools.addTab(menuKey,"中转利润",html);
+        
+        transfer.listMain("","","","","","",dateJson.startDate,dateJson.endDate);
+    };
+
+    transfer.listMain = function(lineProductName,lineProductId,fromPartnerAgencyName,fromPartnerAgencyId,customerType,orderNumber,startTime,endTime){
+        transfer.searchData = {
+            pageNo : 0,
+            lineProductName : lineProductName,
+            lineProductId : lineProductId,
+            fromPartnerAgencyName : fromPartnerAgencyName,
+            fromPartnerAgencyId : fromPartnerAgencyId,
+            customerType : customerType,
+            orderNumber : orderNumber,
+            startTime : startTime,
+            endTime : endTime,
+            sortType: 'auto'
+        };
+        var searchParam = JSON.stringify(transfer.searchData);
+
+        $.ajax({
+            url: KingServices.build_url('profitOutRemark', 'listProfitOutSelect'),
+            type: 'POST',
+            success: function(data) {
+                var result = showDialog(data);
+                if(result){
+                    transfer.lineProductList = data.lineProducts;
+                    transfer.partnerAgencyList = data.partnerAgencys;
+                    for(var i = 0; i < transfer.lineProductList.length; i++){
+                        transfer.lineProductList[i].value = transfer.lineProductList[i].name;
+                    }
+                    for(var i = 0; i < transfer.partnerAgencyList.length; i++){
+                        transfer.partnerAgencyList[i].value = transfer.partnerAgencyList[i].travelAgencyName;
+                    }
+                    transfer.listTransfer(0);
+                    //搜索事件
+                    var  $tab = $("#tab-" + menuKey + "-content");
+                    $tab.find(".T-search").off().on('click', function(event) {
+                        event.preventDefault();
+                        transfer.listTransfer(0);
+                    });
+                }
+            }
+        });
+    };
+
+    transfer.listTransfer = function(pageNo,lineProductName,lineProductId,fromPartnerAgencyName,fromPartnerAgencyId,customerType,orderNumber,startTime,endTime){
+        if (transfer.$searchArea && arguments.length === 1) {
+            // 初始化页面后，可以获取页面的参数
+            lineProductName = transfer.$searchArea.find("input[name=lineProductName]").val(),
+            lineProductId = transfer.$searchArea.find("input[name=lineProductId]").val(),
+            fromPartnerAgencyName = transfer.$searchArea.find("input[name=fromPartnerAgencyName]").val(),
+            fromPartnerAgencyId = transfer.$searchArea.find("input[name=fromPartnerAgencyId]").val(),
+            customerType = transfer.$searchArea.find("select[name=customerType]").val(),
+            orderNumber = transfer.$searchArea.find("input[name=orderNumber]").val(),
+            startTime = transfer.$searchArea.find("input[name=startTime]").val(),
+            endTime = transfer.$searchArea.find("input[name=endTime]").val(),
+            billStatus = transfer.$searchArea.find(".T-status button").data("value")
+        }
+        if(startTime > endTime){
+            showMessageDialog($("#confirm-dialog-message"),"开始时间不能大于结束时间，请重新选择！");
+            return false;
+        }
+        lineProductName = (lineProductName == "全部") ? "" : lineProductName;
+        fromPartnerAgencyName = (fromPartnerAgencyName == "全部") ? "" : fromPartnerAgencyName;
+
+        // 修正页码
+        pageNo = pageNo || 0;
+        transfer.searchData = {
+            pageNo : pageNo,
+            lineProductName : lineProductName,
+            lineProductId : lineProductId,
+            fromPartnerAgencyName : fromPartnerAgencyName,
+            fromPartnerAgencyId : fromPartnerAgencyId,
+            customerType : customerType,
+            orderNumber : orderNumber,
+            startTime : startTime,
+            endTime : endTime,
+            sortType: 'auto'
+        };
+
+        var searchParam = JSON.stringify(transfer.searchData);
+        $.ajax({
+            url:KingServices.build_url("profitOutRemark","listProfitOut"),
+            type: "POST",
+            data: { searchParam : searchParam},
+            success: function(data) {
+                var result = showDialog(data);
+                if (result) {
+                    data.results = JSON.parse(data.results);
+                    console.log(data.results);
+                    var html = listTemplate(data);
+
+                    $("#tab-" + menuKey + "-content").find(".T-transfer-list").html(html);
+                    var $tab = $("#tab-" + menuKey + "-content");
+                    $tab.find(".T-totalSize").text("共计 " + data.totalCount + " 条记录");
+                    transfer.$searchArea = $tab.find('.T-search-area');
+
+                    transfer.getSumData($tab,transfer.searchData);
+                    transfer.getQuery($tab);
+
+                    //报表内的操作
+                    $tab.find(".T-transfer-list").off().on("click",".T-option",function(){
+                        var $this = $(this),
+                            $tr = $this.closest('tr'),
+                            id = $tr.data("id"),
+                            lid = $tr.data("lid");
+                        if($this.hasClass('T-viewLineproduct')){
+                            KingServices.viewLineProduct(lid);
+                        } else if($this.hasClass('T-viewGroup')){
+                            transfer.viewGroup(id);
+                        } else if($this.hasClass('T-costDetail')){
+                            transfer.viewCostDetail(id);
+                        }
+                    });
+
+                    //绑定翻页组件
+                    laypage({
+                        cont: $tab.find('.T-pagenation'),
+                        pages: data.totalPage,
+                        curr: (pageNo + 1),
+                        jump: function(obj, first) {
+                            if (!first) { 
+                                transfer.listTransfer(obj.curr - 1);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    };
+
+    //查看游客小组
+    transfer.viewGroup = function(id){
+        $.ajax({
+            url: KingServices.build_url('financial/customerAccount', 'findTouristGroupDetail'),
+            type: 'post',
+            data: {touristGroupId: id},
+        })
+        .done(function(data) {
+            if (showDialog(data)) {
+                data.memberList = JSON.parse(data.memberList || false) || [];
+                var html = touristsTemplate(data);
+                var addGuide = layer.open({
+                    type: 1,
+                    title:"查看小组",
+                    skin: 'layui-layer-rim', //加上边框
+                    area: '850px', //宽高
+                    zIndex:1028,
+                    content: html,
+                    scrollbar: false
+                });
+            }
+        });
+    };
+
+    //查看中转明细
+    transfer.viewCostDetail = function(id){
+        $.ajax({
+            url: KingServices.build_url('profitOutRemark', 'outRemarkFinancialDetail'),
+            type: 'post',
+            data: {id: id},
+        })
+        .done(function(data) {
+            if (showDialog(data)) {
+                data.memberList = JSON.parse(data.memberList || false) || [];
+                var html = touristsTemplate(data);
+                var addGuide = layer.open({
+                    type: 1,
+                    title:"中转成本明细",
+                    skin: 'layui-layer-rim', //加上边框
+                    area: '1050px', //宽高
+                    zIndex:1028,
+                    content: html,
+                    scrollbar: false
+                });
+            }
+        });
+    };
+
+    transfer.getSumData = function($tab,args){
+        $.ajax({
+            url: KingServices.build_url("profitOutRemark","listProfitOutCount"),
+            type: 'POST',
+            data: {searchParam : JSON.stringify(args)},
+        })
+        .done(function(data) {
+            if(showDialog(data)){
+                $tab.find(".T-totalCount").text(data.countMap.adultCount + " 大" + data.countMap.childCount + " 小");
+                $tab.find(".T-totalNeed").text(data.countMap.needIncomeMoney);
+                $tab.find(".T-totalCost").text(data.countMap.totalPayMoney);
+                $tab.find(".T-totalProfit").text(data.countMap.grossProfitMoney);
+                $tab.find(".T-perProfit").text(data.countMap.perGrossProfitMoney);
+            }
+        });
+    };
+
+    transfer.getQuery = function($tab){
+        var $lineProduct = $tab.find("input[name=lineProductName]"),
+            $partner = $tab.find("input[name=fromPartnerAgencyName]");
+
+        //线路产品
+        $lineProduct.autocomplete({
+            minLength: 0,
+            source : transfer.lineProductList,
+            change: function(event, ui) {
+                if (!ui.item)  {
+                    $(this).nextAll('input[name=lineProductId]').val('');
+                }
+            },
+            select: function(event, ui) {
+                $(this).blur().nextAll('input[name=lineProductId]').val(ui.item.id);
+            }
+        }).on("click",function(){
+            $lineProduct.autocomplete('search', '');
+        });  
+
+        //来源
+        $partner.autocomplete({
+            minLength: 0,
+            source : transfer.partnerAgencyList,
+            change: function(event, ui) {
+                if (!ui.item)  {
+                    $(this).nextAll('input[name=fromPartnerAgencyId]').val('');
+                }
+            },
+            select: function(event, ui) {
+                $(this).blur().nextAll('input[name=fromPartnerAgencyId]').val(ui.item.id);
+            }
+        }).on("click",function(){
+            $partner.autocomplete('search', '');
+        });
+    };
+
+    exports.init = transfer.initModule;
+});
