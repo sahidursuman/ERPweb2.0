41fb9e698c0ac9cf7666df4917223cceaf360874
liaojialing <609095033@qq.com>
2016-01-20 21:22:25 +0800

    b#5431:中转成本明细记录操作时间缺少时分秒

--- js/template/financial/transferProfits/view/viewCostDetail.js
+++ js/template/financial/transferProfits/view/viewCostDetail.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":23,"md5":"00556e3d3be3ac0a38456c356e3ec643"}*/
+/*TMODJS:{"debug":true,"version":25,"md5":"fd5572540f894a13da0c914b2c8e88e6"}*/
 define(function(require) {
     return require("../../../template")("financial/transferProfits/view/viewCostDetail", function($data, $filename) {
         try {
@@ -13,7 +13,7 @@ define(function(require) {
                 $out += '</td> <td><span class="F-float F-money">', $line = 18, $out += $escape(rs.payDifferenceMoney), 
                 $out += "</span></td> <td>", $line = 19, $out += $escape(rs.remark), $out += "</td> <td>", 
                 $line = 20, null == creator || "" == rs.creator ? ($out += "-", $line = 20) : ($line = 20, 
-                $out += $escape(rs.creator), $line = 20), $out += "</td> <td>", $line = 21, $out += $escape($helpers.dateFormat(rs.createTime, "yyyy-MM-dd")), 
+                $out += $escape(rs.creator), $line = 20), $out += "</td> <td>", $line = 21, $out += $escape($helpers.dateFormat(rs.createTime, "yyyy-MM-dd hh:mm:ss")), 
                 $out += "</td> </tr> ", $line = 23;
             }), $out += " </tbody> </table> </div>", new String($out);
         } catch (e) {
@@ -22,7 +22,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '<div class="row" style="margin: 0; padding: 10px 10px 0;">\r\n	<table class="table table-striped table-bordered table-hover">\r\n        <thead>\r\n            <tr>\r\n                <th>应付类别</th>\r\n                <th>发生业务</th>\r\n                <th>金额</th>\r\n                <th>备注</th>\r\n                <th>操作人</th>\r\n                <th>操作时间</th>\r\n            </tr>\r\n        </thead>\r\n        <tbody>\r\n            {{each financialNeedPays as rs}}\r\n			<tr>\r\n			    <td>{{if rs.receivableType == null || !rs.receivableType}}-{{else}}{{rs.receivableType.name}}{{/if}}</td>\r\n			    <td>{{if rs.businessType == null || !rs.businessType}}-{{else}}{{rs.businessType.name}}{{/if}}</td>\r\n			    <td><span class="F-float F-money">{{rs.payDifferenceMoney}}</span></td>\r\n			    <td>{{rs.remark}}</td>\r\n			    <td>{{if creator == null || rs.creator == ""}}-{{else}}{{rs.creator}}{{/if}}</td>\r\n			    <td>{{rs.createTime | dateFormat:\'yyyy-MM-dd\'}}</td>\r\n			</tr>\r\n			{{/each}}\r\n        </tbody>\r\n    </table>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '<div class="row" style="margin: 0; padding: 10px 10px 0;">\r\n	<table class="table table-striped table-bordered table-hover">\r\n        <thead>\r\n            <tr>\r\n                <th>应付类别</th>\r\n                <th>发生业务</th>\r\n                <th>金额</th>\r\n                <th>备注</th>\r\n                <th>操作人</th>\r\n                <th>操作时间</th>\r\n            </tr>\r\n        </thead>\r\n        <tbody>\r\n            {{each financialNeedPays as rs}}\r\n			<tr>\r\n			    <td>{{if rs.receivableType == null || !rs.receivableType}}-{{else}}{{rs.receivableType.name}}{{/if}}</td>\r\n			    <td>{{if rs.businessType == null || !rs.businessType}}-{{else}}{{rs.businessType.name}}{{/if}}</td>\r\n			    <td><span class="F-float F-money">{{rs.payDifferenceMoney}}</span></td>\r\n			    <td>{{rs.remark}}</td>\r\n			    <td>{{if creator == null || rs.creator == ""}}-{{else}}{{rs.creator}}{{/if}}</td>\r\n			    <td>{{rs.createTime | dateFormat:\'yyyy-MM-dd hh:mm:ss\'}}</td>\r\n			</tr>\r\n			{{/each}}\r\n        </tbody>\r\n    </table>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
