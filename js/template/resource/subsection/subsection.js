a395ca1b937b049540fe038a75cd8221d782f51a
zhangjiangfeng <2826758674@qq.com>
2016-02-01 19:07:24 +0800

    中转分段线路修改

--- js/template/resource/subsection/subsection.js
+++ js/template/resource/subsection/subsection.js
@@ -332,7 +332,7 @@ define(function(require, exports) {
 	    	/* Act on the event */
 	    	var $that=$(this),$parents = $that.closest("tr"), id = $parents.data("entity-id");
 	    	if ($that.hasClass('T-searchLine')) {//搜搜线路
-	    		subsection.getLineProductList(0, '');
+	    		subsection.getLayerProductList(0, '');
 	    	}else if($that.hasClass('T-btn-operation-delete')){//删除
 				subsection.deleteOperation(id,$that);
 				subsection.$tabSub.data('isEdited', true);
@@ -384,7 +384,7 @@ define(function(require, exports) {
 			}
 			var html = ''
 			+ '<tr data-entity-id="">'
-			+ '<td><div class="hct-input-group col-xs-12 T-search-line"><input type="text" name="lineProductName" readonly="" class="bind-change col-xs-12" value=""><span class="hct-group-search cursor">[搜索]</span><input type="hidden" name="lineProductId" value=""></div></td>'
+			+ '<td><div class="hct-input-group col-xs-12 T-search-line"><input type="text" name="lineProductName" readonly="" class="bind-change col-xs-12" value=""><span class="hct-group-search T-searchLine T-option cursor">[搜索]</span><input type="hidden" name="lineProductId" value=""></div></td>'
 			+ '<td><input type="text" name="customerType" class="col-sm-12" readonly="readonly" /></td>'
 			+ '<td><input type="text" name="days" class="col-sm-10 F-float F-count" readonly="readonly" /><span class="col-sm-2" style="line-height: 30px">天</span></td>'
 			+ '<td><input class="datepicker T-startTime col-sm-12" name="startTime" type="text" value="" /></td>'
@@ -420,14 +420,6 @@ define(function(require, exports) {
 			subsection.datePicker("T-startTime");
 			/*subsection.lineProductChoose();*/
 			validator = rule.checkdSaveSubsection($tbody);
-
-			//新增费用项目
-			/*subsection.$tabSub.find('.T-subsectionOperationTbody').find('.T-add').off().on('click', function(event) {
-				event.preventDefault();
-				var $that = $(this),$tbody =subsection.$tbody;
-				 Act on the event *
-				subsection.addFeeItem($that, $tbody);
-			});*/
 
 			$tbody.find('.T-type').on('change', function(event) {
 				event.preventDefault();
@@ -715,85 +707,9 @@ define(function(require, exports) {
 			return value;
 		}
 	};
-	/**
-	 * 线路产品选择
-	 * @param  {[type]}  [description]
-	 */
-/*	subsection.lineProductChoose = function() {
-		var chooseLineProduct = subsection.$tabSub.find(".T-chooseLineProduct");
-		chooseLineProduct.autocomplete({
-			minLength:0,
-			change :function(event,ui){
-				if(ui.item == null){
-					var $this = $(this),$parents = $this.closest('tr');
-					$this.val("");
-					$parents.find("input[name=lineProductId]").val("");
-					$parents.find("input[name=days]").val("");
-					$parents.find("input[name=customerType]").val("");
-				}
-			},
-			select :function(event,ui){
-				var $this = $(this),$parents = $this.closest('tr');
-				$this.val(ui.item.name);
-				$parents.find("input[name=lineProductId]").val(ui.item.id).trigger('change');
-				if(ui.item.customerType == 0){
-					$parents.find("input[name=customerType]").val("散客");
-				}else{
-					$parents.find("input[name=customerType]").val("团体");
-				}
-				$parents.find("input[name=days]").val(ui.item.days);
-				var currentDays = ui.item.days;
-				    currentTime = $parents.find('.T-startTime').val();
-				if ($parents.next('tr').length > 0 && !!currentTime && !!currentDays) {
-					$parents.next('tr').find('.T-startTime').val(subsection.startIntime(currentDays,currentTime));
-				};
-				validator = rule.updateCheckdSaveSubsection(validator);
 
-				var sum = 0;
-				$parents.siblings('tr').each(function() {
-					sum += ($(this).find('[name="needPayAllMoney"]').val() || 0)*1;
-				});
-			}
-		}).unbind("click").click(function(){
-			var obj =this;
-			$.ajax({
-				url:KingServices.build_url('innerTransferOperation', "getLineProductList"),
-				type: 'POST',
-                showLoading: false,
-                success: function(data) {
-					var result = showDialog(data);
-					if(result){
-						var lineProductList = data.lineProductList;
-						if(lineProductList && lineProductList.length > 0){
-							for(var i=0; i < lineProductList.length; i++){
-								lineProductList[i].value = "("+lineProductList[i].days+"天)"+lineProductList[i].name;
-							}
-							$(obj).autocomplete('option','source', lineProductList);
-							$(obj).autocomplete('search', '');
-						}else{
-							layer.tips('没有线路信息', obj, {
-							    tips: [1, '#3595CC'],
-							    time: 2000
-							});
-						}
-					}
-                }
-			})
-		})
-	}*/
-
-
-
-
-		/**
-	 * 获取线路产品数据，并填入选择线路产品的对话框
-	 * @param  {object} $dialog dialog的Jquery对象
-	 * @param  {int} type    0：新增 1：更新
-	 * @param  {int} page    页码
-	 * @param  {string} name    搜索关键字
-	 * @return {[type]}         [description]
-	 */
-	subsection.getLineProductList = function(page, name) {
+	//初始化线路Layer层
+	subsection.getLayerProductList = function(){
 		var html=searchListTemplate({});  
 		var searchTravelLinelayer = layer.open({
 			type: 1,
@@ -805,12 +721,25 @@ define(function(require, exports) {
 			scrollbar: false,
 		});
 		var $dialog = $('.T-subsection-lineproduct-search');
+		//初始化线路产品数据
+		subsection.getLineProductList($dialog, 0, '');
+	};
+
+	/**
+	 * 获取线路产品数据，并填入选择线路产品的对话框
+	 * @param  {object} $dialog dialog的Jquery对象
+	 * @param  {int} type    0：新增 1：更新
+	 * @param  {int} page    页码
+	 * @param  {string} name    搜索关键字
+	 * @return {[type]}         [description]
+	 */
+	subsection.getLineProductList = function($dialog, page, name) {
 		page = page || 0;
 		$.ajax({
 			url:KingServices.build_url('innerTransferOperation', "getLineProductList"),
 			type: 'POST',
             showLoading: false,
-			data: "name=" + name + "&page=" + page
+			data: "name=" + name + "&pageNo=" + page
 		})
 		.done(function(data) {
 			if (showDialog(data)) {
@@ -818,14 +747,20 @@ define(function(require, exports) {
 				var listHtml = lineproductSearchList(data);
 				$tbody=$dialog.find('.T-normal-list');
 				$tbody.html(listHtml);
-				$tbody.closest('.tab-pane').find('.T-total').text(data.recordSize);
+				$tbody.closest('.tab-pane').find('.T-total').text(data.totalCount);
 
-				$dialog.find('.T-btn-search').on('click', function(event) {
-					event.preventDefault();
+				//线路产品搜索
+				$dialog.find('.T-btn-search').off().on('click', function(event) {
 					/* Act on the event */
 					var $that=$(this),$searchLine=$that.closest('.form-inline');
 					var name=$searchLine.find('.T-name').val();
 					subsection.getLineProductList($dialog, 0, name);
+				});
+
+				//提交选中线路
+				$dialog.find('.T-btn-submit').on('click', function(event) {
+					event.preventDefault();
+					/* Act on the event */
 				});
 
 				// 绑定翻页组件
@@ -835,7 +770,7 @@ define(function(require, exports) {
 				    curr: (data.pageNo + 1),
 				    jump: function(obj, first) {
 				    	if (!first) {  // 避免死循环，第一次进入，不调用页面方法
-							subsection.getLineProductList(obj.curr -1, name);
+							subsection.getLineProductList($dialog, obj.curr -1, name);
 				    	}
 				    }
 				});	
