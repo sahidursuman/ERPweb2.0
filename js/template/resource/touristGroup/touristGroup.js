5f8c24da6c03dad02f1e3a787ea96019067563c4
zhangjiangfeng <2826758674@qq.com>
2016-02-20 12:26:15 +0800

    删除注释

--- js/template/resource/touristGroup/touristGroup.js
+++ js/template/resource/touristGroup/touristGroup.js
@@ -312,16 +312,11 @@ define(function(require, exports) {
                     //外转确认需清空线路产品
                     if (status == undefined || status == null || status == "") {
                         if (Tools.addTab(updateTabId, "添加游客", html)) {
-                            var $updateTabId = $('#' + updateTab),
-                                $reciveObj=$updateTabId.find('.T-recive'),
-                                $sendObj=$updateTabId.find('.T-send');
+                            var $updateTabId = $('#' + updateTab);
                             $updateTabId.find('input[name=accompanyGuideName]').val("");
                             $updateTabId.find('input[name=accompanyGuideMobile]').val("");
                             $updateTabId.find('input[name=otaOrderNumber]').val("");
                             $updateTabId.find('input[name=orderNumber]').val("");
-                            //清空中转勾选
-                            $reciveObj.find('input[type=checkbox]').prop('checked',false);
-                            $sendObj.find('input[type=checkbox]').prop('checked',false);
                             touristGroup.updateEvents(typeOut);
                         }
                     } else if (!!status && !!InnerTransfer) {
