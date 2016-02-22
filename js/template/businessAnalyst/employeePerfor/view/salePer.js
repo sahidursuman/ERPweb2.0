f7a443f2e0b8193c9e953ba94a515e6a91f2ce82
zhangjiangfeng <2826758674@qq.com>
2016-02-22 17:59:44 +0800

    员工业绩

---
+++ js/template/businessAnalyst/employeePerfor/view/salePer.js
@@ -0,0 +1,4 @@
+/*TMODJS:{"debug":true,"version":14,"md5":"89b894cf46a57c672090589e6dd82332"}*/
+define(function(require) {
+    return require("../../../template")("businessAnalyst/employeePerfor/view/salePer", '<table class="table table-striped table-bordered table-hover T-showHighLight table-fixed"> <colgroup> <col style="width:50%;"></col> <col style="width:50%;"></col> </colgroup> <thead> <tr class="bg-blur"> <th class="col-sm-1">员工</th> <th class="col-sm-2">收客量</th> </tr> </thead> <tbody> <tr> <td>员工</td> <td>收客量</td> </tr> </tbody> </table> <div class="row pageMode"> <div class="col-xs-6"> <small>共计 条记录</small> </div> <div class="col-xs-6"> <div class="dataTables_paginate paging_simple_numbers T-pagenation"> </div> </div> </div>');
+});
