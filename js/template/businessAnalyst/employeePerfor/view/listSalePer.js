332eb8b90f0f41745b6219128b285c51593b51ff
zhangjiangfeng <2826758674@qq.com>
2016-02-23 11:46:39 +0800

    员工业绩调试Java

--- js/template/businessAnalyst/employeePerfor/view/listSalePer.js
+++ js/template/businessAnalyst/employeePerfor/view/listSalePer.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":22,"md5":"3140a698b6dbc5772fee3e9e855d1cd1"}*/
+/*TMODJS:{"debug":true,"version":29,"md5":"01bd00f8ed9ca5d16c8c352167496f75"}*/
 define(function(require) {
     return require("../../../template")("businessAnalyst/employeePerfor/view/listSalePer", function($data, $filename) {
         try {
@@ -7,7 +7,7 @@ define(function(require) {
             return $out += '<table class="table table-striped table-bordered table-hover T-showHighLight table-fixed"> <colgroup> <col style="width:50%;"></col> <col style="width:50%;"></col> </colgroup> <thead> <tr class="bg-blur"> <th class="col-sm-1">员工</th> <th class="col-sm-2">收客量</th> </tr> </thead> <tbody> ', 
             $line = 13, $each(result, function(salePerL) {
                 $out += " <tr> <td>", $line = 15, $out += $escape(salePerL.name), $out += "</td> <td>", 
-                $line = 16, $out += $escape(salePerL.getAdultCount), $out += "大", $line = 16, $out += $escape(salePerL.getChildCount), 
+                $line = 16, $out += $escape(salePerL.adultCount), $out += "大", $line = 16, $out += $escape(salePerL.childCount), 
                 $out += "小</td> </tr> ", $line = 18;
             }), $out += ' </tbody> </table> <div class="row pageMode"> <div class="col-xs-6"> <small>共计 ', 
             $line = 23, $out += $escape(searchParam.totalCount), $out += ' 条记录</small> </div> <div class="col-xs-6"> <div class="dataTables_paginate paging_simple_numbers T-listSalePer-pagenation"> </div> </div> </div>', 
@@ -18,7 +18,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '<table class="table table-striped table-bordered table-hover T-showHighLight table-fixed">\r\n	<colgroup>\r\n		<col style="width:50%;"></col>\r\n		<col style="width:50%;"></col>\r\n	</colgroup>\r\n	<thead>\r\n		<tr class="bg-blur">\r\n			<th class="col-sm-1">员工</th>\r\n			<th class="col-sm-2">收客量</th>\r\n		</tr>\r\n	</thead>\r\n	<tbody>\r\n		{{each result as salePerL}}\r\n		   <tr>\r\n	   		    <td>{{salePerL.name}}</td>\r\n	   			<td>{{salePerL.getAdultCount}}大{{salePerL.getChildCount}}小</td>\r\n	   		</tr>\r\n	   	{{/each}}\r\n	</tbody>\r\n</table>\r\n<div class="row pageMode">\r\n	<div class="col-xs-6">\r\n		<small>共计 {{searchParam.totalCount}}  条记录</small>\r\n	</div>\r\n	<div class="col-xs-6">\r\n		<div class="dataTables_paginate paging_simple_numbers T-listSalePer-pagenation">\r\n		</div>\r\n	</div>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '<table class="table table-striped table-bordered table-hover T-showHighLight table-fixed">\r\n	<colgroup>\r\n		<col style="width:50%;"></col>\r\n		<col style="width:50%;"></col>\r\n	</colgroup>\r\n	<thead>\r\n		<tr class="bg-blur">\r\n			<th class="col-sm-1">员工</th>\r\n			<th class="col-sm-2">收客量</th>\r\n		</tr>\r\n	</thead>\r\n	<tbody>\r\n		{{each result as salePerL}}\r\n		   <tr>\r\n	   		    <td>{{salePerL.name}}</td>\r\n	   			<td>{{salePerL.adultCount}}大{{salePerL.childCount}}小</td>\r\n	   		</tr>\r\n	   	{{/each}}\r\n	</tbody>\r\n</table>\r\n<div class="row pageMode">\r\n	<div class="col-xs-6">\r\n		<small>共计 {{searchParam.totalCount}} 条记录</small>\r\n	</div>\r\n	<div class="col-xs-6">\r\n		<div class="dataTables_paginate paging_simple_numbers T-listSalePer-pagenation">\r\n		</div>\r\n	</div>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
