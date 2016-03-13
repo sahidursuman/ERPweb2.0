765ff91d7ea086494a26cbc4fdc384ba5d43dbcb
xiongzuyao <x@xiongzy.com>
2016-03-13 17:46:23 +0800

    【游客管理】新版本制作

--- js/template/resource/touristGroupN/touristGroup.js
+++ js/template/resource/touristGroupN/touristGroup.js
@@ -6,14 +6,17 @@ define(function(require, exports) {
 		},
 		//模板文件
 		T = {
-			list : require('./view/list'),
-			listTable : require('./view/listTable'),
-			update : require('./view/update'),
-			guestInfo : require('./view/guestInfo'),
-			updateMoney : require('./view/updateMoney'),
-			updateBus : require('./view/updateBus'),
-			updateHotel : require('./view/updateHotel'),
-			updateOther : require('./view/updateOther'),
+			list : require('./view/list'),//列表页
+			listTable : require('./view/listTable'),//列表页表格
+			update : require('./view/update'),//编辑页面
+			guestInfo : require('./view/guestInfo'),//客人信息
+			updateMoney : require('./view/updateMoney'),//编辑金额
+			updateBus : require('./view/updateBus'),//编辑车
+			updateHotel : require('./view/updateHotel'),//编辑房
+			updateOther : require('./view/updateOther'),//编辑其它
+            updateInnerTurn : require('./view/updateInnerTurn'),//编辑内转
+            updateOuterTurn : require('./view/updateOuterTurn'),//编辑外转
+            chooseHotel : require('./view/chooseHotel'),//自选酒店
 		},
 
 		touristGroup = {};
@@ -26,7 +29,7 @@ define(function(require, exports) {
     	touristGroup.listTouristGroup(0, args);
     };
     /**
-     * 游客管理列表
+     * 游客管理列表页
      * @param  {number} page 翻页
      * @param  {object} args 请求参数
      * @param  {object} $tab tab的jQuery对象
@@ -53,6 +56,12 @@ define(function(require, exports) {
     	});
     };
 
+    /**
+     * 获取游客管理列表
+     * @param  {object} args 请求参数
+     * @param  {object} $tab tab的jQuery对象
+     * @return {[type]}      [description]
+     */
     touristGroup.getList = function(args, $tab){
     	$.ajax({
             url: KingServices.build_url('touristGroup','listTouristGroup'),
@@ -141,15 +150,19 @@ define(function(require, exports) {
 
     /**
      * 列表页初始化事件
-     * @param  {[type]} $tab [description]
-     * @return {[type]}      [description]
+     * @param  {object} $tab tab的jQuery对象
+     * @return {object}      touristGroup对象
      */
     touristGroup.init_events = function($tab){
     	var $searchArea = $tab.find(".T-search-area");
     	$searchArea.find('.T-choosePorB').on('change', function() {
 
         });
-
+        //添加游客小组事件
+        $tab.find('.T-touristGroup-add').on('click', function(){
+            touristGroup.touristGroupUpdate();
+        });
+        //表内操作
     	$tab.find('.T-touristGroup').on('click', '.T-action', function(event){
     		event.preventDefault();
     		var $that = $(this), id = $that.closest('tr').data('id');
@@ -161,9 +174,16 @@ define(function(require, exports) {
     	return this;
     };
 
-
+    /**
+     * 编辑游客管理
+     * @param  {number} id 游客ID
+     * @return {[type]}    [description]
+     */
     touristGroup.touristGroupUpdate = function(id){
-    	$.ajax({
+        if (Tools.addTab(K.update , '编辑小组', T.update())) {
+            touristGroup.commonEvents($("#tab-" + K.update + "-content"));
+        }
+    	/*$.ajax({
     		url : KingServices.build_url('touristGroup', 'viewTouristGroupDetails'),
     		data : {id : id},
     		type: 'POST',
@@ -175,9 +195,14 @@ define(function(require, exports) {
                     }
     			}
     		}
-    	});
+    	});*/
     };
 
+    /**
+     * 编辑和添加通用事件
+     * @param  {object} $tab tab的jQuery对象
+     * @return {[type]}      [description]
+     */
     touristGroup.commonEvents = function($tab){
     	//添加接团
     	$tab.find('.T-add-join-group').on('click', function(){
@@ -217,7 +242,13 @@ define(function(require, exports) {
     			touristGroup.updateJionGroupMoney(1);
     		}else if($that.hasClass('T-hotel')){
     			touristGroup.updateJionGroupHotel(1);
-    		}
+    		}else if($that.hasClass('T-inner-turn')){
+                touristGroup.updateInnerTurn();
+            }else if($that.hasClass('T-outer-turn')){
+                touristGroup.updateOuterTurn();
+            }else if($that.hasClass('T-delete')){
+                $that.closest('tr').remove();
+            }
     	});
     	//送团事件
     	$tab.find('.T-send-group-list').on('click', '.T-action', function(event){
@@ -244,7 +275,8 @@ define(function(require, exports) {
 				$tab.find('.T-other-info-cont').addClass('hidden');
     		}
     	});
-    	
+        //绑定日期事件
+        Tools.setDatePicker($tab.find('.datepicker'));
     };
 
     //添加接团
@@ -285,6 +317,31 @@ define(function(require, exports) {
     	$tab.find('.T-send-group-list').append(html);
     };
 
+    //游客列表序号自动升序
+    touristGroup.memberNumber = function($obj) {
+        var $tbody = $obj.find('tbody.T-addTouristTbody').children('tr');
+        $tbody.each(function(i) {
+            if (i >= 0) {
+                $(this).children().eq(0).text(i + 1);
+            }
+        });
+    };
+    //添加游客
+    touristGroup.addVisotor = function($obj) {
+        var html = '<tr>' +
+            '<td>' + '</td>' +
+            '<td><input name="name" type="text" class="col-sm-12  no-padding-right" /></td>' +
+            '<td><input name="mobileNumber" type="text" class="col-sm-12  no-padding-right T-mobileNumber"  maxlength="11"  /></td>' +
+            '<td><select name="idCardType" value="idCardTypeId" class="col-xs-12"><option value="0" selected="selected">身份证</option><option value="1">护照</option><option value="2">其它</option></select></td>' +
+            '<td><input name="idCardNumber" type="text" class="col-sm-12  no-padding-right" /></td>' +
+            '<td><div class="checkbox"><label><input type="checkbox" class="ace " value="1" name="isContactUser"><span class="lbl"></span></label></div></td>' +
+            '<td><a class="cursor T-delete">删除</a></td>' +
+            '</tr>';
+        var $tbody = $obj.find('.T-addTouristTbody')
+        $tbody.append(html);
+        touristGroup.memberNumber($obj);
+    };
+
     //编辑客人信息
     touristGroup.updateGuestInfo = function(){
     	var layerIndex = layer.open({
@@ -295,8 +352,21 @@ define(function(require, exports) {
 		    zIndex:1028,
 		    content: T.guestInfo(),
 		    scrollbar: false,
-		    success:function(){
-		    	
+		    success:function(obj){
+                var $layer = $(obj);
+                $layer.find('.T-add-tourist-more').on('click', function(){
+                    touristGroup.batchAddTourists($layer);
+                });
+                $layer.find('.T-add-tourist').on('click', function(){
+                    touristGroup.addVisotor($layer);
+                });
+                $layer.find('.T-addTouristTbody').on('click', '.T-action', function(event){
+                    event.preventDefault();
+                    var $that = $(this);
+                    if($that.hasClass('T-delete')){
+                        $that.closest('tr').remove();
+                    }
+                });
 		    }
 		});
     };
@@ -344,22 +414,28 @@ define(function(require, exports) {
 		});
     };
 
-    //更新接/送团车辆
+    //更新接/参/送团房
     touristGroup.updateJionGroupHotel = function(type){
-    	var title = "接团住宿";
+    	var title = "接团住宿", data = {};
     	if(type === 1 || type === 2){
     		title = "返程住宿";
     	}
+        data.type = type;
     	var layerIndex = layer.open({
 			type: 1,
 		    title: title,
 		    skin: 'layui-layer-rim', //加上边框
 		    area: '560px', //宽高
 		    zIndex:1028,
-		    content: T.updateHotel(),
+		    content: T.updateHotel(data),
 		    scrollbar: false,
-		    success:function(){
-		    	
+		    success:function(obj){
+                var $layer = $(obj);
+		    	if(type === 1){
+                    $layer.find('.T-choose-hotel').on('click', function() {
+                        touristGroup.chooseHotel();
+                    });
+                }
 		    }
 		});
     };
@@ -378,11 +454,90 @@ define(function(require, exports) {
 		    zIndex:1028,
 		    content: T.updateOther(),
 		    scrollbar: false,
-		    success:function(){
-		    	
+		    success:function(obj){
+		    	var $layer = $(obj).on('click', '.T-action', function(event) {
+                    event.preventDefault();
+                    var $that = $(this);
+                    if($that.hasClass('T-restaurant')){
+                        console.log("1");
+                    }
+                });
+                $layer.find('.T-action-plan').on('change', 'input[type="checkbox"]', function(event){
+                    event.preventDefault();
+                    var $that = $(this), 
+                        $label = $that.closest('label'),
+                        type = $label.data('type');
+
+                    if(type === "restaurant"){
+                        $layer.find('.T-ask-restaurant').toggleClass('hidden');
+                    }else if(type === "ticket"){
+                        $layer.find('.T-ask-ticket').toggleClass('hidden');
+                    }else if(type === "other"){
+                        $layer.find('.T-ask-other').toggleClass('hidden');
+                    }
+                });
+                $layer.find('.T-btn-save').on('click', function(){
+                    layer.close(layerIndex);
+                });
 		    }
 		});
     };
 
+
+
+    //参团内转操作
+    touristGroup.updateInnerTurn = function(){
+        var layerIndex = layer.open({
+            type: 1,
+            title: "编辑内转信息",
+            skin: 'layui-layer-rim', //加上边框
+            area: '870px', //宽高
+            zIndex:1028,
+            content: T.updateInnerTurn(),
+            scrollbar: false,
+            success:function(){
+                
+            }
+        });
+    };
+
+    //参团外转操作
+    touristGroup.updateOuterTurn = function(){
+        var layerIndex = layer.open({
+            type: 1,
+            title: "编辑外转信息",
+            skin: 'layui-layer-rim', //加上边框
+            area: '870px', //宽高
+            zIndex:1028,
+            content: T.updateInnerTurn(),
+            scrollbar: false,
+            success:function(){
+                
+            }
+        });
+    };
+
+    //自选酒店
+    touristGroup.chooseHotel = function(){
+        var layerIndex = layer.open({
+            type: 1,
+            title: "自选酒店",
+            skin: 'layui-layer-rim', //加上边框
+            area: '570px', //宽高
+            zIndex:1028,
+            content: T.chooseHotel(),
+            scrollbar: false,
+            success:function(){
+                
+            }
+        });
+    };
+    
+    //批量添加游客
+    touristGroup.batchAddTourists = function($obj) {
+        seajs.use("" + ASSETS_ROOT + modalScripts.arrange_plan,function(module){
+            module.addVisotorMore($obj.find('.T-addTouristTbody'), touristGroup.memberNumber);
+        });
+    };
     exports.init = touristGroup.initModule;
 });
