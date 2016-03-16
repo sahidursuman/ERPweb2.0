82aee4d0c1a7312c183887db4243454243e9b2d6
xiongzuyao <x@xiongzy.com>
2016-03-16 15:20:46 +0800

    【游客管理】新版本开发

---
+++ js/template/resource/touristGroupN/view/chooseClientList.js
@@ -0,0 +1,4 @@
+/*TMODJS:{"debug":true,"version":1,"md5":"d35756fa9543dbd722bd5c13653b4e57"}*/
+define(function(require) {
+    return require("../../../template")("resource/touristGroupN/view/chooseClientList", ' <tr> <td>成都辉煌酒店</td> <td>成都辉煌酒店</td> <td>成都辉煌酒店</td> <td>成都辉煌酒店</td> <td>成都辉煌酒店</td> <td> <input type="radio" name="chooseClient" class="ace"> <label class="lbl"></label> </td> </tr>');
+});
