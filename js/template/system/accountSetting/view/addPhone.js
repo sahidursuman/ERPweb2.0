e321cba9e5769da882d04fcd4504f92be3516ae7
duqinqin <13880834409@163.com>
2016-03-21 15:48:24 +0800

    账户设置申请借款添加编辑操作

--- js/template/system/accountSetting/view/addPhone.js
+++ js/template/system/accountSetting/view/addPhone.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":6,"md5":"8b0b5a45ff4594c864f442e2179fea82"}*/
+/*TMODJS:{"debug":true,"version":11,"md5":"1f285c885fc37a0adf817dfbc620a70e"}*/
 define(function(require) {
     return require("../../../template")("system/accountSetting/view/addPhone", function($data, $filename) {
         try {
@@ -12,7 +12,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '<div class="applicationMain-other">\r\n	<form class="T-main-form" role="form" onsubmit="return false">\r\n		<label style="margin-left: 40px;font-family: \'宋体\';color: #666666;margin-top: 10px;">请输入您要绑定的手机号码</label>\r\n		<div class="application">\r\n		<input type="text" placeholder="请输入手机号" name="mobile" class="newPhone" value="{{mobile}}"  style="width: 370px;height: 40px !important;"></div>\r\n		<div class="application" style="margin-top: 20px;padding: 0px 0px 20px 0px;"><input type="text" name="verifyCode" placeholder="手机验证码" class="Verification-code" style="width: 240px;height: 40px !important;">\r\n		<input class="applicationBtn" type="button" id="sended"  style="width: 120px;height: 40px !important;margin-left: 6px;" value="发送验证码"></div>\r\n\r\n		<button class="btn btn-block btn-primary T-newChangePhone guideSubmit" style="width:400px;margin-left:20px;"> <i class="ace-icon fa fa-check"></i> 确定 </button>\r\n	</form>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '<div class="applicationMain-other">\r\n	<form class="T-main-form" role="form" onsubmit="return false">\r\n		<label style="margin-left: 40px;font-family: \'宋体\';color: #666666;margin-top: 10px;">请输入您要绑定的手机号码</label>\r\n		<div class="application">\r\n		<input type="text" placeholder="请输入手机号" name="mobile" class="newPhone" value="{{mobile}}"  style="width: 370px;height: 40px !important;"></div>\r\n		<div class="application" style="margin-top: 20px;padding: 0px 0px 20px 0px;"><input type="text" name="verifyCode" placeholder="手机验证码" class="Verification-code" style="width: 240px;height: 40px !important;">\r\n		<input class="applicationBtn" type="button" id="sended"  style="width: 120px;height: 40px !important;margin-left: 6px;" value="发送验证码"></div>\r\n\r\n		<button class="btn btn-block btn-primary T-newChangePhone guideSubmit" style="width:400px;margin-left:20px;"> <i class="ace-icon fa fa-check"></i> 确定 </button> \r\n	</form>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
