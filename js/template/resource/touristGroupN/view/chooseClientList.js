eed8e3dbbe2aff41c3827d13967f848f23ed4819
xiongzuyao <x@xiongzy.com>
2016-03-22 19:40:15 +0800

    【游客管理】新版本制作

--- js/template/resource/touristGroupN/view/chooseClientList.js
+++ js/template/resource/touristGroupN/view/chooseClientList.js
@@ -1,25 +1,2 @@
-/*TMODJS:{"debug":true,"version":9,"md5":"16b73d894dea8a127e97de35fad04214"}*/
-define(function(require) {
-    return require("../../../template")("resource/touristGroupN/view/chooseClientList", function($data, $filename) {
-        try {
-            var $utils = this, $helpers = $utils.$helpers, $line = 0, $each = $utils.$each, travelAgencyList = $data.travelAgencyList, $escape = ($data.rs, 
-            $data.$index, $utils.$escape), $out = "";
-            return $line = 1, $each(travelAgencyList, function(rs) {
-                $out += ' <tr class="cursor"> <td name="travelAgencyName">', $line = 3, $out += $escape(rs.travelAgencyName), 
-                $out += "</td> <td>", $line = 4, $out += $escape($helpers.getTravelAgencyType(rs.type)), 
-                $out += "</td> <td>", $line = 5, $out += $escape($helpers.getTravelAgencyLevel(rs.level)), 
-                $out += "</td> <td>", $line = 6, $out += $escape(rs.contactRealname), $out += "</td> <td>", 
-                $line = 7, $out += $escape(rs.contactMobileNumber), $out += '</td> <td> <input type="radio" name="chooseClient" class="ace"> <label class="lbl"></label> </td> </tr> ', 
-                $line = 13;
-            }), new String($out);
-        } catch (e) {
-            throw {
-                filename: $filename,
-                name: "Render Error",
-                message: e.message,
-                line: $line,
-                source: '{{each travelAgencyList as rs}}\r\n<tr class="cursor">\r\n	<td name="travelAgencyName">{{rs.travelAgencyName}}</td>\r\n	<td>{{rs.type | getTravelAgencyType}}</td>\r\n	<td>{{rs.level | getTravelAgencyLevel}}</td>\r\n	<td>{{rs.contactRealname}}</td>\r\n	<td>{{rs.contactMobileNumber}}</td>\r\n	<td>\r\n		<input type="radio" name="chooseClient" class="ace">\r\n		<label class="lbl"></label>\r\n	</td>\r\n</tr>\r\n{{/each}}'.split(/\n/)[$line - 1].replace(/^\s+/, "")
-            };
-        }
-    });
-});
+/*TMODJS:{"version":10,"md5":"86a954460873051fd05d30d44c445b19"}*/
+define(function(require){return require("../../../template")("resource/touristGroupN/view/chooseClientList",function(a){"use strict";var b=this,c=b.$helpers,d=b.$each,e=a.travelAgencyList,f=(a.rs,a.$index,b.$escape),g="";return d(e,function(a){g+=' <tr class="cursor" data-id="',g+=f(a.travelAgencyId),g+='"> <td name="travelAgencyName">',g+=f(a.travelAgencyName),g+="</td> <td>",g+=f(c.getTravelAgencyType(a.type)),g+="</td> <td>",g+=f(c.getTravelAgencyLevel(a.level)),g+="</td> <td>",g+=f(a.contactRealname),g+="</td> <td>",g+=f(a.contactMobileNumber),g+='</td> <td> <input type="radio" name="chooseClient" class="ace"> <label class="lbl"></label> </td> </tr> '}),new String(g)})});
