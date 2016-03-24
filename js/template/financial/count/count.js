2490ffab55b5742ccf0fd22684c7f3994e5aac89
wenJoy <laojiaodaoniu@126.com>
2016-03-24 20:45:00 +0800

    修复报账审核打不开

--- js/template/financial/count/count.js
+++ js/template/financial/count/count.js
@@ -796,7 +796,7 @@ define(function(require, exports){
 		$guideObj.find('input[type=text]').off('change').on('change',function(){
 			var $that = $(this);
 				tagName = $that.attr('name');
-			if (tagName !='remark')) {
+			if (tagName !='remark') {
 				Count.calculateCost($that);
 				//计算金额
 				Count.autoGuideSum($that,$obj);
