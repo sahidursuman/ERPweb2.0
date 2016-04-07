57a40a88898b2a7b54b0d3b328fb2e66f62ddc47
roger <guiyun_wei@163.com>
2016-04-07 18:58:52 +0800

    【导游排期】联调

--- js/template/arrange/guide/guide.js
+++ js/template/arrange/guide/guide.js
@@ -1,305 +1,245 @@
 define(function(require, exports) {
-	var MainTemplate = require('./view/listMain'),
-		ListTemplate = require('./view/list'),
-		menuKey = 'guide_arrange_table';
+    var MainTemplate = require('./view/listMain'),
+        ListTemplate = require('./view/list'),
+        menuKey = 'guide_arrange_table';
 
 
-	 // 定义业务对象
-	 var GuideArrange = {};
+    // 定义业务对象
+    var GuideArrange = {},
+        colorList = {},
+        list, $table, tbl_start, tbl_end;
 
-	 // 初始化首页页面
-	 GuideArrange.initMain = function() {
-	 	Tools.addTab(menuKey, '导游排班表', MainTemplate());
+    // 初始化首页页面
+    GuideArrange.initMain = function() {
+        Tools.addTab(menuKey, '导游排期表', MainTemplate());
 
-	 	GuideArrange.init_event();
-	 };
+        GuideArrange.init_event();
+    };
 
-	 // 绑定首页事件
-	 GuideArrange.init_event = function() {
-	 	var $tab = $('#tab-'+ menuKey + '-content'),
-	 		$searchArea = $tab.find('.T-search-area'),
-	 		$form = $searchArea.find('form');
+    // 绑定首页事件
+    GuideArrange.init_event = function() {
+        var $tab = $('#tab-' + menuKey + '-content'),
+            $searchArea = $tab.find('.T-search-area'),
+            $form = $searchArea.find('form');
 
- 		GuideArrange.initSelect($tab);
+        GuideArrange.initSelect($tab);
 
- 		Tools.setDatePicker($searchArea.find('.datepicker'), true);
+        Tools.setDatePicker($searchArea.find('.datepicker'), true);
 
- 		// search
- 		$searchArea.find('.T-search').on('click', function(event) {
- 			event.preventDefault();
- 			GuideArrange.initGuideList($form);
- 		}).trigger('click');
+        // search
+        $searchArea.find('.T-search').on('click', function(event) {
+            event.preventDefault();
+            GuideArrange.initGuideList($form);
+        }).trigger('click');
 
- 		GuideArrange.$tab = $tab;
-	 };
+        // 查看计划
+        $tab.find('.T-table-area').on('click', '.T-tranplan', function(event) {
+            event.preventDefault();
+            var $that = $(this),
+                // 团散标记：0 散客团，1 团体团，默认值 0
+                type = $that.data('type'),
+                id = $that.data('id'),
+                triplanModuleScript = modalScripts.arrange_plan; //团队
 
-	 /**
-	  * 初始化安排表
-	  * @return {[type]}      [description]
-	  */
-	 GuideArrange.initGuideList = function($form, page) {
-	 	var args = $form.serializeJson();
+            if (type == '0') {
+                triplanModuleScript = modalScripts.arrange_singlePlan;
+            }
 
-	 	args.pageNo = page || 0;
-	 	$.ajax({
-	 		url: KingServices.build_url('guideDate', 'findPager'),
-	 		type: 'get',
-	 		data: args,
-	 	})
-	 	.done(function(data) {	 		
-	 		if (showDialog(data)) {
-	 			// 加载表格框架
-	 			$form.parent().next().html(ListTemplate(data.searchParam.totalCount));
+            seajs.use("" + ASSETS_ROOT + triplanModuleScript, function(module) {
+                module.viewTripPlan(id * 1);
+            });
+        });
 
- 				GuideArrange.loadList(data.guideList);
+        GuideArrange.$tab = $tab;
+    };
 
-		 		laypage({
-		 		    cont: GuideArrange.$tab.find('.T-pagenation'), //容器。值支持id名、原生dom对象，jquery对象,
-		 		    pages: data.searchParam.totalPage, //总页数
-		 		    curr: (args.pageNo + 1),
-		 		    jump: function(obj, first) {
-		 		        if (!first) {  // 避免死循环，第一次进入，不调用页面方法
-		 		            GuideArrange.initGuideList($form, obj.curr -1);
-		 		        };
-		 		    }
-		 		});
-	 		}
-	 	});
-	 };
+    /**
+     * 初始化安排表
+     * @return {[type]}      [description]
+     */
+    GuideArrange.initGuideList = function($form, page) {
+        var args = $form.serializeJson();
 
-	 var list, $table, tbl_start, tbl_end;
-	 GuideArrange.loadList = function(guides) {
-	 	// 初始化数据
-	 	$table = GuideArrange.$tab.find('table'),
-	 	tbl_start = tbl_end = 0;
+        args.pageNo = page || 0;
+        $.ajax({
+                url: KingServices.build_url('guideDate', 'findPager'),
+                type: 'get',
+                data: args,
+            })
+            .done(function(data) {
+                if (showDialog(data)) {
+                    // 加载表格框架
 
- 		for (var i = 0, len = guides.length; i < len; i ++) {
- 			var str = [], innerTd = [];
+                    var html = GuideArrange.loadList(data.guideList, data.searchParam.totalCount);
+                    $form.parent().next().html(html || '');
 
- 			for (var j = 0, jLen = guides[i].guideArrangeList.length, tmp; j < jLen; j ++ ) {
- 				guides[i].guideArrangeList.sort(function(x, y) {
- 					if (!tbl_start || tbl_start > x.startTime) {
- 						tbl_start = x.startTime;
- 					}
-
- 					if (!tbl_end || tbl_end < x.endTime) {
- 						tbl_end = x.endTime;
- 					}
-
- 					if (!tbl_start || tbl_start > y.startTime) {
- 						tbl_start = y.startTime;
- 					}
-
- 					if (!tbl_end || tbl_end < y.endTime) {
- 						tbl_end = y.endTime;
- 					}
- 					return (x.startTime > y.startTime)? 1: -1;
- 				});
- 			}
-
- 			console.info(tbl_start)
- 			console.info(tbl_end)
-			// innerTd.push('<td class="T-tranplan" data-id="'+ tmp.tripPlanId +'">'+ tmp.lineProductName +'</td>')
- 			// str.push('<tr><td>'+ guides[i].name +'</td>');
-
- 			// str.push('</tr>');
- 		}
-	 }
-	 // function pendTab() {
-	 // 	var $table = $('')
-	 // }
-	 // var $table = $('#guide-arrange'),
-	 // 	tbl_start = 0, tbl_end = 0,
-	 // 	// 储存已经添加的名称
-	 // 	nameList = [];
-
-	 // function pushItem(info) {
-	 // 	appendTable(info);
-	 // 	addLine(info);
-	 // }
-
-	 // function appendTable(info) {
-	 // 	var start = info.startTime,
-	 // 		end = info.endTime,
-	 // 		left = 0,
-	 // 		right = 0,
-	 // 		dayList = [start, end, tbl_start, tbl_end].sort(),
-	 // 		right_start = tbl_end;
-
-	 // 	if (tbl_start === 0)  {
-	 // 		tbl_start = start;
-	 // 		tbl_end = end;
-	 // 		left = Tools.getDateDiff(start, end) +1;
-	 // 	} else {
-	 // 		switch(dayList.indexOf(end))  {
-	 // 			// 最后一个的位置
-	 // 			case 1: 		// 全部左侧
-	 // 				left = Tools.getDateDiff(start, tbl_start);
-	 // 				tbl_start = start;
-	 // 				break;
-	 // 			case 2: 		// 左侧在左
-	 // 				if (start < tbl_start) {
-	 // 					left = Tools.getDateDiff(start, tbl_start);
-	 // 					tbl_start = start;
-	 // 				}
-	 // 				break;
-	 // 			case 3:
-	 // 				if (start < tbl_start) {  // 左右侧
-	 // 					left = Tools.getDateDiff(start, tbl_start);
-	 // 					tbl_start = start;
-	 // 					rgith = Tools.getDateDiff(tbl_end, end);
-	 // 				} else if (start > tbl_end) { // 左侧在内部
-	 // 					right = Tools.getDateDiff(tbl_end, end);
-	 // 					tbl_end = end;
-	 // 				} else if (start <= tbl_end) {  // 左侧在外部
-	 // 					right = Tools.getDateDiff(start, end);
-	 // 					tbl_end = end;
-	 // 					tbl_start = start;							
-	 // 				}
-	 // 				break;
-	 // 			default:
-	 // 				return;
-	 // 		}
-	 // 	}
-
-	 // 	if (left) {
-	 // 		var str = [], tds = [];
-	 // 		for (var i = 0, day; i < left; i ++) {
-	 // 			day = addDay(tbl_start, i);
-	 // 			str.push('<th class="'+ day + '">'+ day.split('-')[2] +'</th>');
-	 // 			tds.push('<td></td>');
-	 // 		}
-	 // 		$(str.join('')).insertAfter($table.find('.name-header'));
-
-	 // 		tds = tds.join('');
-	 // 		$table.find('#arrange-list').children('tr').each(function() {
-	 // 			$(tds).insertAfter($(this).find('.name'));
-	 // 		})
-	 // 	}
-
-	 // 	if (right++) {
-	 // 		var str = [], tds = [];
-	 // 		for (var i = 1, day; i < right; i ++) {
-	 // 			day = addDay(right_start, i);
-	 // 			str.push('<th class="'+ day + '">'+ day.split('-')[2] +'</th>');
-	 // 			tds.push('<td></td>');
-	 // 		}
-	 // 		$table.find('thead>tr').append(str.join(''));
-
-	 // 		tds = tds.join('');
-	 // 		$table.find('#arrange-list').children('tr').each(function() {
-	 // 			$(this).append(tds);
-	 // 		})
-	 // 	}
-	 // }
-
-	 // function addLine(info) {
-	 // 	var index = nameList.indexOf(info.name),
-	 // 		start = info.startTime,
-	 // 		end = info.endTime,
-	 // 		days = Tools.getDateDiff(start, end) + 1;
-
-	 // 	if (index >= 0) {
-	 // 		var $tr = $table.find('#arrange-list').children('tr').eq(index), 
-	 // 			$th = $table.find('thead').find('.' + start);
-
-	 // 		if ($th.length) {
-	 // 			var i = $th.index(), $tds = $tr.children('td'), 
-	 // 				len = $tds.length -1,
-	 // 				$td,
-	 // 				left = $th.position().left;
-
-	 // 			if (len < i) {
-	 // 				i = len;
-	 // 			}
-	 // 			while(i >= 0) {
-	 // 				$td = $tds.eq(i);
-	 // 				if (left === $td.position().left) {
-	 // 					break;
-	 // 				}
-	 // 				i --;
-	 // 			}
-
-	 // 			if (i >= 0) {
-	 // 				// days ++;
-	 // 				var $next = $td.next();
-	 // 				$td[0].colSpan = days;
-	 // 				$td.text(info.lineProduct).addClass('bg-blue');
-
-	 // 				for (var i = 1, $next, $tmp; i < days && $next.length; i ++ ) {
-	 // 					$tmp = $next;
-	 // 					$next = $next.next();
-	 // 					$tmp.remove();
-	 // 				}
-	 // 			}
-	 // 		}
-	 // 	} else {
-	 // 		var str = [], len;
-
-	 // 		// name
-	 // 		str.push('<tr><td class="name">'+ info.name + '</td>');
-
-	 // 		// start 
-	 // 		len = Tools.getDateDiff(tbl_start, start);
-	 // 		if (len) {
-	 // 			str.push(new Array(len + 1).join('<td></td>'));
-	 // 		}
-
-	 // 		str.push('<td class="bg-blue" colspan="'+ days +'">'+ info.lineProduct +'</td>');
-
-	 // 		// end
-	 // 		len = Tools.getDateDiff(end, tbl_end);
-	 // 		if (len) {
-	 // 			str.push(new Array(len + 1).join('<td></td>'));
-	 // 		}
-	 // 		str.push('</tr>');
-
-	 // 		nameList.push(info.name);
-	 // 		$('#arrange-list').append(str.join(''));
-	 // 	}
-	 // }
-
-	 /**
-	  * 绑定下拉菜单
-	  * @param  {object} $tab 顶层元素
-	  * @return {[type]}      [description]
-	  */
-	 GuideArrange.initSelect = function($tab) {
-	 	$tab.find('.T-guideName').autocomplete({
-			minLength:0,
-			change:function(event,ui){
-				if(ui.item == null){
-					$(this).closest('div').find('input[name="guideId"]').val('');
-				}
-			},
-			select:function(event,ui){
-				$(this).blur();
-				$(this).closest('div').find('input[name="guideId"]').val(ui.item.id);
-			}
-		}).off("click").on("click", function(){
-			var obj = this;
-			$.ajax({
-				url:KingServices.build_url("guide","findAll"),
-				type:'POST',
-				data:{
-					menuKey:"resource_guide"
-				},
-                showLoading:false,
-                success: function(data) {
-					var result = showDialog(data);
-					if(result){
-						var guideList = JSON.parse(data.guideList);
-						if(guideList != null && guideList.length > 0){
-							for(var i=0;i<guideList.length;i++){
-								guideList[i].value = guideList[i].realname;
-							}
-						}
-						$(obj).autocomplete('option','source', guideList);
-						$(obj).autocomplete('search', '');
-					}
+                    laypage({
+                        cont: GuideArrange.$tab.find('.T-pagenation'), //容器。值支持id名、原生dom对象，jquery对象,
+                        pages: data.searchParam.totalPage, //总页数
+                        curr: (args.pageNo + 1),
+                        jump: function(obj, first) {
+                            if (!first) { // 避免死循环，第一次进入，不调用页面方法
+                                GuideArrange.initGuideList($form, obj.curr - 1);
+                            };
+                        }
+                    });
                 }
             });
-		});
-	 }
-	 exports.init = GuideArrange.initMain;
+    };
+
+    /**
+     * 组织表格数据
+     * @param  {object} guides 导游安排数据
+     * @param  {int} total  记录总条数
+     * @return {string}        表格的HTML
+     */
+    GuideArrange.loadList = function(guides, total) {
+        // 初始化数据
+        $table = GuideArrange.$tab.find('table'),
+            tbl_start = tbl_end = 0;
+        var data = {},
+            thStr = [],
+            tdStr = [],
+            date,
+            len = guides.length;
+
+        for (var i = 0, len = guides.length; i < len; i++) {
+            guides[i].guideArrangeList.sort(function(x, y) {
+                GuideArrange._fetchRange(x);
+                GuideArrange._fetchRange(y);
+                return (x.startTime > y.startTime) ? 1 : -1;
+            });
+
+            if (guides[i].guideArrangeList.length === 1) {
+                GuideArrange._fetchRange(guides[i].guideArrangeList[0]);
+            }
+        }
+
+        // 组织th
+        for (i = 0, date;; i++) {
+            date = Tools.addDay(tbl_start, i);
+
+            if (date <= tbl_end) {
+                thStr.push('<th title="' + date + '">' + date.split('-')[2] + '</th>');
+            } else {
+                break;
+            }
+        }
+
+        // 组织td
+        for (i = 0; i < len; i++) {
+            var same = 0,
+                start = tbl_start,
+                end = tbl_end;
+
+            tdStr.push('<tr data-guide-id="' + guides[i].guideId + '"><td>' + guides[i].guideName + '</td>');
+            for (var j = 0, jLen = guides[i].guideArrangeList.length, tmp, days; j < jLen; j++) {
+
+                tmp = guides[i].guideArrangeList[j];
+                tdStr.push((new Array(Tools.getDateDiff(tmp.startTime, start, true) + 1)).join('<td></td>'));
+
+                tdStr.push('<td colspan="' + (Tools.getDateDiff(tmp.startTime, tmp.endTime, true) + 1) + '" title="' + tmp.startTime + '-' + tmp.endTime + '" class="T-tranplan cursor" data-id="' + tmp.tripPlanId + '" data-type="' + tmp.tripPlanType + '" style="background-color:' + GuideArrange.getBackGroundColor(tmp.tripPlanId) + ';color:#fff;">' + tmp.lineProductName + '</td>');
+                start = Tools.addDay(tmp.endTime, 1);
+            }
+
+            tdStr.push((new Array(Tools.getDateDiff(tmp.endTime, end, true) + 1)).join('<td></td>'));
+
+            tdStr.push('</tr>');
+        }
+
+        return ListTemplate({
+            total: total,
+            thStr: thStr.join(''),
+            tdStr: tdStr.join(''),
+        });
+    };
+
+    /**
+     * 获取背景颜色
+     * @param  {int} id 发团ID
+     * @return {string}    颜色值
+     */
+    GuideArrange.getBackGroundColor = function(id) {
+        if (!colorList[id]) {
+            colorList[id] = GuideArrange.getDiffColor(JSON.stringify(colorList));
+        }
+
+        return colorList[id];
+    };
+
+    /**
+     * 获取不同的颜色
+     * @param  {array} colors [description]
+     * @return {[type]}        [description]
+     */
+    GuideArrange.getDiffColor = function(colors) {
+        var color;
+        do {
+            color = '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
+            if (colors.indexOf(color) < 0) {
+                break;
+            }
+        } while (true);
+
+        return color;
+    };
+
+    /**
+     * 获取最大时间与最小时间
+     * @param  {object} item 安排对象
+     * @return {[type]}      [description]
+     */
+    GuideArrange._fetchRange = function(item) {
+        if (!tbl_start || tbl_start > item.startTime) {
+            tbl_start = item.startTime;
+        }
+
+        if (!tbl_end || tbl_end < item.endTime) {
+            tbl_end = item.endTime;
+        }
+    };
+
+    /**
+     * 绑定下拉菜单
+     * @param  {object} $tab 顶层元素
+     * @return {[type]}      [description]
+     */
+    GuideArrange.initSelect = function($tab) {
+        $tab.find('.T-guideName').autocomplete({
+            minLength: 0,
+            change: function(event, ui) {
+                if (ui.item == null) {
+                    $(this).closest('div').find('input[name="guideId"]').val('');
+                }
+            },
+            select: function(event, ui) {
+                $(this).blur();
+                $(this).closest('div').find('input[name="guideId"]').val(ui.item.id);
+            }
+        }).off("click").on("click", function() {
+            var obj = this;
+            $.ajax({
+                url: KingServices.build_url("guide", "findAll"),
+                type: 'POST',
+                data: {
+                    menuKey: "resource_guide"
+                },
+                showLoading: false,
+                success: function(data) {
+                    var result = showDialog(data);
+                    if (result) {
+                        var guideList = JSON.parse(data.guideList);
+                        if (guideList != null && guideList.length > 0) {
+                            for (var i = 0; i < guideList.length; i++) {
+                                guideList[i].value = guideList[i].realname;
+                            }
+                        }
+                        $(obj).autocomplete('option', 'source', guideList);
+                        $(obj).autocomplete('search', '');
+                    }
+                }
+            });
+        });
+    }
+    exports.init = GuideArrange.initMain;
 });
--- js/template/arrange/guide/view/list.js
+++ js/template/arrange/guide/view/list.js
@@ -1,10 +1,12 @@
-/*TMODJS:{"debug":true,"version":4,"md5":"01024c521b42fb8263ed3867555972a1"}*/
+/*TMODJS:{"debug":true,"version":10,"md5":"46b5e16647da30f2c0f15c3bdf3f0f59"}*/
 define(function(require) {
     return require("../../../template")("arrange/guide/view/list", function($data, $filename) {
         try {
-            var $utils = this, $line = ($utils.$helpers, 0), $escape = $utils.$escape, totalPage = $data.totalPage, $out = "";
-            return $out += '<table class="table table-striped table-bordered table-hover T-showHighLight"> <thead> <th class="T-name-header">姓名</th> </thead> <tbody class="T-list"></tbody> </table> <div class="row"> <div class="col-md-5">共计 ', 
-            $line = 9, $out += $escape(totalPage), $out += ' 条记录</div> <div class="col-md-7"> <div class="T-pagenation"></div> </div> </div> ', 
+            var $utils = this, $line = ($utils.$helpers, 0), $string = $utils.$string, thStr = $data.thStr, tdStr = $data.tdStr, $escape = $utils.$escape, total = $data.total, $out = "";
+            return $out += '<table class="table table-bordered"> <thead> <tr class="bg-blur T-tr-fixed"> <th class="T-name-header">姓名</th> ', 
+            $line = 5, $out += $string(thStr), $out += ' </tr> </thead> <tbody class="T-list"> ', 
+            $line = 9, $out += $string(tdStr), $out += ' </tbody> </table> <div class="row"> <div class="col-md-5">共计 ', 
+            $line = 14, $out += $escape(total), $out += ' 条记录</div> <div class="col-md-7"> <div class="T-pagenation"></div> </div> </div> ', 
             new String($out);
         } catch (e) {
             throw {
@@ -12,7 +14,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '<table class="table table-striped table-bordered table-hover T-showHighLight">\r\n	<thead>\r\n		<th class="T-name-header">姓名</th>\r\n	</thead>\r\n	<tbody class="T-list"></tbody>\r\n</table>\r\n\r\n<div class="row">\r\n	<div class="col-md-5">共计 {{totalPage}} 条记录</div>\r\n	<div class="col-md-7">\r\n		<div class="T-pagenation"></div>\r\n	</div>\r\n</div>\r\n'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '<table class="table table-bordered">\r\n	<thead>\r\n		<tr class="bg-blur T-tr-fixed">\r\n			<th class="T-name-header">姓名</th>\r\n			{{#thStr}}\r\n		</tr>\r\n	</thead>\r\n	<tbody class="T-list">\r\n		{{#tdStr}}\r\n	</tbody>\r\n</table>\r\n\r\n<div class="row">\r\n	<div class="col-md-5">共计 {{total}} 条记录</div>\r\n	<div class="col-md-7">\r\n		<div class="T-pagenation"></div>\r\n	</div>\r\n</div>\r\n'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
