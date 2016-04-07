a3a9b54f31bcc14d112a0d9254197bd4b012c668
roger <guiyun_wei@163.com>
2016-04-07 12:40:23 +0800

    【导游排期】联调

--- js/template/arrange/guide/guide.js
+++ js/template/arrange/guide/guide.js
@@ -1,5 +1,6 @@
 define(function(require, exports) {
 	var MainTemplate = require('./view/listMain'),
+		ListTemplate = require('./view/list'),
 		menuKey = 'guide_arrange_table';
 
 
@@ -16,17 +17,18 @@ define(function(require, exports) {
 	 // 绑定首页事件
 	 GuideArrange.init_event = function() {
 	 	var $tab = $('#tab-'+ menuKey + '-content'),
-	 		$searchArea = $tab.find('.T-search-area');
+	 		$searchArea = $tab.find('.T-search-area'),
+	 		$form = $searchArea.find('form');
 
  		GuideArrange.initSelect($tab);
 
  		Tools.setDatePicker($searchArea.find('.datepicker'), true);
 
  		// search
- 		$searchArea.find('.T-serach').on('click', function(event) {
+ 		$searchArea.find('.T-search').on('click', function(event) {
  			event.preventDefault();
- 			GuideArrange.initGuideList();
- 		});
+ 			GuideArrange.initGuideList($form);
+ 		}).trigger('click');
 
  		GuideArrange.$tab = $tab;
 	 };
@@ -35,180 +37,227 @@ define(function(require, exports) {
 	  * 初始化安排表
 	  * @return {[type]}      [description]
 	  */
-	 GuideArrange.initGuideList = function(page) {
-	 	var args = $tab.find('form').serializeJson();
+	 GuideArrange.initGuideList = function($form, page) {
+	 	var args = $form.serializeJson();
 
-	 	args.page = page || 0;
+	 	args.pageNo = page || 0;
 	 	$.ajax({
-	 		url: KingServices.build_url('', ''),
+	 		url: KingServices.build_url('guideDate', 'findPager'),
 	 		type: 'get',
 	 		data: args,
 	 	})
 	 	.done(function(data) {	 		
+	 		if (showDialog(data)) {
+	 			// 加载表格框架
+	 			$form.parent().next().html(ListTemplate(data.searchParam.totalCount));
 
-	 		laypage({
-	 		    cont: $tab.find('.T-pagenation'), //容器。值支持id名、原生dom对象，jquery对象,
-	 		    pages: data.searchParam.totalPage, //总页数
-	 		    curr: (page + 1),
-	 		    jump: function(obj, first) {
-	 		        if (!first) {  // 避免死循环，第一次进入，不调用页面方法
-	 		            GuideArrange.initGuideList(obj.curr -1);
-	 		        };
-	 		    }
-	 		});
+ 				GuideArrange.loadList(data.guideList);
+
+		 		laypage({
+		 		    cont: GuideArrange.$tab.find('.T-pagenation'), //容器。值支持id名、原生dom对象，jquery对象,
+		 		    pages: data.searchParam.totalPage, //总页数
+		 		    curr: (args.pageNo + 1),
+		 		    jump: function(obj, first) {
+		 		        if (!first) {  // 避免死循环，第一次进入，不调用页面方法
+		 		            GuideArrange.initGuideList($form, obj.curr -1);
+		 		        };
+		 		    }
+		 		});
+	 		}
 	 	});
 	 };
 
-	 var $table = $('#guide-arrange'),
-	 	tbl_start = 0, tbl_end = 0,
-	 	// 储存已经添加的名称
-	 	nameList = [];
+	 var list, $table, tbl_start, tbl_end;
+	 GuideArrange.loadList = function(guides) {
+	 	// 初始化数据
+	 	$table = GuideArrange.$tab.find('table'),
+	 	tbl_start = tbl_end = 0;
 
-	 function pushItem(info) {
-	 	appendTable(info);
-	 	addLine(info);
+ 		for (var i = 0, len = guides.length; i < len; i ++) {
+ 			var str = [], innerTd = [];
+
+ 			for (var j = 0, jLen = guides[i].guideArrangeList.length, tmp; j < jLen; j ++ ) {
+ 				guides[i].guideArrangeList.sort(function(x, y) {
+ 					if (!tbl_start || tbl_start > x.startTime) {
+ 						tbl_start = x.startTime;
+ 					}
+
+ 					if (!tbl_end || tbl_end < x.endTime) {
+ 						tbl_end = x.endTime;
+ 					}
+
+ 					if (!tbl_start || tbl_start > y.startTime) {
+ 						tbl_start = y.startTime;
+ 					}
+
+ 					if (!tbl_end || tbl_end < y.endTime) {
+ 						tbl_end = y.endTime;
+ 					}
+ 					return (x.startTime > y.startTime)? 1: -1;
+ 				});
+ 			}
+
+ 			console.info(tbl_start)
+ 			console.info(tbl_end)
+			// innerTd.push('<td class="T-tranplan" data-id="'+ tmp.tripPlanId +'">'+ tmp.lineProductName +'</td>')
+ 			// str.push('<tr><td>'+ guides[i].name +'</td>');
+
+ 			// str.push('</tr>');
+ 		}
 	 }
+	 // function pendTab() {
+	 // 	var $table = $('')
+	 // }
+	 // var $table = $('#guide-arrange'),
+	 // 	tbl_start = 0, tbl_end = 0,
+	 // 	// 储存已经添加的名称
+	 // 	nameList = [];
 
-	 function appendTable(info) {
-	 	var start = info.startTime,
-	 		end = info.endTime,
-	 		left = 0,
-	 		right = 0,
-	 		dayList = [start, end, tbl_start, tbl_end].sort(),
-	 		right_start = tbl_end;
+	 // function pushItem(info) {
+	 // 	appendTable(info);
+	 // 	addLine(info);
+	 // }
 
-	 	if (tbl_start === 0)  {
-	 		tbl_start = start;
-	 		tbl_end = end;
-	 		left = getDay(start, end) +1;
-	 	} else {
-	 		switch(dayList.indexOf(end))  {
-	 			// 最后一个的位置
-	 			case 1: 		// 全部左侧
-	 				left = getDay(start, tbl_start);
-	 				tbl_start = start;
-	 				break;
-	 			case 2: 		// 左侧在左
-	 				if (start < tbl_start) {
-	 					left = getDay(start, tbl_start);
-	 					tbl_start = start;
-	 				}
-	 				break;
-	 			case 3:
-	 				if (start < tbl_start) {  // 左右侧
-	 					left = getDay(start, tbl_start);
-	 					tbl_start = start;
-	 					rgith = getDay(tbl_end, end);
-	 				} else if (start > tbl_end) { // 左侧在内部
-	 					right = getDay(tbl_end, end);
-	 					tbl_end = end;
-	 				} else if (start <= tbl_end) {  // 左侧在外部
-	 					right = getDay(start, end);
-	 					tbl_end = end;
-	 					tbl_start = start;							
-	 				}
-	 				break;
-	 			default:
-	 				return;
-	 		}
-	 	}
+	 // function appendTable(info) {
+	 // 	var start = info.startTime,
+	 // 		end = info.endTime,
+	 // 		left = 0,
+	 // 		right = 0,
+	 // 		dayList = [start, end, tbl_start, tbl_end].sort(),
+	 // 		right_start = tbl_end;
 
-	 	if (left) {
-	 		var str = [], tds = [];
-	 		for (var i = 0, day; i < left; i ++) {
-	 			day = addDay(tbl_start, i);
-	 			str.push('<th class="'+ day + '">'+ day.split('-')[2] +'</th>');
-	 			tds.push('<td></td>');
-	 		}
-	 		$(str.join('')).insertAfter($table.find('.name-header'));
+	 // 	if (tbl_start === 0)  {
+	 // 		tbl_start = start;
+	 // 		tbl_end = end;
+	 // 		left = Tools.getDateDiff(start, end) +1;
+	 // 	} else {
+	 // 		switch(dayList.indexOf(end))  {
+	 // 			// 最后一个的位置
+	 // 			case 1: 		// 全部左侧
+	 // 				left = Tools.getDateDiff(start, tbl_start);
+	 // 				tbl_start = start;
+	 // 				break;
+	 // 			case 2: 		// 左侧在左
+	 // 				if (start < tbl_start) {
+	 // 					left = Tools.getDateDiff(start, tbl_start);
+	 // 					tbl_start = start;
+	 // 				}
+	 // 				break;
+	 // 			case 3:
+	 // 				if (start < tbl_start) {  // 左右侧
+	 // 					left = Tools.getDateDiff(start, tbl_start);
+	 // 					tbl_start = start;
+	 // 					rgith = Tools.getDateDiff(tbl_end, end);
+	 // 				} else if (start > tbl_end) { // 左侧在内部
+	 // 					right = Tools.getDateDiff(tbl_end, end);
+	 // 					tbl_end = end;
+	 // 				} else if (start <= tbl_end) {  // 左侧在外部
+	 // 					right = Tools.getDateDiff(start, end);
+	 // 					tbl_end = end;
+	 // 					tbl_start = start;							
+	 // 				}
+	 // 				break;
+	 // 			default:
+	 // 				return;
+	 // 		}
+	 // 	}
 
-	 		tds = tds.join('');
-	 		$table.find('#arrange-list').children('tr').each(function() {
-	 			$(tds).insertAfter($(this).find('.name'));
-	 		})
-	 	}
+	 // 	if (left) {
+	 // 		var str = [], tds = [];
+	 // 		for (var i = 0, day; i < left; i ++) {
+	 // 			day = addDay(tbl_start, i);
+	 // 			str.push('<th class="'+ day + '">'+ day.split('-')[2] +'</th>');
+	 // 			tds.push('<td></td>');
+	 // 		}
+	 // 		$(str.join('')).insertAfter($table.find('.name-header'));
 
-	 	if (right++) {
-	 		var str = [], tds = [];
-	 		for (var i = 1, day; i < right; i ++) {
-	 			day = addDay(right_start, i);
-	 			str.push('<th class="'+ day + '">'+ day.split('-')[2] +'</th>');
-	 			tds.push('<td></td>');
-	 		}
-	 		$table.find('thead>tr').append(str.join(''));
+	 // 		tds = tds.join('');
+	 // 		$table.find('#arrange-list').children('tr').each(function() {
+	 // 			$(tds).insertAfter($(this).find('.name'));
+	 // 		})
+	 // 	}
 
-	 		tds = tds.join('');
-	 		$table.find('#arrange-list').children('tr').each(function() {
-	 			$(this).append(tds);
-	 		})
-	 	}
-	 }
+	 // 	if (right++) {
+	 // 		var str = [], tds = [];
+	 // 		for (var i = 1, day; i < right; i ++) {
+	 // 			day = addDay(right_start, i);
+	 // 			str.push('<th class="'+ day + '">'+ day.split('-')[2] +'</th>');
+	 // 			tds.push('<td></td>');
+	 // 		}
+	 // 		$table.find('thead>tr').append(str.join(''));
 
-	 function addLine(info) {
-	 	var index = nameList.indexOf(info.name),
-	 		start = info.startTime,
-	 		end = info.endTime,
-	 		days = getDay(start, end) + 1;
+	 // 		tds = tds.join('');
+	 // 		$table.find('#arrange-list').children('tr').each(function() {
+	 // 			$(this).append(tds);
+	 // 		})
+	 // 	}
+	 // }
 
-	 	if (index >= 0) {
-	 		var $tr = $table.find('#arrange-list').children('tr').eq(index), 
-	 			$th = $table.find('thead').find('.' + start);
+	 // function addLine(info) {
+	 // 	var index = nameList.indexOf(info.name),
+	 // 		start = info.startTime,
+	 // 		end = info.endTime,
+	 // 		days = Tools.getDateDiff(start, end) + 1;
 
-	 		if ($th.length) {
-	 			var i = $th.index(), $tds = $tr.children('td'), 
-	 				len = $tds.length -1,
-	 				$td,
-	 				left = $th.position().left;
+	 // 	if (index >= 0) {
+	 // 		var $tr = $table.find('#arrange-list').children('tr').eq(index), 
+	 // 			$th = $table.find('thead').find('.' + start);
 
-	 			if (len < i) {
-	 				i = len;
-	 			}
-	 			while(i >= 0) {
-	 				$td = $tds.eq(i);
-	 				if (left === $td.position().left) {
-	 					break;
-	 				}
-	 				i --;
-	 			}
+	 // 		if ($th.length) {
+	 // 			var i = $th.index(), $tds = $tr.children('td'), 
+	 // 				len = $tds.length -1,
+	 // 				$td,
+	 // 				left = $th.position().left;
 
-	 			if (i >= 0) {
-	 				// days ++;
-	 				var $next = $td.next();
-	 				$td[0].colSpan = days;
-	 				$td.text(info.lineProduct).addClass('bg-blue');
+	 // 			if (len < i) {
+	 // 				i = len;
+	 // 			}
+	 // 			while(i >= 0) {
+	 // 				$td = $tds.eq(i);
+	 // 				if (left === $td.position().left) {
+	 // 					break;
+	 // 				}
+	 // 				i --;
+	 // 			}
 
-	 				for (var i = 1, $next, $tmp; i < days && $next.length; i ++ ) {
-	 					$tmp = $next;
-	 					$next = $next.next();
-	 					$tmp.remove();
-	 				}
-	 			}
-	 		}
-	 	} else {
-	 		var str = [], len;
+	 // 			if (i >= 0) {
+	 // 				// days ++;
+	 // 				var $next = $td.next();
+	 // 				$td[0].colSpan = days;
+	 // 				$td.text(info.lineProduct).addClass('bg-blue');
 
-	 		// name
-	 		str.push('<tr><td class="name">'+ info.name + '</td>');
+	 // 				for (var i = 1, $next, $tmp; i < days && $next.length; i ++ ) {
+	 // 					$tmp = $next;
+	 // 					$next = $next.next();
+	 // 					$tmp.remove();
+	 // 				}
+	 // 			}
+	 // 		}
+	 // 	} else {
+	 // 		var str = [], len;
 
-	 		// start 
-	 		len = getDay(tbl_start, start);
-	 		if (len) {
-	 			str.push(new Array(len + 1).join('<td></td>'));
-	 		}
+	 // 		// name
+	 // 		str.push('<tr><td class="name">'+ info.name + '</td>');
 
-	 		str.push('<td class="bg-blue" colspan="'+ days +'">'+ info.lineProduct +'</td>');
+	 // 		// start 
+	 // 		len = Tools.getDateDiff(tbl_start, start);
+	 // 		if (len) {
+	 // 			str.push(new Array(len + 1).join('<td></td>'));
+	 // 		}
 
-	 		// end
-	 		len = getDay(end, tbl_end);
-	 		if (len) {
-	 			str.push(new Array(len + 1).join('<td></td>'));
-	 		}
-	 		str.push('</tr>');
+	 // 		str.push('<td class="bg-blue" colspan="'+ days +'">'+ info.lineProduct +'</td>');
 
-	 		nameList.push(info.name);
-	 		$('#arrange-list').append(str.join(''));
-	 	}
-	 }
+	 // 		// end
+	 // 		len = Tools.getDateDiff(end, tbl_end);
+	 // 		if (len) {
+	 // 			str.push(new Array(len + 1).join('<td></td>'));
+	 // 		}
+	 // 		str.push('</tr>');
+
+	 // 		nameList.push(info.name);
+	 // 		$('#arrange-list').append(str.join(''));
+	 // 	}
+	 // }
 
 	 /**
 	  * 绑定下拉菜单
--- js/template/arrange/guide/view/list.js
+++ js/template/arrange/guide/view/list.js
@@ -1,10 +1,10 @@
-/*TMODJS:{"debug":true,"version":1,"md5":"ccf3df15d17972994a7526ef7596a5e5"}*/
+/*TMODJS:{"debug":true,"version":4,"md5":"01024c521b42fb8263ed3867555972a1"}*/
 define(function(require) {
     return require("../../../template")("arrange/guide/view/list", function($data, $filename) {
         try {
-            var $utils = this, $line = ($utils.$helpers, 0), $escape = $utils.$escape, searchParam = $data.searchParam, $out = "";
-            return $out += '<div> <table class="table table-striped table-bordered table-hover T-showHighLight"> <thead> <th class="T-name-header">姓名</th> </thead> <tbody class="T-list"></tbody> </table> <div class="row"> <div class="col-md-5">共计 ', 
-            $line = 10, $out += $escape(searchParam.totalPage), $out += ' 条记录</div> <div class="col-md-7"> <div class="T-pagenation"></div> </div> </div> </div>', 
+            var $utils = this, $line = ($utils.$helpers, 0), $escape = $utils.$escape, totalPage = $data.totalPage, $out = "";
+            return $out += '<table class="table table-striped table-bordered table-hover T-showHighLight"> <thead> <th class="T-name-header">姓名</th> </thead> <tbody class="T-list"></tbody> </table> <div class="row"> <div class="col-md-5">共计 ', 
+            $line = 9, $out += $escape(totalPage), $out += ' 条记录</div> <div class="col-md-7"> <div class="T-pagenation"></div> </div> </div> ', 
             new String($out);
         } catch (e) {
             throw {
@@ -12,7 +12,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '<div>\r\n	<table class="table table-striped table-bordered table-hover T-showHighLight">\r\n		<thead>\r\n			<th class="T-name-header">姓名</th>\r\n		</thead>\r\n		<tbody class="T-list"></tbody>\r\n	</table>\r\n\r\n	<div class="row">\r\n		<div class="col-md-5">共计 {{searchParam.totalPage}} 条记录</div>\r\n		<div class="col-md-7">\r\n			<div class="T-pagenation"></div>\r\n		</div>\r\n	</div>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '<table class="table table-striped table-bordered table-hover T-showHighLight">\r\n	<thead>\r\n		<th class="T-name-header">姓名</th>\r\n	</thead>\r\n	<tbody class="T-list"></tbody>\r\n</table>\r\n\r\n<div class="row">\r\n	<div class="col-md-5">共计 {{totalPage}} 条记录</div>\r\n	<div class="col-md-7">\r\n		<div class="T-pagenation"></div>\r\n	</div>\r\n</div>\r\n'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
