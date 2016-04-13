89b8bb96704a94fce6d6658ab84d41553cfd87c7
guiyun <wei_guiyun@163.com>
2016-04-13 18:54:18 +0800

    【导游排期】解决未安排多一列的问题

--- js/template/arrange/guide/guide.js
+++ js/template/arrange/guide/guide.js
@@ -128,7 +128,11 @@ define(function(require, exports) {
 
         // 组织td
         var start, end, newList, same,
+            blankLine = '';
+
+        if (tbl_end != 0 && tbl_start != 0) {
             blankLine = (new Array(Tools.getDateDiff(tbl_start, tbl_end, true) + 2)).join('<td></td>');
+        }
 
         for (i = 0; i < len; i++) {
             // 重新组织安排列表，处理日期重复的情况
