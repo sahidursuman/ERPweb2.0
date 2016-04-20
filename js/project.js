eddcef9cd1ba1613751d02eb9ba68519bf38a932
wenJoy <laojiaodaoniu@126.com>
2016-04-20 11:33:35 +0800

    报账审核--所有模块标题增加字体颜色

--- js/project.js
+++ js/project.js
@@ -2037,7 +2037,7 @@ Tools.setDateHSPicker = function($obj,className){
     $obj.find('.' + className).datetimepicker({
         autoclose: true,
         todayHighlight: true,
-        format: 'YYYY-MM-DD HH:mm',
+        format: 'L',
         language: 'zh-CN'
        });  
 };
