729445f0479151438bc18cfe633e8b73a38a9713
xiongzuyao <x@xiongzy.com>
2016-04-22 19:35:14 +0800

    代订安排、代订订单开发

--- js/template/arrange/arrangeCenter/bookingView/scenceView.js
+++ js/template/arrange/arrangeCenter/bookingView/scenceView.js
@@ -1,4 +1,38 @@
-/*TMODJS:{"debug":true,"version":1,"md5":"32a6f1f5a580efee593549d99e2d42a1"}*/
+/*TMODJS:{"debug":true,"version":5,"md5":"24a4c8cec07b3fb5502064f8a6f04774"}*/
 define(function(require) {
-    return require("/js/template/template")("arrange/arrangeCenter/bookingView/scenceView", "");
+    return require("../../../template")("arrange/arrangeCenter/bookingView/scenceView", function($data, $filename) {
+        try {
+            var $utils = this, $helpers = $utils.$helpers, $line = 0, $escape = $utils.$escape, bookingOrder = $data.bookingOrder, required = $data.required, arrangeList = $data.arrangeList, $each = $utils.$each, status = ($data.rs, 
+            $data.$index, $data.status), $out = "";
+            return $out += '<div class="base-info" data-id="', $line = 1, $out += $escape(bookingOrder.id), 
+            $out += '"> 代订单号：<span class="mar-r-20">', $line = 2, $out += $escape(bookingOrder.orderNumber), 
+            $out += '</span> 客人信息： <span class="mar-r-20">', $line = 3, $out += $escape(bookingOrder.touristRealname), 
+            $out += " ", $line = 3, $out += $escape(bookingOrder.adultCount), $out += "大", $line = 3, 
+            $out += $escape(bookingOrder.childCount), $out += "小</span> 外联销售：", $line = 3, $out += $escape(bookingOrder.outOPUserName), 
+            $out += ' </div> <div class=" ui-sortable-handle"> <h5 class=""> <label class="middle title-size">景区</label> </h5> <p class="panel hct-mh-required">现景区计划要求：', 
+            $line = 9, $out += $escape(required), $out += '</p> <div class="widget-body"> <div class="widget-main no-padding"> <table class="table table-striped table-bordered table-hover margin-top" style="border: 1px solid #ccc"> <thead> <tr> <th class="th-border" width="110"><span class="necessary">*</span>日期</th> <th class="th-border" width="70">时段</th> <th class="th-border"><span class="necessary">*</span>景区</th> <th class="th-border"><span class="necessary">*</span>收费项目</th> <th class="th-border" width="60">时长</th> <th class="th-border" width="80">单价</th> <th class="th-border" width="80">数量</th> <th class="th-border" width="80">优惠</th> <th class="th-border">订单号</th> <th class="th-border" width="80">应付</th> <th class="th-border" width="80">预付款</th> <th class="th-border">备注</th> <th class="th-border" width="60">操作</th> </tr> </thead> <tbody class="T-scenicList"> ', 
+            $line = 31, arrangeList && arrangeList.length > 0 && ($out += " ", $line = 32, $each(arrangeList, function(rs) {
+                $out += ' <tr data-id="', $line = 33, $out += $escape(rs.id), $out += '"> <td>', 
+                $line = 34, $out += $escape($helpers.dateFormat(rs.startTime, "yyyy-MM-dd")), $out += "</td> <td>", 
+                $line = 35, $out += $escape(rs.tourTime), $out += "</td> <td>", $line = 36, $out += $escape(rs.scenicName), 
+                $out += "</td> <td>", $line = 37, $out += $escape(rs.scenicItemName), $out += "</td> <td>", 
+                $line = 38, $out += $escape(rs.tourDuration), $out += "</td> <td>", $line = 39, 
+                $out += $escape(rs.costPrice), $out += "</td> <td>", $line = 40, $out += $escape(rs.roomCount), 
+                $out += "</td> <td>", $line = 41, $out += $escape(rs.reduceMoney), $out += "</td> <td>", 
+                $line = 42, $out += $escape(rs.orderNumber), $out += "</td> <td>", $line = 43, $out += $escape(rs.sumCostMoney), 
+                $out += "</td> <td>", $line = 44, $out += $escape(rs.prePayMoney), $out += "</td> <td>", 
+                $line = 45, $out += $escape(rs.remark), $out += "</td> </tr> ", $line = 47;
+            }), $out += " ", $line = 48), $out += ' </tbody> </table> </div> </div> </div> <div class="text-center mar-t-20"> <span class="checkbox checkbox-inline mar-0" data-target="#booking_receive_scence"> <button class="btn btn-xs btn-default T-btn-close btn-primary w100 mar-r-20"> <i class="ace-icon fa fa-times"></i> 关闭 </button> <label> <input name="status" type="checkbox" class="ace T-finishedArrange" disabled ', 
+            $line = 59, 1 == status && ($out += "checked", $line = 59), $out += '> <span class="lbl font-w"> 景区安排完成</span> </label> </span> </div>', 
+            new String($out);
+        } catch (e) {
+            throw {
+                filename: $filename,
+                name: "Render Error",
+                message: e.message,
+                line: $line,
+                source: '<div class="base-info" data-id="{{bookingOrder.id}}">\r\n    代订单号：<span class="mar-r-20">{{bookingOrder.orderNumber}}</span> 客人信息：\r\n    <span class="mar-r-20">{{bookingOrder.touristRealname}} {{bookingOrder.adultCount}}大{{bookingOrder.childCount}}小</span> 外联销售：{{bookingOrder.outOPUserName}}\r\n</div>\r\n<div class=" ui-sortable-handle">\r\n    <h5 class="">\r\n        <label class="middle title-size">景区</label>\r\n    </h5>\r\n    <p class="panel hct-mh-required">现景区计划要求：{{required}}</p>\r\n    <div class="widget-body">\r\n        <div class="widget-main no-padding">\r\n            <table class="table table-striped table-bordered table-hover margin-top" style="border: 1px solid #ccc">\r\n                <thead>\r\n                    <tr>\r\n                        <th class="th-border" width="110"><span class="necessary">*</span>日期</th>\r\n                        <th class="th-border" width="70">时段</th>\r\n                        <th class="th-border"><span class="necessary">*</span>景区</th>\r\n                        <th class="th-border"><span class="necessary">*</span>收费项目</th>\r\n                        <th class="th-border" width="60">时长</th>\r\n                        <th class="th-border" width="80">单价</th>\r\n                        <th class="th-border" width="80">数量</th>\r\n                        <th class="th-border" width="80">优惠</th>\r\n                        <th class="th-border">订单号</th>\r\n                        <th class="th-border" width="80">应付</th>\r\n                        <th class="th-border" width="80">预付款</th>\r\n                        <th class="th-border">备注</th>\r\n                        <th class="th-border" width="60">操作</th>\r\n                    </tr>\r\n                </thead>\r\n                <tbody class="T-scenicList">\r\n                {{if !!arrangeList && arrangeList.length > 0}}\r\n                {{each arrangeList as rs}}\r\n                    <tr data-id="{{rs.id}}">\r\n                        <td>{{rs.startTime | dateFormat: \'yyyy-MM-dd\'}}</td>\r\n                        <td>{{rs.tourTime}}</td>\r\n                        <td>{{rs.scenicName}}</td>\r\n                        <td>{{rs.scenicItemName}}</td>\r\n                        <td>{{rs.tourDuration}}</td>\r\n                        <td>{{rs.costPrice}}</td>\r\n                        <td>{{rs.roomCount}}</td>\r\n                        <td>{{rs.reduceMoney}}</td>\r\n                        <td>{{rs.orderNumber}}</td>\r\n                        <td>{{rs.sumCostMoney}}</td>\r\n                        <td>{{rs.prePayMoney}}</td>\r\n                        <td>{{rs.remark}}</td>\r\n                    </tr>\r\n                {{/each}}\r\n                {{/if}}\r\n                </tbody>\r\n            </table>\r\n        </div>\r\n    </div>\r\n</div>\r\n\r\n<div class="text-center mar-t-20">\r\n    <span class="checkbox checkbox-inline mar-0" data-target="#booking_receive_scence">\r\n        <button class="btn btn-xs btn-default T-btn-close btn-primary w100 mar-r-20"> <i class="ace-icon fa fa-times"></i> 关闭 </button>\r\n        <label>\r\n            <input name="status" type="checkbox" class="ace T-finishedArrange" disabled {{if status == 1}}checked{{/if}}>\r\n            <span class="lbl font-w"> 景区安排完成</span>\r\n        </label>\r\n    </span>\r\n</div>'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+            };
+        }
+    });
 });
