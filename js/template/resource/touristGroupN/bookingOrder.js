e866e40b513772b9ccdf03849c5aaa1c35aa19eb
wenJoy <laojiaodaoniu@126.com>
2016-05-19 13:51:36 +0800

    B8833 【订单中心】代订订单，客户无联系人的，代订结算单不显示（）. B8832 【订单中心】代订订单，代订结算单去掉备注

--- js/template/resource/touristGroupN/bookingOrder.js
+++ js/template/resource/touristGroupN/bookingOrder.js
@@ -214,7 +214,7 @@ define(function(require, exports, module) {
         }).done(function(data){
             if(showDialog(data)){
                 var partnerAgencyName = data.bookingOrder.partnerAgencyName;
-                if(data.bookingOrder.contactRealname != null){
+                if(data.bookingOrder.contactRealname != null &&  data.bookingOrder.contactRealname != ""){
                     partnerAgencyName = data.bookingOrder.partnerAgencyName + "（"+ data.bookingOrder.contactRealname +"）";
                 }
                 data.bookingOrder.partnerAgencyName = partnerAgencyName;
