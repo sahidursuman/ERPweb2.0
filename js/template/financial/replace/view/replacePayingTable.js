c700cb8dbea78c255cce813f02d3da8e2ccd8012
liaojialing <609095033@qq.com>
2016-06-02 10:08:30 +0800

    b#9085:1280分辨率下，对账后面的查看按钮看不见了。

--- js/template/financial/replace/view/replacePayingTable.js
+++ js/template/financial/replace/view/replacePayingTable.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":48,"md5":"54ec92aeb8caea8a7d16788ae989db29"}*/
+/*TMODJS:{"debug":true,"version":51,"md5":"49e0e82d91fcaad7c03f53209e103b07"}*/
 define(function(require) {
     return require("../../../template")("financial/replace/view/replacePayingTable", function($data, $filename) {
         try {
@@ -17,9 +17,9 @@ define(function(require) {
                 $line = 11, $out += $escape(rs.payMoney), $out += '"></td> <td><textarea class="col-sm-12 hct-textarea T-remark" name="payRemark" maxlength="1000">', 
                 $line = 12, $out += $escape(rs.payRemark), $out += "</textarea></td> <td>", $line = 13, 
                 $out += $escape(rs.checkTime), $out += "</td> <td>", $line = 14, $out += $escape(rs.checkUserName), 
-                $out += '</td> <td> <label class="pos-rel"> <span class="lbl">', $line = 17, rs.isConfirmAccount ? ($out += "已", 
-                $line = 17) : ($out += "未", $line = 17), $out += '对账</span> </label> <label class="cursor" data-right="1290004"> <a> |</a></label> <a class="cursor T-action T-view-Received" data-right="1290004">查看</a> </td> </tr> ', 
-                $line = 23;
+                $out += "</td> <td> ", $line = 16, rs.isConfirmAccount ? ($out += "已", $line = 16) : ($out += "未", 
+                $line = 16), $out += '对账 <label class="cursor" data-right="1290004"> <a> |</a></label> <a class="cursor T-action T-view-Received" data-right="1290004">查看</a> </td> </tr> ', 
+                $line = 21;
             }), new String($out);
         } catch (e) {
             throw {
@@ -27,7 +27,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '{{each bookinAccountList as rs index}}\r\n    <tr data-id="{{rs.id}}">\r\n        <td>{{rs.orderNumber}}</td>\r\n        <td>{{rs.startTime}}</td>\r\n        <td>{{rs.projectName}}</td>\r\n        <td class="T-ctrl-tip" title="{{#rs.newDetail}}" ><span style="height:35px;">{{#rs.newDetail}}</span></td>\r\n        <td><span class="F-float F-money">{{rs.bookingMoney}}</span></td>\r\n        <td><a class="cursor T-action T-receive-money F-float F-money">{{rs.receiveMoney}}</a></td>\r\n        <td><span class="F-float F-money">{{rs.settlementMoney}}</span></td>\r\n        <td><span class="F-float F-money">{{rs.unReceiveMoney}}</span></td>\r\n        <td><input type="text" class="col-xs-12 T-reciveMoney F-float F-money" name="payMoney" value="{{rs.payMoney}}"></td>\r\n        <td><textarea class="col-sm-12 hct-textarea T-remark" name="payRemark" maxlength="1000">{{rs.payRemark}}</textarea></td>\r\n        <td>{{rs.checkTime}}</td>\r\n        <td>{{rs.checkUserName}}</td>\r\n        <td>\r\n            <label class="pos-rel">\r\n                <span class="lbl">{{if !!rs.isConfirmAccount}}已{{else}}未{{/if}}对账</span>\r\n            </label>\r\n            <label class="cursor" data-right="1290004"> <a> |</a></label>\r\n            <a class="cursor T-action T-view-Received" data-right="1290004">查看</a>\r\n        </td>\r\n    </tr>\r\n{{/each}}'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '{{each bookinAccountList as rs index}}\r\n    <tr data-id="{{rs.id}}">\r\n        <td>{{rs.orderNumber}}</td>\r\n        <td>{{rs.startTime}}</td>\r\n        <td>{{rs.projectName}}</td>\r\n        <td class="T-ctrl-tip" title="{{#rs.newDetail}}" ><span style="height:35px;">{{#rs.newDetail}}</span></td>\r\n        <td><span class="F-float F-money">{{rs.bookingMoney}}</span></td>\r\n        <td><a class="cursor T-action T-receive-money F-float F-money">{{rs.receiveMoney}}</a></td>\r\n        <td><span class="F-float F-money">{{rs.settlementMoney}}</span></td>\r\n        <td><span class="F-float F-money">{{rs.unReceiveMoney}}</span></td>\r\n        <td><input type="text" class="col-xs-12 T-reciveMoney F-float F-money" name="payMoney" value="{{rs.payMoney}}"></td>\r\n        <td><textarea class="col-sm-12 hct-textarea T-remark" name="payRemark" maxlength="1000">{{rs.payRemark}}</textarea></td>\r\n        <td>{{rs.checkTime}}</td>\r\n        <td>{{rs.checkUserName}}</td>\r\n        <td>\r\n            {{if !!rs.isConfirmAccount}}已{{else}}未{{/if}}对账\r\n            <label class="cursor" data-right="1290004"> <a> |</a></label>\r\n            <a class="cursor T-action T-view-Received" data-right="1290004">查看</a>\r\n        </td>\r\n    </tr>\r\n{{/each}}'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
