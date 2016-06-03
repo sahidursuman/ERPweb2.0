581cea0ba6a2690e18a927c205d5abffd51e3638
guiyun <wei_guiyun@163.com>
2016-06-03 17:43:54 +0800

    【订单中心】修正线路产品与客户的选择效果

--- js/template/resource/touristGroupN/view/tourists/choose/chooseLineProductList.js
+++ js/template/resource/touristGroupN/view/tourists/choose/chooseLineProductList.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":9,"md5":"d175b3e3ac2fc02f1031bedea11029a8"}*/
+/*TMODJS:{"debug":true,"version":10,"md5":"859e10c6b9fe165581efa6941a49e9bc"}*/
 define(function(require) {
     return require("../../../../../template")("resource/touristGroupN/view/tourists/choose/chooseLineProductList", function($data, $filename) {
         try {
@@ -10,8 +10,8 @@ define(function(require) {
                 $out += "</td> <td>", $line = 5, $out += $escape($helpers.getCustomerType(rs.customerType, !0)), 
                 $out += '</td> <td name="days">', $line = 6, $out += $escape(rs.days), $out += "</td> <td>", 
                 $line = 7, rs.businessGroup && ($line = 7, $out += $escape(rs.businessGroup.name), 
-                $out += " ", $line = 7), $out += '</td> <td> <input type="radio" name="chooseLineProduct" class="ace"> <label class="lbl"></label> </td> </tr> ', 
-                $line = 13;
+                $out += " ", $line = 7), $out += '</td> <td> <a class="T-action T-chooseLineProduct cursor" name="chooseLineProduct" > <span>选择</span> </a> </td> </tr> ', 
+                $line = 12;
             }), new String($out);
         } catch (e) {
             throw {
@@ -19,7 +19,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '{{each lineProductList as rs}}\r\n<tr data-id="{{rs.id}}">\r\n	<td name="lineProductName">{{rs.name}}</td>\r\n	<td>{{rs.type}}</td>\r\n	<td>{{rs.customerType | getCustomerType:true}}</td>\r\n    <td name="days">{{rs.days}}</td>\r\n    <td>{{if !!rs.businessGroup}}{{rs.businessGroup.name}} {{/if}}</td>\r\n	<td>\r\n		<input type="radio" name="chooseLineProduct" class="ace">\r\n		<label class="lbl"></label>\r\n	</td>\r\n</tr>\r\n{{/each}}'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '{{each lineProductList as rs}}\r\n<tr data-id="{{rs.id}}">\r\n	<td name="lineProductName">{{rs.name}}</td>\r\n	<td>{{rs.type}}</td>\r\n	<td>{{rs.customerType | getCustomerType:true}}</td>\r\n    <td name="days">{{rs.days}}</td>\r\n    <td>{{if !!rs.businessGroup}}{{rs.businessGroup.name}} {{/if}}</td>\r\n	<td>\r\n        <a class="T-action T-chooseLineProduct cursor" name="chooseLineProduct" > <span>选择</span> </a>\r\n	</td>\r\n</tr>\r\n{{/each}}'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
