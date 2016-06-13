3ab4842414b11213201dcf1837d1ed3e4f9636bb
wenJoy <laojiaodaoniu@126.com>
2016-06-13 16:38:55 +0800

    B9284 【单项操作】代订->票，安排界面缺少要求的票务类型 B9289 【代订利润】把应收的超链接去掉。

--- js/template/financial/replaceProfit/view/list.js
+++ js/template/financial/replaceProfit/view/list.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":247,"md5":"4e587e731ad46d89187d7a3b29558e3e"}*/
+/*TMODJS:{"debug":true,"version":248,"md5":"80cb4602facab2486cc11e756879cb29"}*/
 define(function(require) {
     return require("../../../template")("financial/replaceProfit/view/list", function($data, $filename) {
         try {
@@ -19,9 +19,9 @@ define(function(require) {
                 $line = 24) : ($out += " ", $line = 25, $out += $escape(rs.financialScenic), $out += " ", 
                 $line = 26), $out += " </td> <td> ", $line = 29, "" == rs.financialBus ? ($out += " - ", 
                 $line = 31) : ($out += " ", $line = 32, $out += $escape(rs.financialBus), $out += " ", 
-                $line = 33), $out += ' </td> <td><a class="T-option T-needPayDetail F-float F-money">', 
-                $line = 35, $out += $escape(rs.sumSaleCostMoney), $out += '</a></td> <td><a class="T-option T-costDetail F-float F-money">', 
-                $line = 36, $out += $escape(rs.sumCostMoney), $out += '</a></td> <td><span class="F-float F-money">', 
+                $line = 33), $out += " </td> <td>", $line = 35, $out += $escape(rs.sumSaleCostMoney), 
+                $out += '</td> <td><a class="T-option T-costDetail F-float F-money">', $line = 36, 
+                $out += $escape(rs.sumCostMoney), $out += '</a></td> <td><span class="F-float F-money">', 
                 $line = 37, $out += $escape(rs.financialGrossProfit), $out += "</span></td> </tr> ", 
                 $line = 39;
             }), new String($out);
@@ -31,7 +31,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '{{each bookingOrderList as rs}}\r\n<tr data-id="{{rs.id}}">\r\n	<td><a class="T-option T-replaceDetail">{{rs.orderNumber}}</a></td>\r\n	<td>{{if rs.partnerAgency != null}}{{rs.partnerAgency.travelAgencyName}}{{/if}}</td>\r\n	<td>{{if rs.outOPUser}}{{rs.outOPUser.realName}}{{else}}-{{/if}}</td>\r\n	<td>{{if !!rs.businessGroup}}{{rs.businessGroup.name}}{{/if}}-{{if rs.group}}{{rs.group.name}}{{/if}}</td>\r\n	<td>\r\n		{{if rs.financialHotel == ""}}\r\n			-\r\n		{{else}}\r\n			{{rs.financialHotel}}\r\n		{{/if}}\r\n	</td>\r\n	<td>\r\n		{{if rs.financialTicket == ""}}\r\n			-\r\n		{{else}}\r\n			{{rs.financialTicket}}\r\n		{{/if}}\r\n	</td>\r\n	<td>\r\n		{{if rs.financialScenic == ""}}\r\n			-\r\n		{{else}}\r\n			{{rs.financialScenic}}\r\n		{{/if}}\r\n	</td>\r\n	<td>\r\n		{{if rs.financialBus == ""}}\r\n			-\r\n		{{else}}\r\n			{{rs.financialBus}}\r\n		{{/if}}\r\n	</td>\r\n	<td><a class="T-option T-needPayDetail F-float F-money">{{rs.sumSaleCostMoney}}</a></td>\r\n	<td><a class="T-option T-costDetail F-float F-money">{{rs.sumCostMoney}}</a></td>\r\n	<td><span class="F-float F-money">{{rs.financialGrossProfit}}</span></td>\r\n</tr>\r\n{{/each}}'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '{{each bookingOrderList as rs}}\r\n<tr data-id="{{rs.id}}">\r\n	<td><a class="T-option T-replaceDetail">{{rs.orderNumber}}</a></td>\r\n	<td>{{if rs.partnerAgency != null}}{{rs.partnerAgency.travelAgencyName}}{{/if}}</td>\r\n	<td>{{if rs.outOPUser}}{{rs.outOPUser.realName}}{{else}}-{{/if}}</td>\r\n	<td>{{if !!rs.businessGroup}}{{rs.businessGroup.name}}{{/if}}-{{if rs.group}}{{rs.group.name}}{{/if}}</td>\r\n	<td>\r\n		{{if rs.financialHotel == ""}}\r\n			-\r\n		{{else}}\r\n			{{rs.financialHotel}}\r\n		{{/if}}\r\n	</td>\r\n	<td>\r\n		{{if rs.financialTicket == ""}}\r\n			-\r\n		{{else}}\r\n			{{rs.financialTicket}}\r\n		{{/if}}\r\n	</td>\r\n	<td>\r\n		{{if rs.financialScenic == ""}}\r\n			-\r\n		{{else}}\r\n			{{rs.financialScenic}}\r\n		{{/if}}\r\n	</td>\r\n	<td>\r\n		{{if rs.financialBus == ""}}\r\n			-\r\n		{{else}}\r\n			{{rs.financialBus}}\r\n		{{/if}}\r\n	</td>\r\n	<td>{{rs.sumSaleCostMoney}}</td>\r\n	<td><a class="T-option T-costDetail F-float F-money">{{rs.sumCostMoney}}</a></td>\r\n	<td><span class="F-float F-money">{{rs.financialGrossProfit}}</span></td>\r\n</tr>\r\n{{/each}}'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
