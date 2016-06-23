0b9ceb776137e43d8dd1b8a973b5eff9a608eac5
wenJoy <laojiaodaoniu@126.com>
2016-06-23 11:38:04 +0800

    B9431 【报账审核】新增购物，选择购物店后，人数返佣没有展示出来，请检查

--- js/template/financial/count/count.js
+++ js/template/financial/count/count.js
@@ -2322,8 +2322,11 @@ define(function(require, exports){
 			var sum = Count.changeTwoDecimal($(this).val());
 			sumgQuanpMoney += sum;
 		});
-			$sumConsumeMoney.val(sumSGmoney)//金额小计
+		
+		if($obj.attr('name') == "shopGuideMoney"){
 			$showConsumeMoney.text(sumSGmoney);
+			$sumConsumeMoney.val(sumSGmoney)//金额小计
+		}
 		sumMoney = Count.changeTwoDecimal(sumShopMoney);//购物收入
 	        
     	var $mainTable = $parentObj.find('.T-main-table');
