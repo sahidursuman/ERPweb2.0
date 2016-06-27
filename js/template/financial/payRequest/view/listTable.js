b695dd23fc2d531107aee63b799420e6a2c550e0
liaojialing <609095033@qq.com>
2016-06-27 11:55:33 +0800

    b#9455:取消付款申请需要对账才能付款的限制

--- js/template/financial/payRequest/view/listTable.js
+++ js/template/financial/payRequest/view/listTable.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"debug":true,"version":76,"md5":"c15ed98fcd567131e03d078617e123e6"}*/
+/*TMODJS:{"debug":true,"version":78,"md5":"dca5a3b4d8b2140d3f1219b55c5bca4e"}*/
 define(function(require) {
     return require("../../../template")("financial/payRequest/view/listTable", function($data, $filename) {
         try {
@@ -27,9 +27,8 @@ define(function(require) {
                 $line = 19, $out += $escape(rs.checkRemark), $out += "</textarea></td> <td>-</td> <td>-</td> ", 
                 $line = 22), $out += ' <td><label class="pos-rel"> <input type="checkbox" class="ace T-checkbox" ', 
                 $line = 24, (rs.change && 1 == rs.isChecked || !rs.change && 1 == rs.isConfirmAccount) && ($out += 'checked="checked"', 
-                $line = 24), $out += ' /> <span class="lbl"></span> 对账 </label> <a class="cursor"> |</a> <a class="cursor ', 
-                $line = 28, rs.isConfirmAccount ? ($out += "T-pay", $line = 28) : ($out += "gray", 
-                $line = 28), $out += '">付款</a> </td> </tr> ', $line = 31;
+                $line = 24), $out += ' /> <span class="lbl"></span> 对账 </label> <a class="cursor"> |</a> <a class="cursor T-pay">付款</a> </td> </tr> ', 
+                $line = 31;
             }), new String($out);
         } catch (e) {
             throw {
@@ -37,7 +36,7 @@ define(function(require) {
                 name: "Render Error",
                 message: e.message,
                 line: $line,
-                source: '{{each result as rs}}\r\n<tr class="T-checkTr {{if rs.change}}success{{/if}}" data-id="{{rs.id}}" data-type="{{rs.type}}" data-confirm="{{rs.isConfirmAccount}}">\r\n    <td>{{rs.type}}</td>\r\n    <td>{{rs.accountType}}</td>\r\n    <td>{{rs.accountTime}}</td>\r\n    <td>{{rs.count}}</td>\r\n    <td>{{rs.unitName}}</td>\r\n    <td>{{rs.resourceName}}</td>\r\n    <td><span class="F-float F-money">{{rs.needPay}}</span></td>\r\n    <td><span class="F-float F-money T-payedDetail" data-money="{{rs.payed}}">{{rs.payed}}</span></td>\r\n    <td class="F-float F-money" name="unPayedMoney">{{if rs.change}}{{rs.unPayedMoney}}{{else}}{{rs.unpay}}{{/if}}</td>\r\n    {{if rs.isConfirmAccount}}\r\n    <td><span class="F-float F-money">{{rs.realNeedPay}}</span></td>\r\n    <td>{{rs.checkRemark}}</td>\r\n    <td>{{rs.checkTime | dateFormat:"yyyy-MM-dd hh:mm:ss"}}</td>\r\n    <td>{{rs.checkUserName}}</td>\r\n    {{else}}\r\n    <td><input type="text" class="F-float F-money" name="settlementMoney" value="{{if rs.change}}{{rs.settlementMoney}}{{else}}{{rs.needPay}}{{/if}}" /></td>\r\n    <td><textarea class="col-sm-12 hct-textarea" name="checkRemark" maxlength="1000">{{rs.checkRemark}}</textarea></td>\r\n    <td>-</td>\r\n    <td>-</td>\r\n    {{/if}}\r\n    <td><label class="pos-rel">\r\n            <input type="checkbox" class="ace T-checkbox" {{if (rs.change && rs.isChecked == 1) || (!rs.change && rs.isConfirmAccount == 1)}}checked="checked"{{/if}} />\r\n            <span class="lbl"></span> 对账\r\n        </label>\r\n        <a class="cursor"> |</a>\r\n        <a class="cursor {{if rs.isConfirmAccount}}T-pay{{else}}gray{{/if}}">付款</a>\r\n    </td>\r\n</tr>\r\n{{/each}}'.split(/\n/)[$line - 1].replace(/^\s+/, "")
+                source: '{{each result as rs}}\r\n<tr class="T-checkTr {{if rs.change}}success{{/if}}" data-id="{{rs.id}}" data-type="{{rs.type}}" data-confirm="{{rs.isConfirmAccount}}">\r\n    <td>{{rs.type}}</td>\r\n    <td>{{rs.accountType}}</td>\r\n    <td>{{rs.accountTime}}</td>\r\n    <td>{{rs.count}}</td>\r\n    <td>{{rs.unitName}}</td>\r\n    <td>{{rs.resourceName}}</td>\r\n    <td><span class="F-float F-money">{{rs.needPay}}</span></td>\r\n    <td><span class="F-float F-money T-payedDetail" data-money="{{rs.payed}}">{{rs.payed}}</span></td>\r\n    <td class="F-float F-money" name="unPayedMoney">{{if rs.change}}{{rs.unPayedMoney}}{{else}}{{rs.unpay}}{{/if}}</td>\r\n    {{if rs.isConfirmAccount}}\r\n    <td><span class="F-float F-money">{{rs.realNeedPay}}</span></td>\r\n    <td>{{rs.checkRemark}}</td>\r\n    <td>{{rs.checkTime | dateFormat:"yyyy-MM-dd hh:mm:ss"}}</td>\r\n    <td>{{rs.checkUserName}}</td>\r\n    {{else}}\r\n    <td><input type="text" class="F-float F-money" name="settlementMoney" value="{{if rs.change}}{{rs.settlementMoney}}{{else}}{{rs.needPay}}{{/if}}" /></td>\r\n    <td><textarea class="col-sm-12 hct-textarea" name="checkRemark" maxlength="1000">{{rs.checkRemark}}</textarea></td>\r\n    <td>-</td>\r\n    <td>-</td>\r\n    {{/if}}\r\n    <td><label class="pos-rel">\r\n            <input type="checkbox" class="ace T-checkbox" {{if (rs.change && rs.isChecked == 1) || (!rs.change && rs.isConfirmAccount == 1)}}checked="checked"{{/if}} />\r\n            <span class="lbl"></span> 对账\r\n        </label>\r\n        <a class="cursor"> |</a>\r\n        <a class="cursor T-pay">付款</a>\r\n    </td>\r\n</tr>\r\n{{/each}}'.split(/\n/)[$line - 1].replace(/^\s+/, "")
             };
         }
     });
