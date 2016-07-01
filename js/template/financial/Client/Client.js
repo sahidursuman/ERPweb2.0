7533b5ed6d78460b950899f598a2805821425381
liaojialing <609095033@qq.com>
2016-07-01 17:10:57 +0800

    解决冲突

--- js/template/financial/Client/Client.js
+++ js/template/financial/Client/Client.js
@@ -254,19 +254,8 @@ define(function(require, exports) {
         })
         .done(function(data) {
             if(showDialog(data)){
-<<<<<<< HEAD
-                $tab.find('.T-sumCount').text(data.sumCount);
-                $tab.find('.T-sumContractMoney').text(data.sumContractMoney);
-                $tab.find('.T-sumStMoney').text(data.sumSettlementMoney);
-                $tab.find('.T-sumReceiveMoney').text(data.sumReceiveMoney);
-                $tab.find('.T-travelIncome').text(data.sumAgencyMoney);
-                $tab.find('.T-guideIncome').text(data.sumGuideMoney);
-                $tab.find('.T-sumUnReceivedMoney').text(data.sumUnReceivedMoney);
-                $tab.find('.T-sumBalance').text(data.sumBalance);
-=======
                 $tab.data("total",data);
                 Client.loadListSumData($tab);
->>>>>>> remotes/origin/release-1.9.9.4
             }
         });
     };  
